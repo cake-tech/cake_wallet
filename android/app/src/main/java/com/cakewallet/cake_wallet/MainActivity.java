481bab76ad7032400a90ad3d84d8f06a9c9254a2
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2020-03-13 19:31:41 +0200

    bitcoin | added bitcoinj library, created BitcoinWalletManager java class

--- android/app/src/main/java/com/cakewallet/cake_wallet/MainActivity.java
+++ android/app/src/main/java/com/cakewallet/cake_wallet/MainActivity.java
@@ -1,13 +1,27 @@
 package com.cakewallet.cake_wallet;
 
+import androidx.annotation.NonNull;
 import android.os.Bundle;
 import io.flutter.app.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterFragmentActivity {
+  private static final String BITCOIN_WALLET_MANAGER_CHANNEL = "BITCOIN_WALLET_MANAGER";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+  }
+
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith((PluginRegistry) flutterEngine);
+    new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), BITCOIN_WALLET_MANAGER_CHANNEL)
+            .setMethodCallHandler(
+                    (call, result) -> new BitcoinWalletManager().onMethodCall(call, result)
+            );
   }
 }
