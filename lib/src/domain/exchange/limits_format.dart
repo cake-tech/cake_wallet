877522d12e3a0027d40f4066e1e6a83494b17b44
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2020-04-13 20:31:00 +0300

    CWA-199 | moved limitsFormat() to xmrto_exchange_provider

--- lib/src/domain/exchange/limits_format.dart
+++
@@ -1,7 +0,0 @@
-import 'package:intl/intl.dart';
-
-final amountFormat = NumberFormat()
-  ..maximumFractionDigits = 3
-  ..minimumFractionDigits = 1;
-
-double limitsFormat(double limit) => double.parse(amountFormat.format(limit));
