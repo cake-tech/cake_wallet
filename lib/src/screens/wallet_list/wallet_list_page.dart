6a799df7dafbeca8594ab0f639afa0dd7270d253
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2020-04-24 15:01:19 +0300

    CWA-201 | moved WalletTile class to wallet_tile.dart; changed design for PrimaryImageButton; changed background color for wallet_list_page; added short address to current wallet in the wallet_tile

--- lib/src/screens/wallet_list/wallet_list_page.dart
+++ lib/src/screens/wallet_list/wallet_list_page.dart
@@ -9,7 +9,9 @@ import 'package:cake_wallet/src/widgets/primary_button.dart';
 import 'package:cake_wallet/src/screens/base_page.dart';
 import 'package:cake_wallet/src/widgets/scollable_with_bottom_section.dart';
 import 'package:cake_wallet/src/stores/wallet_list/wallet_list_store.dart';
+import 'package:cake_wallet/src/stores/wallet/wallet_store.dart';
 import 'package:cake_wallet/src/screens/wallet_list/wallet_menu.dart';
+import 'package:cake_wallet/src/screens/wallet_list/widgets/wallet_tile.dart';
 
 class WalletListPage extends BasePage {
 
@@ -27,17 +29,20 @@ class WalletListBody extends StatefulWidget {
 
 class WalletListBodyState extends State<WalletListBody> {
   final moneroIcon = Image.asset('assets/images/monero.png', height: 24, width: 24);
+  final newWalletImage = Image.asset('assets/images/new_wallet.png', height: 12, width: 12, color: PaletteDark.historyPanel);
+  final restoreWalletImage = Image.asset('assets/images/restore_wallet.png', height: 12, width: 12, color: Colors.white,);
   WalletListStore _walletListStore;
   ScrollController scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
+    final walletStore = Provider.of<WalletStore>(context);
     _walletListStore = Provider.of<WalletListStore>(context);
 
     return SafeArea(
       child: Container(
         padding: EdgeInsets.only(top: 32),
-        color: PaletteDark.menuHeader,
+        color: PaletteDark.historyPanel,
         child: ScrollableWithBottomSection(
             contentPadding: EdgeInsets.only(bottom: 20),
             content: Container(
@@ -46,7 +51,7 @@ class WalletListBodyState extends State<WalletListBody> {
                     shrinkWrap: true,
                     physics: const NeverScrollableScrollPhysics(),
                     separatorBuilder: (_, index) => Divider(
-                        color: PaletteDark.menuHeader, height: 16),
+                        color: PaletteDark.historyPanel, height: 16),
                     itemCount: _walletListStore.wallets.length,
                     itemBuilder: (__, index) {
                       final wallet = _walletListStore.wallets[index];
@@ -54,6 +59,13 @@ class WalletListBodyState extends State<WalletListBody> {
 
                       final isCurrentWallet =
                       _walletListStore.isCurrentWallet(wallet);
+
+                      String shortAddress = '';
+
+                      if (isCurrentWallet) {
+                        shortAddress = walletStore.subaddress.address;
+                        shortAddress = shortAddress.replaceRange(4, shortAddress.length - 4, '...');
+                      }
 
                       final walletMenu = WalletMenu(context);
                       final items = walletMenu.generateItemsForWalletMenu(isCurrentWallet);
@@ -75,7 +87,7 @@ class WalletListBodyState extends State<WalletListBody> {
                                   max: screenWidth,
                                   image: moneroIcon,
                                   walletName: wallet.name,
-                                  walletAddress: '',
+                                  walletAddress: shortAddress,
                                   isCurrent: isCurrentWallet
                               ),
                             ),
@@ -87,49 +99,7 @@ class WalletListBodyState extends State<WalletListBody> {
                                     final color = colors[index];
                                     final image = images[index];
 
-                                    final content = Center(
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: <Widget>[
-                                          image,
-                                          SizedBox(height: 5),
-                                          Text(
-                                            item,
-                                            textAlign: TextAlign.center,
-                                            style: TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.white
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    );
-
-                                    if (index == 0) {
-                                      return GestureDetector(
-                                        onTap: () {
-                                          scrollController.animateTo(0.0, duration: Duration(milliseconds: 500), curve: Curves.fastOutSlowIn);
-                                          walletMenu.action(
-                                              walletMenu.listItems.indexOf(item), wallet, isCurrentWallet);
-                                        },
-                                        child: Container(
-                                          height: 108,
-                                          width: 108,
-                                          color: PaletteDark.menuHeader,
-                                          child: Container(
-                                            padding: EdgeInsets.only(left: 5, right: 5),
-                                            decoration: BoxDecoration(
-                                                borderRadius: BorderRadius.only(
-                                                    topLeft: Radius.circular(12),
-                                                    bottomLeft: Radius.circular(12)
-                                                ),
-                                                color: color
-                                            ),
-                                            child: content,
-                                          ),
-                                        ),
-                                      );
-                                    }
+                                    final radius = index == 0 ? 12.0 : 0.0;
 
                                     return GestureDetector(
                                       onTap: () {
@@ -140,9 +110,34 @@ class WalletListBodyState extends State<WalletListBody> {
                                       child: Container(
                                         height: 108,
                                         width: 108,
-                                        padding: EdgeInsets.only(left: 5, right: 5),
-                                        color: color,
-                                        child: content,
+                                        color: PaletteDark.historyPanel,
+                                        child: Container(
+                                          padding: EdgeInsets.only(left: 5, right: 5),
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.only(
+                                                  topLeft: Radius.circular(radius),
+                                                  bottomLeft: Radius.circular(radius)
+                                              ),
+                                              color: color
+                                          ),
+                                          child: Center(
+                                            child: Column(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: <Widget>[
+                                                image,
+                                                SizedBox(height: 5),
+                                                Text(
+                                                  item,
+                                                  textAlign: TextAlign.center,
+                                                  style: TextStyle(
+                                                      fontSize: 12,
+                                                      color: Colors.white
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        ),
                                       ),
                                     );
                                   },
@@ -156,143 +151,22 @@ class WalletListBodyState extends State<WalletListBody> {
               ),
             ),
             bottomSection: Column(children: <Widget>[
-              PrimaryIconButton(
-                  onPressed: () => Navigator.of(context).pushNamed(Routes.newWallet),
-                  iconData: Icons.add,
-                  color: Theme.of(context).primaryTextTheme.button.backgroundColor,
-                  borderColor:
-                  Theme.of(context).primaryTextTheme.button.decorationColor,
-                  iconColor: Palette.violet,
-                  iconBackgroundColor: Theme.of(context).primaryIconTheme.color,
-                  text: S.of(context).wallet_list_create_new_wallet),
+              PrimaryImageButton(
+                onPressed: () => Navigator.of(context).pushNamed(Routes.newWallet),
+                image: newWalletImage,
+                text: S.of(context).wallet_list_create_new_wallet,
+                color: Colors.white,
+                textColor: PaletteDark.historyPanel),
               SizedBox(height: 10.0),
-              PrimaryIconButton(
-                  onPressed: () =>
-                      Navigator.of(context).pushNamed(Routes.restoreWalletOptions),
-                  iconData: Icons.refresh,
-                  text: S.of(context).wallet_list_restore_wallet,
-                  color: Theme.of(context).accentTextTheme.button.backgroundColor,
-                  borderColor:
-                  Theme.of(context).accentTextTheme.button.decorationColor,
-                  iconColor: Theme.of(context).primaryTextTheme.caption.color,
-                  iconBackgroundColor: Theme.of(context).accentIconTheme.color)
+              PrimaryImageButton(
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(Routes.restoreWalletOptions),
+                image: restoreWalletImage,
+                text: S.of(context).wallet_list_restore_wallet,
+                color: PaletteDark.historyPanelButton,
+                textColor: Colors.white)
             ])),
       )
     );
   }
-}
-
-class WalletTile extends SliverPersistentHeaderDelegate {
-  WalletTile({
-    @required this.min,
-    @required this.max,
-    @required this.image,
-    @required this.walletName,
-    @required this.walletAddress,
-    @required this.isCurrent
-  });
-
-  final double min;
-  final double max;
-  final Image image;
-  final String walletName;
-  final String walletAddress;
-  final bool isCurrent;
-
-  @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-
-    double opacity = 1 - shrinkOffset / (max - min);
-    opacity = opacity >= 0 ? opacity : 0;
-
-    double panelWidth = 12 * opacity;
-    panelWidth = panelWidth < 12 ? 0 : 12;
-
-    final currentColor = isCurrent
-        ? Colors.white
-        : PaletteDark.menuHeader;
-
-    return Stack(
-      fit: StackFit.expand,
-      overflow: Overflow.visible,
-      children: <Widget>[
-        Positioned(
-          top: 0,
-          right: max - 4,
-          child: Container(
-            height: 108,
-            width: 4,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(topRight: Radius.circular(4), bottomRight: Radius.circular(4)),
-                color: currentColor
-            ),
-          ),
-        ),
-        Positioned(
-          top: 0,
-          right: 12,
-          child: Container(
-            height: 108,
-            width: max - 16,
-            padding: EdgeInsets.only(left: 20, right: 20),
-            color: PaletteDark.menuHeader,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    image,
-                    SizedBox(width: 10),
-                    Text(
-                      walletName,
-                      style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
-        Positioned(
-          top: 0,
-          right: 0,
-          child: Opacity(
-            opacity: opacity,
-            child: Container(
-              height: 108,
-              width: panelWidth,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(12), bottomLeft: Radius.circular(12)),
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        PaletteDark.walletCardTopEndSync,
-                        PaletteDark.walletCardBottomEndSync
-                      ]
-                  )
-              ),
-            ),
-          )
-        ),
-      ],
-    );
-  }
-
-  @override
-  double get maxExtent => max;
-
-  @override
-  double get minExtent => min;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
-
 }
