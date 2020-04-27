6621c95b9dd2d49a59915d58aedbc338da5e2767
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2020-04-27 13:23:55 +0300

    CWA-203 | applied new design to standart_list_row, transaction_details_page and trade_details_page

--- lib/src/screens/transaction_details/standart_list_row.dart
+++ lib/src/screens/transaction_details/standart_list_row.dart
@@ -2,29 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:cake_wallet/palette.dart';
 
 class StandartListRow extends StatelessWidget {
-  StandartListRow({this.title, this.value});
+  StandartListRow({this.title, this.value, this.isDrawTop, this.isDrawBottom});
 
   final String title;
   final String value;
+  final bool isDrawTop;
+  final bool isDrawBottom;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10, bottom: 10),
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(title,
-                style: TextStyle(
-                    fontSize: 14,
-                    color: Theme.of(context).primaryTextTheme.overline.color),
-                textAlign: TextAlign.left),
-            Padding(
-              padding: const EdgeInsets.only(top: 5),
-              child: Text(value,
-                  style: TextStyle(fontSize: 14, color: Palette.wildDarkBlue)),
-            )
-          ]),
+    return Column(
+      children: <Widget>[
+        isDrawTop
+        ? Container(
+          width: double.infinity,
+          height: 1,
+          color: PaletteDark.walletCardTopEndSync,
+        )
+        : Offstage(),
+        Container(
+          width: double.infinity,
+          color: PaletteDark.menuList,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 24, top: 16, bottom: 16, right: 24),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(title,
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          color: PaletteDark.walletCardText),
+                      textAlign: TextAlign.left),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 12),
+                    child: Text(value,
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white)),
+                  )
+                ]),
+          ),
+        ),
+        isDrawBottom
+        ? Container(
+          width: double.infinity,
+          height: 1,
+          color: PaletteDark.walletCardTopEndSync,
+        )
+        : Offstage(),
+      ],
     );
   }
 }
