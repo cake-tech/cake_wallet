75dc42e6da21fe421f4914ed1ddcb146335ef3ee
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2020-04-30 21:59:57 +0300

    CWA-205 | applied new design to restore button, restore options page and restore wallet options page; deleted base restore widget and image widget

--- lib/src/screens/restore/restore_wallet_options_page.dart
+++ lib/src/screens/restore/restore_wallet_options_page.dart
@@ -3,31 +3,59 @@ import 'package:flutter/cupertino.dart';
 import 'package:cake_wallet/routes.dart';
 import 'package:cake_wallet/palette.dart';
 import 'package:cake_wallet/src/screens/restore/widgets/restore_button.dart';
-import 'package:cake_wallet/src/screens/restore/widgets/image_widget.dart';
-import 'package:cake_wallet/src/screens/restore/widgets/base_restore_widget.dart';
 import 'package:cake_wallet/src/screens/base_page.dart';
 import 'package:cake_wallet/generated/i18n.dart';
 import 'package:cake_wallet/src/stores/seed_language/seed_language_store.dart';
 import 'package:provider/provider.dart';
 
 class RestoreWalletOptionsPage extends BasePage {
-  static const _aspectRatioImage = 2.086;
 
   @override
   String get title => S.current.restore_seed_keys_restore;
 
   @override
-  Color get backgroundColor => Palette.creamyGrey;
+  Color get backgroundColor => PaletteDark.historyPanel;
 
-  final _imageSeed = Image.asset('assets/images/seedIco.png');
-  final _imageKeys = Image.asset('assets/images/keysIco.png');
+  final imageSeed = Image.asset('assets/images/restore_seed.png');
+  final imageKeys = Image.asset('assets/images/restore_keys.png');
 
   @override
   Widget body(BuildContext context) {
     final seedLanguageStore = Provider.of<SeedLanguageStore>(context);
-    final isLargeScreen = MediaQuery.of(context).size.height > largeHeight;
 
-    return BaseRestoreWidget(
+    return Container(
+      padding: EdgeInsets.all(24),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            RestoreButton(
+                onPressed: () {
+                  seedLanguageStore.setCurrentRoute(Routes.restoreWalletFromSeed);
+                  Navigator.pushNamed(context, Routes.seedLanguage);
+                },
+                image: imageSeed,
+                title: S.of(context).restore_title_from_seed,
+                description: S.of(context).restore_description_from_seed
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 24),
+              child: RestoreButton(
+                  onPressed: () {
+                    seedLanguageStore.setCurrentRoute(Routes.restoreWalletFromKeys);
+                    Navigator.pushNamed(context, Routes.seedLanguage);
+                  },
+                  image: imageKeys,
+                  title: S.of(context).restore_title_from_keys,
+                  description: S.of(context).restore_description_from_keys
+              ),
+            )
+          ],
+        ),
+      )
+    );
+
+
+      /*BaseRestoreWidget(
       firstRestoreButton: RestoreButton(
         onPressed: () {
           seedLanguageStore.setCurrentRoute(Routes.restoreWalletFromSeed);
@@ -59,6 +87,6 @@ class RestoreWalletOptionsPage extends BasePage {
         textButton: S.of(context).restore_next,
       ),
       isLargeScreen: isLargeScreen,
-    );
+    );*/
   }
 }
