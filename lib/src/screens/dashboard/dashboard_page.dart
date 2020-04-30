c7a07fbf40f83231a1a2c6beac1e6f4a25882896
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2020-04-30 20:35:06 +0300

    CWA-198 | changed background color of dashboard page; added color of bounds and shadows to wallet card; changed color of buttons in the button_header

--- lib/src/screens/dashboard/dashboard_page.dart
+++ lib/src/screens/dashboard/dashboard_page.dart
@@ -9,7 +9,26 @@ class DashboardPage extends BasePage {
   final _bodyKey = GlobalKey();
 
   @override
-  ObstructingPreferredSizeWidget appBar(BuildContext context) => null;
+  Color get backgroundColor => PaletteDark.mainBackgroundColor;
+
+  @override
+  Widget trailing(BuildContext context) {
+    final menuButton = Image.asset('assets/images/header.png');
+
+    return SizedBox(
+      height: 37,
+      width: 37,
+      child: ButtonTheme(
+        minWidth: double.minPositive,
+        child: FlatButton(
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          padding: EdgeInsets.all(0),
+          onPressed: () {},
+          child: menuButton),
+      ),
+    );
+  }
 
   @override
   Widget body(BuildContext context) => DashboardPageBody(key: _bodyKey);
@@ -23,43 +42,17 @@ class DashboardPageBody extends StatefulWidget {
 }
 
 class DashboardPageBodyState extends State<DashboardPageBody> {
-  final menuButton = Image.asset('assets/images/menu_button.png');
 
   @override
   Widget build(BuildContext context) {
-    final List<Color> colors = [PaletteDark.backgroundStart, PaletteDark.backgroundEnd];
 
     return SafeArea(
       child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: colors
-          )
-        ),
+        color: PaletteDark.mainBackgroundColor,
         child: Column(
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.only(top: 14),
-              child: Container(
-                width: double.infinity,
-                alignment: Alignment.centerRight,
-                child: SizedBox(
-                  height: 37,
-                  width: 37,
-                  child: ButtonTheme(
-                    minWidth: double.minPositive,
-                    child: FlatButton(
-                        highlightColor: Colors.transparent,
-                        splashColor: Colors.transparent,
-                        padding: EdgeInsets.all(0),
-                        onPressed: () {},
-                        child: menuButton),
-                  ),
-                ),
-              )
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20, top: 23),
+              padding: EdgeInsets.only(left: 20, top: 10),
               child: WalletCard(),
             ),
             SizedBox(
