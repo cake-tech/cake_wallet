8c6b99da4284b25fbdad2ac5ddc293c251db9472
M <m@cakewallet.com>
2020-05-25 16:06:40 +0300

    Core stuff

---
+++ lib/core/app_store.dart
@@ -0,0 +1,12 @@
+import 'package:mobx/mobx.dart';
+
+part 'app_store.g.dart';
+
+class AppStore = AppStoreBase with _$AppStore;
+
+abstract class AppStoreBase with Store {
+  // Sign Up
+  // Auth
+  // Wallet
+  // Settings 
+}
