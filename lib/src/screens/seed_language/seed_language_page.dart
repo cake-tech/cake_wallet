c59ee804331da0f97aac24fa386b27d446e9a257
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2020-06-10 16:08:57 +0300

    CWA-221 | reworked seed_language_page as adaptive page; added crypto_lock_light; decreased font size of wallet address on the wallet card

--- lib/src/screens/seed_language/seed_language_page.dart
+++ lib/src/screens/seed_language/seed_language_page.dart
@@ -5,10 +5,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:cake_wallet/generated/i18n.dart';
 import 'package:cake_wallet/src/screens/base_page.dart';
 import 'package:cake_wallet/src/widgets/primary_button.dart';
-import 'package:cake_wallet/src/widgets/scollable_with_bottom_section.dart';
 import 'package:cake_wallet/src/stores/seed_language/seed_language_store.dart';
 import 'package:cake_wallet/src/screens/new_wallet/widgets/select_button.dart';
 import 'package:cake_wallet/src/screens/seed_language/widgets/seed_language_picker.dart';
+import 'package:cake_wallet/themes.dart';
+import 'package:cake_wallet/theme_changer.dart';
 
 class SeedLanguage extends BasePage {
   @override
@@ -22,11 +23,15 @@ class SeedLanguageForm extends StatefulWidget {
 
 class SeedLanguageFormState extends State<SeedLanguageForm> {
   static const aspectRatioImage = 1.22;
-  final walletNameImage = Image.asset('assets/images/wallet_name.png');
+  final walletNameImageLight = Image.asset('assets/images/wallet_name_light.png');
+  final walletNameImageDark = Image.asset('assets/images/wallet_name.png');
 
   @override
   Widget build(BuildContext context) {
     final seedLanguageStore = Provider.of<SeedLanguageStore>(context);
+    final _themeChanger = Provider.of<ThemeChanger>(context);
+    final walletNameImage = _themeChanger.getTheme() == Themes.darkTheme
+        ? walletNameImageDark : walletNameImageLight;
 
     final List<String> seedLocales = [
       S.current.seed_language_english,
@@ -40,55 +45,65 @@ class SeedLanguageFormState extends State<SeedLanguageForm> {
     ];
 
     return Container(
-      padding: EdgeInsets.only(top: 24),
-      child: ScrollableWithBottomSection(
-          contentPadding: EdgeInsets.only(left: 24, right: 24, bottom: 24),
-          content: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 12, right: 12),
-                  child: AspectRatio(
-                      aspectRatio: aspectRatioImage,
-                      child: FittedBox(child: walletNameImage, fit: BoxFit.fill)),
-                ),
-                Padding(padding: EdgeInsets.only(top: 40),
-                  child: Text(
-                    S.of(context).seed_language_choose,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600,
-                        color: Theme.of(context).primaryTextTheme.title.color
+      padding: EdgeInsets.all(24),
+      child: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 2,
+            child: AspectRatio(
+              aspectRatio: aspectRatioImage,
+              child: FittedBox(child: walletNameImage, fit: BoxFit.fill)
+            )
+          ),
+          Flexible(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.only(top: 48),
+                      child: Text(
+                        S.of(context).seed_language_choose,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context).primaryTextTheme.title.color
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(top: 24),
-                  child: Observer(
-                      builder: (_) => SelectButton(
-                          image: null,
-                          text: seedLocales[seedLanguages.indexOf(seedLanguageStore.selectedSeedLanguage)],
-                          color: Theme.of(context).accentTextTheme.title.backgroundColor,
-                          textColor: Theme.of(context).primaryTextTheme.title.color,
-                          onTap: () async => await showDialog(
-                              context: context,
-                              builder: (BuildContext context) => SeedLanguagePicker()
+                    Padding(padding: EdgeInsets.only(top: 24),
+                      child: Observer(
+                          builder: (_) => SelectButton(
+                              image: null,
+                              text: seedLocales[seedLanguages.indexOf(seedLanguageStore.selectedSeedLanguage)],
+                              color: Theme.of(context).accentTextTheme.title.backgroundColor,
+                              textColor: Theme.of(context).primaryTextTheme.title.color,
+                              onTap: () async => await showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) => SeedLanguagePicker()
+                              )
                           )
-                      )
-                  ),
+                      ),
+                    )
+                  ],
+                ),
+                Observer(
+                  builder: (context) {
+                    return PrimaryButton(
+                        onPressed: () =>
+                            Navigator.of(context).popAndPushNamed(seedLanguageStore.currentRoute),
+                        text: S.of(context).seed_language_next,
+                        color: Colors.green,
+                        textColor: Colors.white);
+                  },
                 )
-              ]),
-          bottomSectionPadding: EdgeInsets.only(left: 24, right: 24, bottom: 24),
-          bottomSection: Observer(
-            builder: (context) {
-              return PrimaryButton(
-                  onPressed: () =>
-                      Navigator.of(context).popAndPushNamed(seedLanguageStore.currentRoute),
-                  text: S.of(context).seed_language_next,
-                  color: Colors.green,
-                  textColor: Colors.white);
-            },
-          )),
+              ],
+            )
+          )
+        ],
+      )
     );
   }
 }
