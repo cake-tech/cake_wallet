9d15a8f0f546321b9300a682a2c105ddbf26fe3f
OleksandrSobol <dr.alexander.sobol@gmail.com>
2020-09-09 22:12:49 +0300

    CAKE-39 | changed monero logo (contact list, wallet list, new wallet type pages); changed buttons color on alerts, wallet list page, new wallet type page, receive page, monero account list page

--- lib/themes.dart
+++ lib/themes.dart
@@ -48,7 +48,8 @@ class Themes {
       ),
       display3: TextStyle(
         color: Colors.white, // text color of current tile (receive page),
-        decorationColor: Palette.blueCraiola // background of current tile (receive page)
+        //decorationColor: Palette.blueCraiola // background of current tile (receive page)
+        decorationColor: Palette.moderateSlateBlue // background of current tile (receive page)
       ),
       display4: TextStyle(
         color: Palette.violetBlue, // text color of tiles (account list)
@@ -56,7 +57,8 @@ class Themes {
       ),
       subtitle: TextStyle(
         color: Colors.white, // text color of current tile (account list)
-        decorationColor: Palette.blueCraiola // background of current tile (account list)
+        //decorationColor: Palette.blueCraiola // background of current tile (account list)
+        decorationColor: Palette.moderateSlateBlue // background of current tile (account list)
       ),
       body1: TextStyle(
         color: Palette.moderatePurpleBlue, // scrollbar thumb
@@ -132,7 +134,8 @@ class Themes {
       subtitle: TextStyle(
         color: Palette.darkBlueCraiola, // QR code (exchange trade page)
         backgroundColor: Palette.wildPeriwinkle, // divider (exchange trade page)
-        decorationColor: Palette.blueCraiola // crete new wallet button background (wallet list page)
+        //decorationColor: Palette.blueCraiola // crete new wallet button background (wallet list page)
+        decorationColor: Palette.moderateSlateBlue // crete new wallet button background (wallet list page)
       ),
       headline: TextStyle(
         color: Palette.moderateLavender, // first gradient color of wallet action buttons (wallet list page)
@@ -166,7 +169,11 @@ class Themes {
       body1: TextStyle(
         color: Palette.darkGray, // indicators (PIN code)
         decorationColor: Palette.darkGray // switch (PIN code)
-      )
+      ),
+      body2: TextStyle(
+        color: Palette.moderateSlateBlue, // primary buttons, alert right buttons
+        decorationColor: Palette.brightOrange // alert left button
+      ),
     ),
   );
 
@@ -334,7 +341,11 @@ class Themes {
       body1: TextStyle(
         color: PaletteDark.indicatorVioletBlue, // indicators (PIN code)
         decorationColor: PaletteDark.lightPurpleBlue // switch (PIN code)
-      )
+      ),
+      body2: TextStyle(
+        color: Palette.blueCraiola, // primary buttons, alert right buttons
+        decorationColor: Palette.alizarinRed // alert left button
+      ),
     ),
   );
 }
