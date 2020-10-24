081b189b63f57948a4312b2f1d06011c4f915948
Tanner Silva <t@tannersilva.com>
2020-10-24 13:50:12 -0500

    generateName() now returns strings with capitalized words

--- lib/entities/generate_name.dart
+++ lib/entities/generate_name.dart
@@ -2,17 +2,23 @@ import 'dart:math';
 
 import 'package:flutter/services.dart';
 
+extension StringExtension on String {
+  String capitalized() {
+    return "${this[0].toUpperCase()}${this.substring(1)}";
+  }
+}
+
 Future<String> generateName() async {
   final randomThing = Random();
   final adjectiveStringRaw =
-  await rootBundle.loadString('assets/text/Wallet_Adjectives.txt');
+      await rootBundle.loadString('assets/text/Wallet_Adjectives.txt');
   final nounStringRaw =
-  await rootBundle.loadString('assets/text/Wallet_Nouns.txt');
+      await rootBundle.loadString('assets/text/Wallet_Nouns.txt');
   final adjectives = List<String>.from(adjectiveStringRaw.split('\n'));
   final nouns = List<String>.from(nounStringRaw.split('\n'));
   final chosenAdjective = adjectives[randomThing.nextInt(adjectives.length)];
   final chosenNoun = nouns[randomThing.nextInt(nouns.length)];
-  final returnString = chosenAdjective + ' ' + chosenNoun;
-
+  final returnString =
+      chosenAdjective.capitalized() + ' ' + chosenNoun.capitalized();
   return returnString;
 }
