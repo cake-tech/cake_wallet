29832e26ac6d4a4ebe144539b55d14e967ac00b5
OleksandrSobol <dr.alexander.sobol@gmail.com>
2020-10-30 15:11:20 +0200

    CAKE-136 | added "-" symbol to node_address_validator.dart; changed minLength from 1 to 0 in the node_port_validator.dart

--- lib/core/node_address_validator.dart
+++ lib/core/node_address_validator.dart
@@ -6,5 +6,5 @@ class NodeAddressValidator extends TextValidator {
       : super(
             errorMessage: S.current.error_text_node_address,
             pattern:
-                '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\$|^[0-9a-zA-Z.]+\$');
+                '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\$|^[0-9a-zA-Z.\-]+\$');
 }
