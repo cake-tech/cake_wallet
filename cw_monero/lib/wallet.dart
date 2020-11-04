fdb0f49f88d458fe4d87b12a27b62328c44608c7
M <m@cakewallet.com>
2020-11-04 17:54:44 +0200

    Fixes.

--- cw_monero/lib/wallet.dart
+++ cw_monero/lib/wallet.dart
@@ -212,13 +212,18 @@ String getPublicSpendKey() =>
     convertUTF8ToString(pointer: getPublicSpendKeyNative());
 
 class SyncListener {
-  SyncListener({this.onNewBlock});
+  SyncListener({this.onNewBlock}) {
+    _cachedBlockchainHeight = 0;
+    _lastKnownBlockHeight = 0;
+    _initialSyncHeight = 0;
+  }
 
   void Function(int, int, double) onNewBlock;
 
   Timer _updateSyncInfoTimer;
-  int _cachedBlockchainHeight = 0;
-  int _lastKnownBlockHeight = 0;
+  int _cachedBlockchainHeight;
+  int _lastKnownBlockHeight;
+  int _initialSyncHeight;
 
   Future<int> getNodeHeightOrUpdate(int baseHeight) async {
     if (_cachedBlockchainHeight < baseHeight || _cachedBlockchainHeight == 0) {
@@ -231,12 +236,17 @@ class SyncListener {
   void start() {
     _cachedBlockchainHeight = 0;
     _lastKnownBlockHeight = 0;
+    _initialSyncHeight = 0;
     _updateSyncInfoTimer ??=
         Timer.periodic(Duration(milliseconds: 1200), (_) async {
       var syncHeight = getSyncingHeight();
 
       if (syncHeight <= 0) {
         syncHeight = getCurrentHeight();
+      }
+
+      if (_initialSyncHeight <= 0) {
+        _initialSyncHeight = syncHeight;
       }
 
       final bchHeight = await getNodeHeightOrUpdate(syncHeight);
@@ -246,10 +256,15 @@ class SyncListener {
       }
 
       _lastKnownBlockHeight = syncHeight;
-      final track = bchHeight - syncHeight;
+      final track = bchHeight - _initialSyncHeight;
       final diff = track - (bchHeight - syncHeight);
       final ptc = diff <= 0 ? 0.0 : diff / track;
       final left = bchHeight - syncHeight;
+
+      if (syncHeight < 0 || left < 0) {
+        return;
+      }
+
       // 1. Actual new height; 2. Blocks left to finish; 3. Progress in percents;
       onNewBlock?.call(syncHeight, left, ptc);
     });
