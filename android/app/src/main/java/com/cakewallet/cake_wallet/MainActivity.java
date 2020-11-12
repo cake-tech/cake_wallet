52c5d1178708934865b99cb0bb9a939ffbe5cd44
M <m@cakewallet.com>
2020-11-12 20:07:13 +0200

    Fixes for biometrical auth for android

--- android/app/src/main/java/com/cakewallet/cake_wallet/MainActivity.java
+++ android/app/src/main/java/com/cakewallet/cake_wallet/MainActivity.java
@@ -1,6 +1,15 @@
 package com.cakewallet.cake_wallet;
 
-import io.flutter.embedding.android.FlutterActivity;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterActivity {
+
+public class MainActivity extends FlutterFragmentActivity {
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
