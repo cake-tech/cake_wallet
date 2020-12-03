b309d0189256e31e1380002c7e51d90138bd183c
M <m@cakewallet.com>
2020-12-03 19:30:56 +0200

    Removed swipe back on preseed and seed screens for new wallet.

--- lib/src/screens/seed/pre_seed_page.dart
+++ lib/src/screens/seed/pre_seed_page.dart
@@ -22,51 +22,46 @@ class PreSeedPage extends BasePage {
     final image =
         getIt.get<SettingsStore>().isDarkTheme ? imageDark : imageLight;
 
-    return Container(
-      padding: EdgeInsets.all(24),
-      child: Column(
-        children: [
-          Flexible(
-              flex: 2,
-              child: AspectRatio(
-                  aspectRatio: 1,
-                  child: FittedBox(child: image, fit: BoxFit.contain))),
-          Flexible(
-              flex: 3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding:
-                    EdgeInsets.only(top: 70, left: 16, right: 16),
-                    child: Text(
-                      S.of(context).pre_seed_description,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
-                          color: Theme.of(context)
-                              .primaryTextTheme
-                              .caption
-                              .color),
-                    ),
-                  ),
-                  PrimaryButton(
-                      onPressed: () =>
-                          Navigator.of(context).popAndPushNamed(Routes.seed,
-                              arguments: true),
-                      text: S.of(context).pre_seed_button_text,
-                      color: Theme.of(context)
-                          .accentTextTheme
-                          .body2
-                          .color,
-                      textColor: Colors.white)
-                ],
-              )
-          )
-        ],
-      ),
-    );
+    return WillPopScope(
+        onWillPop: () async => false,
+        child: Container(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            children: [
+              Flexible(
+                  flex: 2,
+                  child: AspectRatio(
+                      aspectRatio: 1,
+                      child: FittedBox(child: image, fit: BoxFit.contain))),
+              Flexible(
+                  flex: 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 70, left: 16, right: 16),
+                        child: Text(
+                          S.of(context).pre_seed_description,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                              color: Theme.of(context)
+                                  .primaryTextTheme
+                                  .caption
+                                  .color),
+                        ),
+                      ),
+                      PrimaryButton(
+                          onPressed: () => Navigator.of(context)
+                              .popAndPushNamed(Routes.seed, arguments: true),
+                          text: S.of(context).pre_seed_button_text,
+                          color: Theme.of(context).accentTextTheme.body2.color,
+                          textColor: Colors.white)
+                    ],
+                  ))
+            ],
+          ),
+        ));
   }
-
 }
