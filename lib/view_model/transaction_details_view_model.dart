6897d6f4031161a0d0e0e7e8ee9e6447b561b9d7
OleksandrSobol <dr.alexander.sobol@gmail.com>
2020-12-22 20:42:30 +0200

    CAKE-198 | applied ability to make a note with some details about transaction; added transactionNote property and note getter to transaction_description.dart; added multiline textfield for notes (send_page.dart); added transaction_details_list_item.dart, textfield_list_item.dart, textfield_list_row.dart, transaction_details_view_model.dart to the app

---
+++ lib/view_model/transaction_details_view_model.dart
@@ -0,0 +1,105 @@
+import 'package:cake_wallet/bitcoin/bitcoin_transaction_info.dart';
+import 'package:cake_wallet/entities/transaction_info.dart';
+import 'package:cake_wallet/monero/monero_transaction_info.dart';
+import 'package:cake_wallet/src/screens/transaction_details/standart_list_item.dart';
+import 'package:cake_wallet/src/screens/transaction_details/textfield_list_item.dart';
+import 'package:cake_wallet/src/screens/transaction_details/transaction_details_list_item.dart';
+import 'package:cake_wallet/utils/date_formatter.dart';
+import 'package:cake_wallet/entities/transaction_description.dart';
+import 'package:hive/hive.dart';
+import 'package:mobx/mobx.dart';
+import 'package:cake_wallet/store/settings_store.dart';
+import 'package:cake_wallet/generated/i18n.dart';
+
+part 'transaction_details_view_model.g.dart';
+
+class TransactionDetailsViewModel = TransactionDetailsViewModelBase with _$TransactionDetailsViewModel;
+
+abstract class TransactionDetailsViewModelBase with Store {
+  TransactionDetailsViewModelBase({
+    this.transactionInfo,
+    this.transactionDescriptionBox,
+    this.settingsStore}) : items = [] {
+
+    showRecipientAddress = settingsStore?.shouldSaveRecipientAddress ?? false;
+
+    final dateFormat = DateFormatter.withCurrentLocal();
+    final tx = transactionInfo;
+
+    if (tx is MoneroTransactionInfo) {
+      final _items = [
+        StandartListItem(
+            title: S.current.transaction_details_transaction_id, value: tx.id),
+        StandartListItem(
+            title: S.current.transaction_details_date,
+            value: dateFormat.format(tx.date)),
+        StandartListItem(
+            title: S.current.transaction_details_height, value: '${tx.height}'),
+        StandartListItem(
+            title: S.current.transaction_details_amount,
+            value: tx.amountFormatted()),
+        StandartListItem(title: S.current.send_fee, value: tx.feeFormatted()),
+      ];
+
+      if (tx.key?.isNotEmpty ?? null) {
+        // FIXME: add translation
+        _items.add(StandartListItem(title: 'Transaction Key', value: tx.key));
+      }
+
+      items.addAll(_items);
+    }
+
+    if (tx is BitcoinTransactionInfo) {
+      final _items = [
+        StandartListItem(
+            title: S.current.transaction_details_transaction_id, value: tx.id),
+        StandartListItem(
+            title: S.current.transaction_details_date,
+            value: dateFormat.format(tx.date)),
+        // FIXME: add translation
+        StandartListItem(
+            title: 'Confirmations', value: tx.confirmations?.toString()),
+        StandartListItem(
+            title: S.current.transaction_details_height, value: '${tx.height}'),
+        StandartListItem(
+            title: S.current.transaction_details_amount,
+            value: tx.amountFormatted()),
+        if (tx.feeFormatted()?.isNotEmpty)
+          StandartListItem(title: S.current.send_fee, value: tx.feeFormatted())
+      ];
+
+      items.addAll(_items);
+    }
+
+    if (showRecipientAddress) {
+      final recipientAddress = transactionDescriptionBox.values
+          .firstWhere((val) => val.id == transactionInfo.id, orElse: () => null)
+          ?.recipientAddress;
+
+      if (recipientAddress?.isNotEmpty ?? false) {
+        items.add(StandartListItem(
+            title: S.current.transaction_details_recipient_address,
+            value: recipientAddress));
+      }
+    }
+
+    final description = transactionDescriptionBox.values.firstWhere(
+            (val) => val.id == transactionInfo.id, orElse: () => null);
+
+    if (description != null) {
+      // FIXME: add translation
+      items.add(TextFieldListItem(title: 'Note (tap to change)',
+          value: description.note, onSubmitted: (value) {
+            description.transactionNote = value;
+            description.save();
+          }));
+    }
+  }
+
+  final TransactionInfo transactionInfo;
+  final Box<TransactionDescription> transactionDescriptionBox;
+  final SettingsStore settingsStore;
+
+  final List<TransactionDetailsListItem> items;
+  bool showRecipientAddress;
+}
