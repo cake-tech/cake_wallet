248cb3a0da3c238e3a613b417cf2bd4e137d4594
Tanner Silva <t@tannersilva.com>
2021-01-20 20:01:49 -0600

    Block explorer row in transaction details is now implemented as an item subclass rather than being index dependent in the list

--- lib/src/screens/transaction_details/transaction_details_page.dart
+++ lib/src/screens/transaction_details/transaction_details_page.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:cake_wallet/generated/i18n.dart';
 import 'package:cake_wallet/src/widgets/standart_list_row.dart';
+import 'package:cake_wallet/src/screens/transaction_details/blockexplorer_list_item.dart';
 import 'package:cake_wallet/src/screens/transaction_details/standart_list_item.dart';
 import 'package:cake_wallet/src/screens/base_page.dart';
 import 'package:cake_wallet/utils/date_formatter.dart';
@@ -14,19 +15,6 @@ import 'package:cake_wallet/utils/date_formatter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'package:hive/hive.dart';
-
-String stripURLToTXID(String inputString) {
-  final RegExp txidRegex = new RegExp('[a-z0-9]{64}');
-  return txidRegex.allMatches(inputString).first.group(0);
-}
-
-String blockExplorerName(String inputName) {
-  if (inputName.contains("xmrchain")) {
-    return "XMRChain.net";
-  } else {
-    return "Blockchain.com";
-  }
-}
 
 class TransactionDetailsPage extends BasePage {
   TransactionDetailsPage({this.transactionDetailsViewModel});
@@ -45,31 +33,23 @@ class TransactionDetailsPage extends BasePage {
           final item = transactionDetailsViewModel.items[index];
 
           if (item is StandartListItem) {
-            final isFinalBlockExplorerItem =
-                index == transactionDetailsViewModel.items.length - 1
-                    ? true
-                    : false;
-            if (isFinalBlockExplorerItem == false) {
-              return GestureDetector(
-                onTap: () {
-                  Clipboard.setData(ClipboardData(text: item.value));
-                  showBar<void>(context,
-                      S.of(context).transaction_details_copied(item.title));
-                },
-                child:
-                    StandartListRow(title: '${item.title}:', value: item.value),
-              );
-            } else {
-              return GestureDetector(
-                onTap: () {
-                  launch(item.value);
-                },
-                child: StandartListRow(
-                    title: '${item.title}:',
-                    value:
-                        "View transaction on ${blockExplorerName(item.value)}"),
-              );
-            }
+            return GestureDetector(
+              onTap: () {
+                Clipboard.setData(ClipboardData(text: item.value));
+                showBar<void>(context,
+                    S.of(context).transaction_details_copied(item.title));
+              },
+              child:
+                  StandartListRow(title: '${item.title}:', value: item.value),
+            );
+          }
+
+          if (item is BlockExplorerListItem) {
+            return GestureDetector(
+              onTap: item.onTap,
+              child:
+                  StandartListRow(title: '${item.title}:', value: item.value),
+            );
           }
 
           if (item is TextFieldListItem) {
