9017a99a8d941a6be81e64bb350c6c6844769c62
M <m@cakewallet.com>
2021-03-24 12:11:37 +0200

    Update MainActivity.java
    Undo changes for MainActivity.java file

--- android/app/src/main/java/com/cakewallet/cake_wallet/MainActivity.java
+++ android/app/src/main/java/com/cakewallet/cake_wallet/MainActivity.java
@@ -1,13 +1,13 @@
 package com.cakewallet.cake_wallet;
 
 import androidx.annotation.NonNull;
-
 import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 
 public class MainActivity extends FlutterFragmentActivity {
+    
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
