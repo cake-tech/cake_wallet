0848ede37f8062478afa30ff8417ef5d54320b50
OleksandrSobol <dr.alexander.sobol@gmail.com>
2021-03-31 20:02:44 +0300

    CAKE-305 | fixed transfer to wyre next btc address

--- lib/view_model/wyre_view_model.dart
+++ lib/view_model/wyre_view_model.dart
@@ -2,7 +2,6 @@ import 'package:cake_wallet/entities/wyre_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
 import 'package:cake_wallet/entities/order.dart';
-import 'package:cake_wallet/entities/wallet_type.dart';
 import 'package:cake_wallet/store/dashboard/orders_store.dart';
 import 'package:mobx/mobx.dart';
 
@@ -12,8 +11,7 @@ class WyreViewModel = WyreViewModelBase with _$WyreViewModel;
 
 abstract class WyreViewModelBase with Store {
   WyreViewModelBase(this.ordersSource, this.ordersStore,
-      {@required this.walletId, @required this.address, @required this.type,
-      @required this.wyreService});
+      {@required this.wyreService});
 
   Future<String> get wyreUrl => wyreService.getWyreUrl();
 
@@ -22,17 +20,11 @@ abstract class WyreViewModelBase with Store {
   final Box<Order> ordersSource;
   final OrdersStore ordersStore;
 
-  final String walletId;
-  final WalletType type;
-  final String address;
-
   final WyreService wyreService;
 
   Future<void> saveOrder(String orderId) async {
     try {
       final order = await wyreService.findOrderById(orderId);
-      order.receiveAddress = address;
-      order.walletId = walletId;
       await ordersSource.add(order);
       ordersStore.setOrder(order);
     } catch (e) {
