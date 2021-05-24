4d6cc87529cb25f860d227992a962ea70c623cd7
OleksandrSobol <dr.alexander.sobol@gmail.com>
2021-05-24 21:06:12 +0300

    CAKE-192 | merged main branch into current; fixed call unstoppable domains api for different sdk versions; fixed italian and croatian locales

--- android/app/src/main/java/com/cakewallet/cake_wallet/MainActivity.java
+++ android/app/src/main/java/com/cakewallet/cake_wallet/MainActivity.java
@@ -10,28 +10,28 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 
-import java.security.SecureRandom;
 import com.unstoppabledomains.resolution.DomainResolution;
 import com.unstoppabledomains.resolution.Resolution;
 
-import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Looper;
-
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
+import java.security.SecureRandom;
 
 public class MainActivity extends FlutterFragmentActivity {
     final String UTILS_CHANNEL = "com.cake_wallet/native_utils";
-
     final String UNSTOPPABLE_DOMAIN_CHANNEL = "com.cakewallet.cake_wallet/unstoppable-domain";
 
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        MethodChannel utilsChannel =
+                new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(),
+                        UTILS_CHANNEL);
+
+        utilsChannel.setMethodCallHandler(this::handle);
 
         MethodChannel unstoppableDomainChannel =
                 new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(),
@@ -41,14 +41,30 @@ public class MainActivity extends FlutterFragmentActivity {
     }
 
     private void handle(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+        Handler handler = new Handler(Looper.getMainLooper());
+
         try {
-            if (call.method.equals("getUnstoppableDomainAddress")) {
-                getUnstoppableDomainAddress(call, result);
-            } else {
-                result.notImplemented();
+            switch (call.method) {
+                case "sec_random":
+                    int count = call.argument("count");
+                    SecureRandom random = new SecureRandom();
+                    byte bytes[] = new byte[count];
+                    random.nextBytes(bytes);
+                    handler.post(() -> result.success(bytes));
+                    break;
+                case "getUnstoppableDomainAddress":
+                    int  version = Build.VERSION.SDK_INT;
+                    if (version >= 24) {
+                        getUnstoppableDomainAddress(call, result);
+                    } else {
+                        handler.post(() -> result.success(""));
+                    }
+                    break;
+                default:
+                    handler.post(() -> result.notImplemented());
             }
         } catch (Exception e) {
-            result.error("UNCAUGHT_ERROR", e.getMessage(), null);
+            handler.post(() -> result.error("UNCAUGHT_ERROR", e.getMessage(), null));
         }
     }
 
@@ -66,29 +82,5 @@ public class MainActivity extends FlutterFragmentActivity {
                 handler.post(() -> result.error("INVALID DOMAIN", e.getMessage(), null));
             }
         });
-
-        MethodChannel utilsChannel =
-                new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(),
-                        UTILS_CHANNEL);
-
-        utilsChannel.setMethodCallHandler(this::handle);
-    }
-
-    private void handle(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
-        Handler handler = new Handler(Looper.getMainLooper());
-
-        try {
-            if (call.method.equals("sec_random")) {
-                int count = call.argument("count");
-                SecureRandom random = new SecureRandom();
-                byte bytes[] = new byte[count];
-                random.nextBytes(bytes);
-                handler.post(() -> result.success(bytes));
-            } else {
-                handler.post(() -> result.notImplemented());
-            }
-        } catch (Exception e) {
-            handler.post(() -> result.error("UNCAUGHT_ERROR", e.getMessage(), null));
-        }
     }
 }
