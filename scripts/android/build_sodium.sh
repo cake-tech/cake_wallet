727bab0cec5b6867a9db18c7db6083971ba67aeb
Mykola Misiura <mmisiura@protonmail.com>
2021-08-31 12:27:30 +0300

    Update android build scripts.

--- scripts/android/build_sodium.sh
+++ scripts/android/build_sodium.sh
@@ -1,21 +1,24 @@
-# /bin/bash
+#!/bin/sh
 
-WORKDIR=/opt/android
-TOOLCHAIN_BASE_DIR=${WORKDIR}/toolchain
+. ./config.sh
 SODIUM_SRC_DIR=${WORKDIR}/libsodium
 SODIUM_BRANCH=1.0.16
-ORIGINAL_PATH=$PATH
 
 for arch in "aarch" "aarch64" "i686" "x86_64"
 do
 
-PREFIX=$WORKDIR/prefix_${arch}
-PATH="${TOOLCHAIN_BASE_DIR}_${arch}/bin:${ORIGINAL_PATH}"
+PREFIX=${WORKDIR}/prefix_${arch}
 
 case $arch in
-	"aarch"	) TARGET="arm";;
-	"i686"		) TARGET="x86";;
-	*		) TARGET="${arch}";;
+	"aarch"	) TARGET="arm"
+			  CC=armv7a-linux-androideabi${API}-clang
+			  CXX=armv7a-linux-androideabi${API}-clang++;;
+	"i686"		) TARGET="x86"
+			  CC=${arch}-linux-android${API}-clang
+			  CXX=${arch}-linux-android${API}-clang++;;
+	*		) TARGET="${arch}"
+			  CC=${arch}-linux-android${API}-clang
+			  CXX=${arch}-linux-android${API}-clang++;;
 esac 
 
 HOST="${TARGET}-linux-android"
@@ -24,8 +27,9 @@ rm -rf $SODIUM_SRC_DIR
 git clone https://github.com/jedisct1/libsodium.git $SODIUM_SRC_DIR -b $SODIUM_BRANCH
 cd $SODIUM_SRC_DIR
 ./autogen.sh
-CC=clang CXX=clang++ ./configure --prefix=${PREFIX} --host=${HOST} --enable-static --disable-shared
+./configure --prefix=${PREFIX} --host=${HOST} --enable-static --disable-shared
 make
 make install
 
 done
+
