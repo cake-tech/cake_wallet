fff0d8129490a679be192f7bb4f86ca41ef5e455
M <m@cakewallet.com>
2021-12-30 21:14:48 +0200

    Fixes for wallet menu

--- lib/src/screens/dashboard/wallet_menu_item.dart
+++ lib/src/screens/dashboard/wallet_menu_item.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 class WalletMenuItem {
   WalletMenuItem({
     @required this.title,
-    @required this.image});
+    @required this.image,
+    @required this.handler});
 
   final String title;
   final Image image;
+  final void Function() handler;
 }
