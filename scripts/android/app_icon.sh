5ac56b95c3419194ba71f81ace20916a51488fb0
M <m@cakewallet.com>
2022-01-04 20:33:57 +0200

    Icon generation for android

---
+++ scripts/android/app_icon.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ICON_PATH=""
+DEST_PATH=`pwd`/../../assets/images/app_logo.png
+
+case $APP_IOS_TYPE in
+	"monero.com")
+	ICON_PATH=`pwd`/../../assets/images/monero.com_logo.png;;
+	"cakewallet")
+        ICON_PATH=`pwd`/../../assets/images/cakewallet_logo.png;;
+esac
+
+rm $DEST_PATH
+ln -s $ICON_PATH $DEST_PATH
+
+
