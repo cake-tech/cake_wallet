328b378aac525f10ad7a045036ec2c508f112f1d
M <m@cakewallet.com>
2022-01-04 16:57:19 +0200

    Add config for ios app.

---
+++ scripts/ios/build_deps.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -z "$APP_IOS_TYPE" ]; then
+	echo "Please set APP_IOS_TYPE"
+	exit 1
+fi
+
+DIR=$(dirname "$0")
+
+case $APP_IOS_TYPE in
+	"monero.com") $DIR/build_monero_all.sh ;;
+	"cakewallet") $DIR/build_monero_all.sh ;;
+esac
