53a03294ca7d3045afa5eae7b90f54d2b42db306
M <m@cakewallet.com>
2022-01-04 20:22:24 +0200

    Icons for ios script

---
+++ scripts/ios/app_icon.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+ICON_120_PATH=""
+ICON_180_PATH=""
+DEST_DIR_PATH=`pwd`/../../ios/Runner/Assets.xcassets/AppIcon.appiconset
+
+case $APP_IOS_TYPE in
+	"monero.com")
+	ICON_120_PATH=`pwd`/../../assets/images/monero.com_icon_120.png
+	ICON_180_PATH=`pwd`/../../assets/images/monero.com_icon_180.png;;
+	"cakewallet")
+	ICON_120_PATH=`pwd`/../../assets/images/cakewallet_icon_120.png
+        ICON_180_PATH=`pwd`/../../assets/images/cakewallet_icon_180.png;;
+esac
+
+echo $ICON_120_PATH
+echo $ICON_180_PATH
+rm $DEST_DIR_PATH/app_icon_120.png
+rm $DEST_DIR_PATH/app_icon_180.png
+ln -s $ICON_120_PATH $DEST_DIR_PATH/app_icon_120.png
+ln -s $ICON_180_PATH $DEST_DIR_PATH/app_icon_180.png
+
