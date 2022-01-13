95654dfe472b1ff2175c88bd3fd3ef4a65c1015d
RafiaChy <rafiachowdhury24@gmail.com>
2022-01-14 00:10:27 +0600

    Added translations for the sell alert

--- lib/entities/wallets_manager.dart
+++ lib/entities/wallets_manager.dart
@@ -1,18 +1,18 @@
-import 'package:cake_wallet/entities/wallet.dart';
-import 'package:cake_wallet/entities/wallet_description.dart';
+// import 'package:cake_wallet/entities/wallet.dart';
+// import 'package:cake_wallet/entities/wallet_description.dart';
 
-abstract class WalletsManager {
-  Future<Wallet> create(String name, String password, String language);
+// abstract class WalletsManager {
+//   Future<Wallet> create(String name, String password, String language);
 
-  Future<Wallet> restoreFromSeed(
-      String name, String password, String seed, int restoreHeight);
+//   Future<Wallet> restoreFromSeed(
+//       String name, String password, String seed, int restoreHeight);
 
-  Future<Wallet> restoreFromKeys(String name, String password, String language,
-      int restoreHeight, String address, String viewKey, String spendKey);
+//   Future<Wallet> restoreFromKeys(String name, String password, String language,
+//       int restoreHeight, String address, String viewKey, String spendKey);
 
-  Future<Wallet> openWallet(String name, String password);
+//   Future<Wallet> openWallet(String name, String password);
 
-  Future<bool> isWalletExit(String name);
+//   Future<bool> isWalletExit(String name);
 
-  Future remove(WalletDescription wallet);
-}
+//   Future remove(WalletDescription wallet);
+// }Remote2434
