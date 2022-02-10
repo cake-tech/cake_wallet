88f2417d56d855ed075e7074963601df4944bcbf
Godwin Asuquo <godilite@gmail.com>
2022-02-10 12:13:04 +0100

    remove yat sending page

--- lib/src/screens/send/send_page.dart
+++ lib/src/screens/send/send_page.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 import 'package:cake_wallet/src/screens/send/widgets/send_card.dart';
-import 'package:cake_wallet/src/screens/yat/widgets/yat_close_button.dart';
 import 'package:cake_wallet/src/widgets/alert_with_two_actions.dart';
 import 'package:cake_wallet/src/widgets/template_tile.dart';
 import 'package:cake_wallet/view_model/send/output.dart';
@@ -22,8 +21,6 @@ import 'package:dotted_border/dotted_border.dart';
 import 'package:cake_wallet/src/widgets/alert_with_one_action.dart';
 import 'package:cake_wallet/src/screens/send/widgets/confirm_sending_alert.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-import 'package:cake_wallet/store/yat/yat_store.dart';
-import 'package:cake_wallet/src/screens/yat/yat_sending.dart';
 
 class SendPage extends BasePage {
   SendPage({@required this.sendViewModel}) : _formKey = GlobalKey<FormState>();
@@ -292,11 +289,6 @@ class SendPage extends BasePage {
 
                       await sendViewModel.createTransaction();
 
-                      if (!sendViewModel.isBatchSending &&
-                          sendViewModel.hasYat) {
-                        Navigator.of(context)
-                            .push<void>(YatSending.createRoute(sendViewModel));
-                      }
                     },
                     text: S.of(context).send,
                     color: Theme.of(context).accentTextTheme.body2.color,
@@ -333,7 +325,7 @@ class SendPage extends BasePage {
       }
 
       if (state is ExecutedSuccessfullyState &&
-          !(!sendViewModel.isBatchSending && sendViewModel.hasYat)) {
+          !sendViewModel.isBatchSending) {
         WidgetsBinding.instance.addPostFrameCallback((_) {
           showPopUp<void>(
               context: context,
