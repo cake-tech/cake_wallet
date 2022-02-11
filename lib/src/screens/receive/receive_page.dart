872098d6b8d9f800d25cd2d47a8b2ad1b2f20935
Serhii <borodenko.sv@gmail.com>
2022-02-11 15:49:46 +0200

    Cw 43 subaddress renaming visual bug (#271)
    * changed background
    
    * fixed format
    
    * fix issues from review

--- lib/src/screens/receive/receive_page.dart
+++ lib/src/screens/receive/receive_page.dart
@@ -132,7 +132,13 @@ class ReceivePage extends BasePage {
                     isLight: currentTheme.type == ThemeType.light),
               ),
               Observer(
-                  builder: (_) => ListView.separated(
+                  builder: (_) => ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30)),
+                    child: Container(
+                      color: Theme.of(context).textTheme.display2.decorationColor,
+                      child: ListView.separated(
                       padding: EdgeInsets.all(0),
                       separatorBuilder: (context, _) => Container(
                           height: 1, color: Theme.of(context).dividerColor),
@@ -207,7 +213,7 @@ class ReceivePage extends BasePage {
                               topRight: Radius.circular(30)),
                           child: cell,
                         );
-                      })),
+                      })))),
             ],
           ),
         ));
