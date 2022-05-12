7036247802b919e5b87dba51b94b54d40b3506c2
diego vila <ruahman@gmail.com>
2022-05-12 14:52:02 -0400

    collect input

--- cw_ldk/example/lib/pages/create_channel.dart
+++ cw_ldk/example/lib/pages/create_channel.dart
@@ -6,6 +6,8 @@ class CreateChannelPage extends StatefulWidget {
 }
 
 class _CreateChannelState extends State<CreateChannelPage> {
+  TextEditingController amount;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,32 +20,47 @@ class _CreateChannelState extends State<CreateChannelPage> {
             Text("Please specify amount in satoshis"),
             TextField(
               decoration: InputDecoration(labelText: "amount"),
+              controller: amount,
             ),
             ElevatedButton(
-                onPressed: () {
-                  // Navigator.pop(context);
-                  showDialog<void>(
-                    context: context,
-                    builder: (_) => AlertDialog(
-                        title: Text("Channel Created"),
-                        content: Text(
-                            "You successfuly created a channel with cake."),
-                        actions: <Widget>[
-                          TextButton(
-                            child: const Text('Ok'),
-                            onPressed: () {
-                              final nav = Navigator.of(context);
-                              nav.pop();
-                              nav.pop();
-                            },
-                          )
-                        ]),
-                  );
-                },
-                child: Text("Create Channel"))
+              child: Text("Create Channel"),
+              onPressed: () {
+                // Navigator.pop(context);
+                print(amount.text);
+                showDialog<void>(
+                  context: context,
+                  builder: (_) => AlertDialog(
+                      title: Text("Channel Created"),
+                      content:
+                          Text("You successfuly created a channel with cake."),
+                      actions: <Widget>[
+                        TextButton(
+                          child: const Text('Ok'),
+                          onPressed: () {
+                            final nav = Navigator.of(context);
+                            nav.pop();
+                            nav.pop();
+                          },
+                        )
+                      ]),
+                );
+              },
+            )
           ],
         ),
       )),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    amount = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    amount.dispose();
   }
 }
