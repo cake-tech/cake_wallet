76fe14c9a567fdf0accf3d7fcc701ae04608e9a3
M <m@cakewallet.com>
2022-06-22 12:39:48 +0100

    Add hex and txKey for monero pending transaction.

--- cw_bitcoin/lib/pending_bitcoin_transaction.dart
+++ cw_bitcoin/lib/pending_bitcoin_transaction.dart
@@ -25,6 +25,9 @@ class PendingBitcoinTransaction with PendingTransaction {
   String get id => _tx.getId();
 
   @override
+  String get hex => '';
+
+  @override
   String get amountFormatted => bitcoinAmountToString(amount: amount);
 
   @override
