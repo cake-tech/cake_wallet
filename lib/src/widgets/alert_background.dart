7611d9c2af4d980b2aae653c353a0b89c9281d38
OmarHatem28 <omarh.ismail1@gmail.com>
2022-06-27 12:22:12 +0200

    Fix UI overflow when keyboard pops up

--- lib/src/widgets/alert_background.dart
+++ lib/src/widgets/alert_background.dart
@@ -11,6 +11,7 @@ class AlertBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.transparent,
       body: Container(
         height: double.infinity,
