ad5087e48c9bf705d853ccf561bca61952f3f2a1
Godwin Asuquo <godilite@gmail.com>
2022-06-30 22:02:03 +0300

    add ionia filter widget

---
+++ lib/src/screens/ionia/widgets/ionia_filter_modal.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IoniaFilterModal extends StatelessWidget {
+  const IoniaFilterModal();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
