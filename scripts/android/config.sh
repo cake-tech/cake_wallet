a5e38d6b24583f73d968183e026559ffe9b04466
marco <marco@cypherstack.com>
2022-07-01 21:26:03 -0600

    allow building with threads to be lower

--- scripts/android/config.sh
+++ scripts/android/config.sh
@@ -8,4 +8,8 @@ export ANDROID_NDK_HOME=$ANDROID_NDK_ROOT
 export TOOLCHAIN_DIR="${WORKDIR}/toolchain"
 export TOOLCHAIN_BASE_DIR=$TOOLCHAIN_DIR
 export ORIGINAL_PATH=$PATH
-export THREADS="$(nproc)"
+if [ -z "$OVERRIDE_THREADS" ]; then
+  export THREADS="$(nproc)"
+else
+  export THREADS=$OVERRIDE_THREADS
+fi
