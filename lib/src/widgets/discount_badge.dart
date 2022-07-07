11cc52a8f088fef78691553823930e2fcfcf94d3
Godwin Asuquo <godilite@gmail.com>
2022-07-07 15:47:36 +0300

    Implement show purchased cards

--- lib/src/widgets/discount_badge.dart
+++ lib/src/widgets/discount_badge.dart
@@ -1,33 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:cake_wallet/generated/i18n.dart';
 
-
 class DiscountBadge extends StatelessWidget {
   const DiscountBadge({
     Key key,
     @required this.percentage,
+    this.discountBackground,
   }) : super(key: key);
 
   final double percentage;
+  final AssetImage discountBackground;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-      child: Text( S.of(context).discount(percentage.toString()),
+      child: Text(
+        S.of(context).discount(percentage.toString()),
         style: TextStyle(
           color: Colors.white,
           fontSize: 12,
           fontWeight: FontWeight.w500,
           fontFamily: 'Lato',
-          ),
         ),
+      ),
       decoration: BoxDecoration(
         image: DecorationImage(
           fit: BoxFit.fill,
-          image:  AssetImage('assets/images/badge_discount.png'),
-          ),
-        ), 
-      );
-    }
+          image: discountBackground ?? AssetImage('assets/images/badge_discount.png'),
+        ),
+      ),
+    );
+  }
 }
