e251b446a970ee4ce8ebc09e9cc79dc2570f5060
Marco Salazar <marco@cypherstack.com>
2022-07-15 09:22:29 -0600

    build ios

--- scripts/ios/build_openssl.sh
+++ scripts/ios/build_openssl.sh
@@ -10,7 +10,9 @@ echo "============================ OpenSSL ============================"
 echo "Cloning Open SSL from - $OPEN_SSL_URL"
 git clone $OPEN_SSL_URL $OPEN_SSL_DIR_PATH
 cd $OPEN_SSL_DIR_PATH
-./build-libssl.sh --version=1.1.1k --archs="x86_64 arm64 armv7s armv7" --targets="ios64-cross-arm64" --deprecated
+sed -i '' "s/IOS_MIN_SDK_VERSION=\"12.0\"/IOS_MIN_SDK_VERSION=\"10.0\"/g" build-libssl.sh
+
+./build-libssl.sh --version=1.1.1k --archs="x86_64 arm64 armv7s armv7" --targets="ios64-cross-arm64 ios-cross-armv7 ios-cross-armv7s" --deprecated
 
 mv ${OPEN_SSL_DIR_PATH}/include/* $EXTERNAL_IOS_INCLUDE_DIR
 mv ${OPEN_SSL_DIR_PATH}/lib/* $EXTERNAL_IOS_LIB_DIR
