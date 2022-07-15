65e52e05dcca8e4faaf7ed6c0f68370ad2c1ac7c
Serhii-Borodenko <borodenko.sv@gmail.com>
2022-07-15 16:02:07 +0300

    remove fiat from transactions page

--- lib/src/screens/dashboard/widgets/transactions_page.dart
+++ lib/src/screens/dashboard/widgets/transactions_page.dart
@@ -57,7 +57,8 @@ class TransactionsPage extends StatelessWidget {
                               formattedDate: DateFormat('HH:mm')
                                   .format(transaction.date),
                               formattedAmount: item.formattedCryptoAmount,
-                              formattedFiatAmount: item.formattedFiatAmount,
+                              formattedFiatAmount: dashboardViewModel.balanceViewModel.disableFiat
+                                  ? '' : item.formattedFiatAmount,
                               isPending: transaction.isPending));
                         }
 
