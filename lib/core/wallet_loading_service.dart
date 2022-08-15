4a64f5d6a4d09f1152eefaa123775ed455ff2189
OmarHatem28 <omarh.ismail1@gmail.com>
2022-08-15 21:04:21 +0200

    Add monero sync task in the load function to be registered/cancelled when user changes wallets

--- lib/core/wallet_loading_service.dart
+++ lib/core/wallet_loading_service.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cake_wallet/core/generate_wallet_password.dart';
 import 'package:cake_wallet/core/key_service.dart';
 import 'package:cake_wallet/entities/preferences_key.dart';
@@ -5,13 +7,44 @@ import 'package:cw_core/wallet_base.dart';
 import 'package:cw_core/wallet_service.dart';
 import 'package:cw_core/wallet_type.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:workmanager/workmanager.dart';
+import 'package:cake_wallet/main.dart';
+import 'package:flutter/foundation.dart';
+import 'package:cake_wallet/di.dart';
+
+const moneroSyncTaskKey = "com.cake_wallet.monero_sync_task";
+
+void callbackDispatcher() {
+	Workmanager().executeTask((task, inputData) async {
+		try {
+			switch (task) {
+				case moneroSyncTaskKey:
+					/// The work manager runs on a separate isolate from the main flutter isolate.
+					/// thus we initialize app configs first; hive, getIt, etc...
+					await initializeAppConfigs();
+
+					final name = (inputData['name'] as String) ?? '';
+					final walletLoadingService = getIt.get<WalletLoadingService>();
+
+					await walletLoadingService.load(WalletType.monero, name);
+					break;
+			}
+
+			return Future.value(true);
+		} catch (error, stackTrace) {
+			print(error);
+			print(stackTrace);
+			return Future.error(error);
+		}
+	});
+}
 
 class WalletLoadingService {
 	WalletLoadingService(
 		this.sharedPreferences,
 		this.keyService,
 		this.walletServiceFactory);
-	
+
 	final SharedPreferences sharedPreferences;
 	final KeyService keyService;
 	final WalletService Function(WalletType type) walletServiceFactory;
@@ -22,13 +55,18 @@ class WalletLoadingService {
 		}
 		final walletService = walletServiceFactory?.call(type);
 		final password = await keyService.getWalletPassword(walletName: name);
-  		final wallet = await walletService.openWallet(name, password);
+		final wallet = await walletService.openWallet(name, password);
 
-  		if (type == WalletType.monero) {
-  			await upateMoneroWalletPassword(wallet);
-  		}
+		if (type == WalletType.monero) {
+			await upateMoneroWalletPassword(wallet);
+			if (Platform.isAndroid) {
+				registerSyncTask(name);
+			}
+		} else { /// if wallet is not monero, cancel scheduled tasks
+			cancelSyncTask();
+		}
 
-  		return wallet;
+		return wallet;
 	}
 
 	Future<void> upateMoneroWalletPassword(WalletBase wallet) async {
@@ -48,5 +86,36 @@ class WalletLoadingService {
 		await keyService.saveWalletPassword(walletName: wallet.name, password: password);
 		isPasswordUpdated = true;
 		await sharedPreferences.setBool(key, isPasswordUpdated);
+	}
+
+	void registerSyncTask(String name) async {
+		try {
+			await Workmanager().initialize(
+				callbackDispatcher,
+				isInDebugMode: kDebugMode,
+			);
+
+			await Workmanager().registerPeriodicTask(
+				moneroSyncTaskKey,
+				moneroSyncTaskKey,
+				// TODO: change duration to the desired intervals to run this task
+				initialDelay: const Duration(hours: 1),
+				frequency: Duration(hours: 1),
+				inputData: <String, dynamic> {"name": name},
+				existingWorkPolicy: ExistingWorkPolicy.replace,
+			);
+		} catch (error, stackTrace) {
+			print(error);
+			print(stackTrace);
+		}
+	}
+
+	void cancelSyncTask() {
+		try {
+			Workmanager().cancelByUniqueName(moneroSyncTaskKey);
+		} catch (error, stackTrace) {
+			print(error);
+			print(stackTrace);
+		}
 	}
 }
--- lib/entities/load_current_wallet.dart
+++ lib/entities/load_current_wallet.dart
@@ -1,16 +1,9 @@
-import 'dart:io';
-
 import 'package:cake_wallet/di.dart';
-import 'package:cake_wallet/main.dart';
-import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cake_wallet/store/app_store.dart';
 import 'package:cake_wallet/entities/preferences_key.dart';
 import 'package:cw_core/wallet_type.dart';
 import 'package:cake_wallet/core/wallet_loading_service.dart';
-import 'package:workmanager/workmanager.dart';
-
-const moneroSyncTaskKey = "com.cake_wallet.monero_sync_task";
 
 Future<void> loadCurrentWallet() async {
   final appStore = getIt.get<AppStore>();
@@ -22,62 +15,6 @@ Future<void> loadCurrentWallet() async {
           0;
   final type = deserializeFromInt(typeRaw);
   final walletLoadingService = getIt.get<WalletLoadingService>();
-  if (type == WalletType.monero && Platform.isAndroid) {
-    registerSyncTask();
-  } else {
-    cancelSyncTask();
-  }
   final wallet = await walletLoadingService.load(type, name);
   appStore.changeCurrentWallet(wallet);
-}
-
-void registerSyncTask() async {
-  try {
-    await Workmanager().initialize(
-      callbackDispatcher,
-      isInDebugMode: kDebugMode,
-    );
-
-    await Workmanager().registerPeriodicTask(
-      moneroSyncTaskKey,
-      moneroSyncTaskKey,
-      // TODO: change duration to the desired intervals to run this task
-      initialDelay: const Duration(hours: 1),
-      frequency: Duration(hours: 1),
-    );
-  } catch (error, stackTrace) {
-    print(error);
-    print(stackTrace);
-  }
-}
-
-void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) async {
-    try {
-      switch (task) {
-        case moneroSyncTaskKey:
-
-          /// The work manager runs on a separate isolate from the main flutter isolate.
-          /// thus we initialize app configs first; hive, getIt, etc...
-          await initializeAppConfigs();
-          await loadCurrentWallet();
-          break;
-      }
-
-      return Future.value(true);
-    } catch (error, stackTrace) {
-      print(error);
-      print(stackTrace);
-      return Future.error(error);
-    }
-  });
-}
-
-void cancelSyncTask() {
-  try {
-    Workmanager().cancelByUniqueName(moneroSyncTaskKey);
-  } catch (error, stackTrace) {
-    print(error);
-    print(stackTrace);
-  }
 }
