771175e87c629ca1d8db7a4417191bd15012d52f
Marco <marco@cypherstack.com>
2022-08-26 20:24:53 +0800

    include dates in history

--- scripts/linux/build_sharedfile.sh
+++ scripts/linux/build_sharedfile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 . ./config.sh
-printf $(git log -1 --pretty=format:"%h") >> build/git_commit_version.txt
+printf $(git log -1 --pretty=format:"%h %ad") >> build/git_commit_version.txt
 cd build
 cmake ../cmakefiles/${TYPES_OF_BUILD}
 make -j$(nproc)
