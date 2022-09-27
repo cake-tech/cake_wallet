a20450d289424283cd2670709ae9032a0f17204a
OmarHatem <omarh.ismail1@gmail.com>
2022-09-27 13:15:57 +0200

    replace testing configuration with the configurations agreed on

--- lib/entities/background_tasks.dart
+++ lib/entities/background_tasks.dart
@@ -113,18 +113,16 @@ class BackgroundTasks {
       await Workmanager().registerPeriodicTask(
         moneroSyncTaskKey,
         moneroSyncTaskKey,
-        // initialDelay: syncMode.frequency,
-        // frequency: syncMode.frequency,
-        initialDelay: Duration(minutes: 1),
-        frequency: Duration(minutes: 15),
+        initialDelay: syncMode.frequency,
+        frequency: syncMode.frequency,
         existingWorkPolicy: changeExisting ? ExistingWorkPolicy.replace : ExistingWorkPolicy.keep,
         inputData: <String, dynamic>{"sync_all": syncAll},
-        // constraints: Constraints(
-        //   networkType: NetworkType.connected,
-        //   requiresBatteryNotLow: syncMode.type == SyncType.unobtrusive,
-        //   requiresCharging: syncMode.type == SyncType.unobtrusive,
-        //   requiresDeviceIdle: syncMode.type == SyncType.unobtrusive,
-        // ),
+        constraints: Constraints(
+          networkType: NetworkType.connected,
+          requiresBatteryNotLow: syncMode.type == SyncType.unobtrusive,
+          requiresCharging: syncMode.type == SyncType.unobtrusive,
+          requiresDeviceIdle: syncMode.type == SyncType.unobtrusive,
+        ),
       );
     } catch (error, stackTrace) {
       print(error);
