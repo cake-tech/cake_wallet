5131d41028215fa6251b86c93cd5adc05bbb6441
Godwin Asuquo <godilite@gmail.com>
2022-10-04 23:11:39 +0300

    fix extra id

--- lib/exchange/simpleswap/simpleswap_exchange_provider.dart
+++ lib/exchange/simpleswap/simpleswap_exchange_provider.dart
@@ -106,7 +106,7 @@ class SimpleSwapExchangeProvider extends ExchangeProvider {
     final id = responseJSON['id'] as String;
     final inputAddress = responseJSON['address_from'] as String;
     final settleAddress = responseJSON['user_refund_address'] as String;
-    final extraIdTo = responseJSON['extraIdTo'] as String;
+    final extraIdTo = responseJSON['extra_id_from'] as String;
     return Trade(
       id: id,
       provider: description,
