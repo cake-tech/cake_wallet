c1c19af5d9cc8f0baeb98e49d0f87adfb846c5bc
sneurlax <sneurlax@gmail.com>
2022-10-13 22:01:32 -0500

    select native library correctly for testing in linux by @msalazarm
    🐐

--- cw_monero/lib/api/monero_api.dart
+++ cw_monero/lib/api/monero_api.dart
@@ -1,6 +1,26 @@
 import 'dart:ffi';
 import 'dart:io';
 
-final DynamicLibrary moneroApi = Platform.isAndroid || Platform.isLinux
-    ? DynamicLibrary.open("libcw_monero.so")
-    : DynamicLibrary.open("cw_monero.framework/cw_monero");
+DynamicLibrary get moneroApi {
+  if (Platform.environment.containsKey('FLUTTER_TEST')) {
+    // DynamicLibrary.open(
+    //     'scripts/linux/build/jsoncpp/build/src/lib_json/libjsoncpp.so');
+    // DynamicLibrary.open(
+    //     'scripts/linux/build/jsoncpp/build/src/lib_json/libjsoncpp.so.1');
+    // DynamicLibrary.open(
+    //     'scripts/linux/build/jsoncpp/build/src/lib_json/libjsoncpp.so.1.7.4');
+    //
+    // DynamicLibrary.open(
+    //     'scripts/linux/build/libsecret/_build/libsecret/libsecret-1.so');
+    // DynamicLibrary.open(
+    //     'scripts/linux/build/libsecret/_build/libsecret/libsecret-1.so.0');
+    // DynamicLibrary.open(
+    //     'scripts/linux/build/libsecret/_build/libsecret/libsecret-1.so.0.0.0');
+
+    return DynamicLibrary.open(
+        'crypto_plugins/flutter_libmonero/scripts/linux/build/libcw_monero.so');
+  }
+  return Platform.isAndroid || Platform.isLinux
+      ? DynamicLibrary.open("libcw_monero.so")
+      : DynamicLibrary.open("cw_monero.framework/cw_monero");
+}
