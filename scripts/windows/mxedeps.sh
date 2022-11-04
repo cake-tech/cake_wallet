c910cb401153bb335d0a36450e9bc38e7b34a205
sneurlax <sneurlax@gmail.com>
2022-11-04 09:48:14 -0500

    add MXE and Monero dep references

---
+++ scripts/windows/mxedeps.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# MXE dependencies
+sudo apt-get install -y p7zip-full autoconf automake autopoint bash bison bzip2 cmake flex gettext git g++ gperf intltool libffi-dev libtool libtool-bin libltdl-dev libssl-dev libxml-parser-perl make openssl patch perl pkg-config python ruby scons sed unzip wget xz-utils g++-multilib libc6-dev-i386 lzip
+
+# Install MXE
+mkdir -p ~/development
+cd ~/development
+git clone https://github.com/mxe/mxe.git
+cd mxe
+make cc cmake MXE_TARGETS='x86_64-w64-mingw32.static'
+if ! [[ $PATH == *"/mxe"* ]]; then
+  echo 'export PATH="$HOME/development/mxe/usr/bin:$PATH"' >> ~/.bashrc  # Prepend to PATH
+  source ~/.bashrc
+fi
+make cmake openssl MXE_TARGETS='x86_64-w64-mingw32.static'
+
+sudo apt-get install -y gcc-mingw-w64-x86-64
