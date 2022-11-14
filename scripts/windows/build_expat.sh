0eab12623f42fa91b48c997234ec54202a4e5f19
sneurlax <sneurlax@gmail.com>
2022-11-14 17:20:06 -0600

    expat linting

--- scripts/windows/build_expat.sh
+++ scripts/windows/build_expat.sh
@@ -12,32 +12,24 @@ EXPAT_VERSION=R_2_4_1
 EXPAT_HASH="a28238bdeebc087071777001245df1876a11f5ee"
 EXPAT_SRC_DIR=$WORKDIR/libexpat
 
-for arch in $TYPES_OF_BUILD
-do
-	#case $arch in
-	#	*)
-	#		HOST="${arch}-windows-gnu";; # TODO see if this is needed for Windows
-	#esac
+cd $WORKDIR
+rm -rf $EXPAT_SRC_DIR
+git clone https://github.com/libexpat/libexpat.git -b ${EXPAT_VERSION} ${EXPAT_SRC_DIR}
+cd $EXPAT_SRC_DIR
+test `git rev-parse HEAD` = ${EXPAT_HASH} || exit 1
+cd $EXPAT_SRC_DIR/expat
 
-	PREFIX=$WORKDIR/prefix_${arch}
-
-	cd $WORKDIR
-	rm -rf $EXPAT_SRC_DIR
-	git clone https://github.com/libexpat/libexpat.git -b ${EXPAT_VERSION} ${EXPAT_SRC_DIR}
-	cd $EXPAT_SRC_DIR
-	test `git rev-parse HEAD` = ${EXPAT_HASH} || exit 1
-	cd $EXPAT_SRC_DIR/expat
-
-	./buildconf.sh
-	#CC=clang CXX=clang++
-	CC=x86_64-w64-mingw32-gcc
-	CXX=x86_64-w64-mingw32-g++
-	#if [ ! -z "${MSYSTEM}" ]; then
-		HOST=x86_64-w64-mingw32
-	#else
-		CROSS_COMPILE="x86_64-w64-mingw32.static-"
-	#fi
-	./configure CFLAGS=-fPIC CXXFLAGS=-fPIC --enable-static --disable-shared --prefix=${PREFIX} --host=${HOST}
-	make -j$THREADS
-	make -j$THREADS install
-done
+./buildconf.sh
+CC=x86_64-w64-mingw32-gcc
+CXX=x86_64-w64-mingw32-g++
+HOST=x86_64-w64-mingw32
+CROSS_COMPILE="x86_64-w64-mingw32.static-"
+./configure \
+	CFLAGS=-fPIC \
+	CXXFLAGS=-fPIC \
+	--enable-static \
+	--disable-shared \
+	--prefix=${PREFIX} \
+	--host=${HOST}
+make -j$THREADS
+make -j$THREADS install
