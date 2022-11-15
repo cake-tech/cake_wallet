6057b6f08bf89d6e4c3ce5e548e621ea605f4d8a
sneurlax <sneurlax@gmail.com>
2022-11-14 21:02:51 -0600

    update monero build tag to win-x64

--- scripts/windows/build_monero.sh
+++ scripts/windows/build_monero.sh
@@ -28,12 +28,12 @@ do
 	case $arch in
 		"x86_64"	)
 			BUILD_64=ON
-			TAG="windows-x86_64"
+			TAG="win-x64"
 			ARCH="x86-64"
 			ARCH_ABI="x86_64";;
 		"aarch64"	)
 			BUILD_64=ON
-			TAG="windows-aarch64"
+			TAG="win-x64"
 			ARCH="aarch64"
 			ARCH_ABI="aarch64";;
 	esac
