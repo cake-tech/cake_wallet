27d46960091e40c73f6270e108e68cd3ef55c4a7
sneurlax <sneurlax@gmail.com>
2022-11-15 13:48:31 -0600

    downgrade zmq and use position independent code

--- scripts/windows/build_zmq.sh
+++ scripts/windows/build_zmq.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
 . ./config.sh
+
 ZMQ_SRC_DIR=$WORKDIR/libzmq
+: '
+ZMQ_BRANCH=v4.3.4
+ZMQ_COMMIT_HASH=4097855ddaaa65ed7b5e8cb86d143842a594eebd
+
 ZMQ_BRANCH=v4.3.3
 ZMQ_COMMIT_HASH=04f5bbedee58c538934374dc45182d8fc5926fa3
+
+ZMQ_BRANCH=v4.3.1
+ZMQ_COMMIT_HASH=2cb1240db64ce1ea299e00474c646a2453a8435b
+'
+ZMQ_BRANCH=v4.3.1
+ZMQ_COMMIT_HASH=2cb1240db64ce1ea299e00474c646a2453a8435b
 
 cd $WORKDIR
 rm -rf $ZMQ_SRC_DIR
@@ -11,21 +22,21 @@ git clone https://github.com/zeromq/libzmq.git ${ZMQ_SRC_DIR} -b ${ZMQ_BRANCH}
 cd $ZMQ_SRC_DIR
 git checkout ${ZMQ_COMMIT_HASH}
 ./autogen.sh
-#CC=x86_64-w64-mingw32-gcc
-#CXX=x86_64-w64-mingw32-g++
-#HOST=x86_64-w64-mingw32
-HOST="x86_64-windows-gnu"  # TODO see why we cannot use the same HOST as with the other libraries
-#CROSS_COMPILE="x86_64-w64-mingw32.static-"
-#CXXFLAGS=-std=c++11
+CC=x86_64-w64-mingw32-gcc
+CXX=x86_64-w64-mingw32-g++
+HOST=x86_64-w64-mingw32
+CROSS_COMPILE="x86_64-w64-mingw32.static-"
 ./configure \
 	--without-documentation \
+	--without-docs \
 	--disable-shared \
 	--without-libsodium \
 	--disable-curve \
 	--prefix=${PREFIX} \
 	--host=${HOST} \
-	--enable-static #\
-	#CFLAGS="-Wall -Wno-pedantic-ms-format -DLIBCZMQ_EXPORTS -DZMQ_DEFINED_STDINT -lzmq -lws2_32 -liphlpapi -lrpcrt4"
+	--enable-static \
+	--with-pic #\
+	#CFLAGS="-Wall -Wno-pedantic-ms-format -DLIBCZMQ_EXPORTS -DZMQ_DEFINED_STDINT"
 make -j$THREADS
 make install
 
