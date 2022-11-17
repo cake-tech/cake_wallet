18dcb36c4cbc88ebd71f3aa9c34bd8304493139a
sneurlax <sneurlax@gmail.com>
2022-11-17 15:54:53 -0600

    use available variables and some script linting

--- scripts/windows/build_sharedfile.sh
+++ scripts/windows/build_sharedfile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 . ./config.sh
+
 echo ''$(git log -1 --pretty=format:"%H")' '$(date) >> build/git_commit_version.txt
 VERSIONS_FILE=../../lib/git_versions.dart
 EXAMPLE_VERSIONS_FILE=../../lib/git_versions_example.dart
@@ -10,11 +11,12 @@ fi
 COMMIT=$(git log -1 --pretty=format:"%H")
 OS="LINUX" # TODO detect environment variable
 sed -i "/\/\*${OS}_VERSION/c\\/\*${OS}_VERSION\*\/ const ${OS}_VERSION = \"$COMMIT\";" $VERSIONS_FILE
-cd build
-mkdir monero_build
-mkdir wownero_build
-MONERO_BUILD=$(pwd)/monero_build
-WOWNERO_BUILD=$(pwd)/wownero_build
+
+cd ${WORKDIR}
+mkdir -p monero_build
+mkdir -p wownero_build
+MONERO_BUILD=${WORKDIR}/monero_build
+WOWNERO_BUILD=${WORKDIR}/wownero_build
 
 cd $MONERO_BUILD
 x86_64-w64-mingw32.static-cmake ../../cmakefiles/monero/x86_64
