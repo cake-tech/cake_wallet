deccf1ace38b9af7c7fd2df8162a891699d4efa7
sneurlax <sneurlax@gmail.com>
2022-11-18 13:05:24 -0600

    do not use nice() process prioritization via unistd
    /home/user/src/flutter_libmonero/cw_wownero/ios/Classes/wownero_api.cpp:353:9: error: 'nice' was not declared in this scope despite importing unistd.h apparently because [mingw unistd isn't fully POSIX-compliant](https://stackoverflow.com/questions/9612315/why-does-my-compiler-not-accept-fork-despite-my-inclusion-of-unistd-h) ie doesn't implement `nice`

--- cw_wownero/ios/Classes/wownero_api.cpp
+++ cw_wownero/ios/Classes/wownero_api.cpp
@@ -350,7 +350,7 @@ extern "C"
 
     bool load_wallet(char *path, char *password, int32_t nettype)
     {
-        nice(19);
+        // nice(19);
         Monero::NetworkType networkType = static_cast<Monero::NetworkType>(nettype);
         Monero::WalletManager *walletManager = Monero::WalletManagerFactory::getWalletManager();
         Monero::Wallet *wallet = walletManager->openWallet(std::string(path), std::string(password), networkType);
@@ -442,7 +442,7 @@ extern "C"
 
     bool connect_to_node(char *error)
     {
-        nice(19);
+        // nice(19);
         bool is_connected = get_current_wallet()->connectToDaemon();
 
         if (!is_connected)
@@ -455,7 +455,7 @@ extern "C"
 
     bool setup_node(char *address, char *login, char *password, bool use_ssl, bool is_light_wallet, char *error)
     {
-        nice(19);
+        // nice(19);
         Monero::Wallet *wallet = get_current_wallet();
 
         std::string _login = "";
@@ -525,7 +525,7 @@ extern "C"
     bool transaction_create(char *address, char *payment_id, char *amount,
                                               uint8_t priority_raw, uint32_t subaddr_account, Utf8Box &error, PendingTransactionRaw &pendingTransaction)
     {
-        nice(19);
+        // nice(19);
 
         auto priority = static_cast<Monero::PendingTransaction::Priority>(priority_raw);
         std::string _payment_id;
@@ -565,7 +565,7 @@ extern "C"
     bool transaction_create_mult_dest(char **addresses, char *payment_id, char **amounts, uint32_t size,
                                                   uint8_t priority_raw, uint32_t subaddr_account, Utf8Box &error, PendingTransactionRaw &pendingTransaction)
     {
-        nice(19);
+        // nice(19);
 
         std::vector<std::string> _addresses;
         std::vector<uint64_t> _amounts;
