91e4a958b778eccaffee52ad683d5ee6fcd65ce6
sneurlax <sneurlax@gmail.com>
2022-12-22 15:11:50 -0600

    load dll on windows
    and handle flutter tests

--- cw_monero/lib/api/monero_api.dart
+++ cw_monero/lib/api/monero_api.dart
@@ -1,6 +1,11 @@
 import 'dart:ffi';
 import 'dart:io';
 
-final DynamicLibrary moneroApi = Platform.isAndroid || Platform.isLinux
-    ? DynamicLibrary.open("libcw_monero.so")
-    : DynamicLibrary.open("cw_monero.framework/cw_monero");
+final DynamicLibrary moneroApi = Platform.isWindows
+    ? DynamicLibrary.open("libcw_monero.dll")
+    : Platform.environment.containsKey('FLUTTER_TEST')
+        ? DynamicLibrary.open(
+        'crypto_plugins/flutter_libmonero/scripts/linux/build/libcw_monero.so');
+        : Platform.isAndroid || Platform.isLinux
+            ? DynamicLibrary.open("libcw_monero.so")
+            : DynamicLibrary.open("cw_monero.framework/cw_monero");
--- cw_wownero/lib/api/wownero_api.dart
+++ cw_wownero/lib/api/wownero_api.dart
@@ -1,6 +1,11 @@
 import 'dart:ffi';
 import 'dart:io';
 
-final DynamicLibrary wowneroApi = Platform.isAndroid || Platform.isLinux
-    ? DynamicLibrary.open("libcw_wownero.so")
-    : DynamicLibrary.open("cw_wownero.framework/cw_wownero");
+final DynamicLibrary wowneroApi = Platform.isWindows
+    ? DynamicLibrary.open("libcw_wownero.dll")
+    : io.Platform.environment.containsKey('FLUTTER_TEST')
+        ? DynamicLibrary.open(
+        'crypto_plugins/flutter_libmonero/scripts/linux/build/libcw_wownero.so');
+        : Platform.isAndroid || Platform.isLinux
+            ? DynamicLibrary.open("libcw_wownero.so")
+            : DynamicLibrary.open("cw_wownero.framework/cw_wownero");
