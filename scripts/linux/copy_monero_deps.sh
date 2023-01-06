37bda039f8cce636c4b6f4ad0ce52af9e25edfff
sneurlax <sneurlax@gmail.com>
2023-01-05 19:07:47 -0600

    make copy_monero_deps.sh independently executable
    I don't know why TYPES_OF_BUILD isn't reset and I don't have time for build_monero_all.sh right now, I just need to rebuild my sharedfiles!

--- scripts/linux/copy_monero_deps.sh
+++ scripts/linux/copy_monero_deps.sh
@@ -5,6 +5,7 @@ CW_DIR="$(pwd)"/../../../flutter_libmonero
 CW_EXRTERNAL_DIR=${CW_DIR}/cw_shared_external/ios/External/android
 CW_MONERO_EXTERNAL_DIR=${CW_DIR}/cw_monero/ios/External/android
 CW_WOWNERO_EXTERNAL_DIR=${CW_DIR}/cw_wownero/ios/External/android
+if [ -z ${TYPES_OF_BUILD+x} ]; then TYPES_OF_BUILD=x86_64; fi
 
 for arch in $TYPES_OF_BUILD
 do
