1c139386a71e4d02436eeae4db04f08f3231c735
HardenedSteel <hardenedsteel@protonmail.com>
2023-01-11 02:19:16 +0300

    Add Turkish Language

--- lib/entities/language_service.dart
+++ lib/entities/language_service.dart
@@ -20,7 +20,8 @@ class LanguageService {
     'hr': 'Hrvatski (Croatian)',
     'it': 'Italiano (Italian)',
     'th': 'ภาษาไทย (Thai)',
-    'ar': 'العربية (Arabic)'
+    'ar': 'العربية (Arabic)',
+    'tr': 'Türkçe (Turkish)'
   };
 
   static const Map<String, String> localeCountryCode = {
@@ -40,7 +41,8 @@ class LanguageService {
     'hr': 'hrv',
     'it': 'ita',
     'th': 'tha',
-    'ar': 'sau'
+    'ar': 'sau',
+    'tr': 'tur'
   };
 
   static final list = <String, String> {};
