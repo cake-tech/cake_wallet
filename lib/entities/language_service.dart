a9632b564592ab11c5a4c3bdb956292b9eb71fd1
nahuhh <50635951+nahuhh@users.noreply.github.com>
2023-01-26 11:53:57 +0000

    Tagalog
    add tagalog
    
    tl
    tgl
    phl

--- lib/entities/language_service.dart
+++ lib/entities/language_service.dart
@@ -21,8 +21,9 @@ class LanguageService {
     'it': 'Italiano (Italian)',
     'th': 'ภาษาไทย (Thai)',
     'ar': 'العربية (Arabic)',
+    'tl': 'ᜆᜄᜎᜓᜄ᜔ (Tagalog)',
     'tr': 'Türkçe (Turkish)',
-    'my': 'မြန်မာ (Burmese)',
+    'my': 'မြန်မာ (Burmese)'
   };
 
   static const Map<String, String> localeCountryCode = {
@@ -43,8 +44,9 @@ class LanguageService {
     'it': 'ita',
     'th': 'tha',
     'ar': 'sau',
+    'tl': 'phl',
     'tr': 'tur',
-    'my': 'mmr',
+    'my': 'mmr'
   };
 
   static final list = <String, String> {};
