4353e778bb7b949c5ae23e825dd558b30de2c682
OmarHatem <omarh.ismail1@gmail.com>
2023-01-28 15:47:44 +0200

    Increase QR version to support longer addresses

--- lib/src/screens/receive/widgets/qr_image.dart
+++ lib/src/screens/receive/widgets/qr_image.dart
@@ -8,7 +8,7 @@ class QrImage extends StatelessWidget {
     this.size = 100.0,
     this.backgroundColor,
     Color foregroundColor = Colors.black,
-    int version = 9, // Previous value: 7 something happened after flutter upgrade monero wallets addresses are longer than ver. 7 ???
+    int version = 13, // Previous value: 9 BTC & LTC wallets addresses are longer than ver. 9
     int errorCorrectionLevel = QrErrorCorrectLevel.L,
   }) : _painter = QrPainter(data, foregroundColor, version, errorCorrectionLevel);
 
