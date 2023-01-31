0e9d30c915b5295c600b5b5d468dd41a1fd179f6
OmarHatem <omarh.ismail1@gmail.com>
2023-01-31 17:31:42 +0200

    Fixate QR background and foreground colors in all themes

--- lib/src/screens/receive/widgets/qr_image.dart
+++ lib/src/screens/receive/widgets/qr_image.dart
@@ -5,18 +5,14 @@ class QrImage extends StatelessWidget {
   QrImage({
     required this.data,
     this.size = 100.0,
-    this.backgroundColor,
-    this.foregroundColor = Colors.black,
     this.version = 9, // Previous value: 7 something happened after flutter upgrade monero wallets addresses are longer than ver. 7 ???
     this.errorCorrectionLevel = qr.QrErrorCorrectLevel.L,
   });
 
-  final Color? backgroundColor;
   final double size;
   final String data;
   final int version;
   final int errorCorrectionLevel;
-  final Color foregroundColor;
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +21,8 @@ class QrImage extends StatelessWidget {
       errorCorrectionLevel: errorCorrectionLevel,
       version: version,
       size: size,
-      foregroundColor: foregroundColor,
+      foregroundColor: Colors.black,
+      backgroundColor: Colors.white,
       padding: EdgeInsets.zero,
     );
   }
