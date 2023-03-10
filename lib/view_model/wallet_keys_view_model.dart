ef4fe88c581038d4db43b8e2820275684f4f8e48
Serhii <borodenko.sv@gmail.com>
2023-03-10 14:34:16 +0200

    fix restore height for new wallet

--- lib/view_model/wallet_keys_view_model.dart
+++ lib/view_model/wallet_keys_view_model.dart
@@ -5,6 +5,7 @@ import 'package:cw_core/wallet_base.dart';
 import 'package:cake_wallet/src/screens/transaction_details/standart_list_item.dart';
 import 'package:cake_wallet/monero/monero.dart';
 import 'package:cake_wallet/haven/haven.dart';
+import 'package:cw_monero/api/wallet.dart' as monero_wallet;
 
 part 'wallet_keys_view_model.g.dart';
 
@@ -16,9 +17,11 @@ abstract class WalletKeysViewModelBase with Store {
             ? S.current.wallet_seed
             : S.current.wallet_keys,
         _wallet = wallet,
+        _restoreHeight = wallet.walletInfo.restoreHeight,
         items = ObservableList<StandartListItem>() {
     if (wallet.type == WalletType.monero) {
       final keys = monero!.getKeys(wallet);
+      _currentHeight = monero!.getHeigthByDate(date: DateTime.now());
 
       items.addAll([
         if (keys['publicSpendKey'] != null)
@@ -35,6 +38,7 @@ abstract class WalletKeysViewModelBase with Store {
 
     if (wallet.type == WalletType.haven) {
       final keys = haven!.getKeys(wallet);
+      _currentHeight = haven!.getHeigthByDate(date: DateTime.now());
 
       items.addAll([
         if (keys['publicSpendKey'] != null)
@@ -62,6 +66,10 @@ abstract class WalletKeysViewModelBase with Store {
 
   final WalletBase _wallet;
 
+  final int? _restoreHeight;
+
+  int? _currentHeight;
+
   String get _path {
     switch (_wallet.type) {
       case WalletType.monero:
@@ -77,8 +85,17 @@ abstract class WalletKeysViewModelBase with Store {
     }
   }
 
-  Map<String, String> get _queryParams =>
-      {'seed': _wallet.seed, 'height': _wallet.walletInfo.restoreHeight.toString()};
+  String get restoreHeight {
+    if (_currentHeight == null) {
+      return '';
+    }
+    if (_restoreHeight != 0) {
+      return _restoreHeight.toString();
+    }
+    return ((_currentHeight! / 1000).floor() * 1000).toString();
+  }
+
+  Map<String, String> get _queryParams => {'seed': _wallet.seed, 'height': restoreHeight};
 
   Uri get url {
     return Uri(
