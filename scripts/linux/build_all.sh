94db42884e0a24ab9463cc9e9562b76d95a7e364
sneurlax <sneurlax@gmail.com>
2023-03-21 17:12:38 -0500

    separate git versions file creation into its own script

--- scripts/linux/build_all.sh
+++ scripts/linux/build_all.sh
@@ -4,4 +4,5 @@ mkdir -p build
 ./build_shared.sh
 ./build_monero_all.sh
 ./build_wownero_all.sh
+./create_git_versions_file.sh
 ./build_sharedfile.sh
--- scripts/linux/build_sharedfile.sh
+++ scripts/linux/build_sharedfile.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
 . ./config.sh
-echo ''$(git log -1 --pretty=format:"%H")' '$(date) >> build/git_commit_version.txt
-VERSIONS_FILE=../../lib/git_versions.dart
-EXAMPLE_VERSIONS_FILE=../../lib/git_versions_example.dart
-if [ ! -f "$VERSIONS_FILE" ]; then
-    cp $EXAMPLE_VERSIONS_FILE $VERSIONS_FILE
-fi
-COMMIT=$(git log -1 --pretty=format:"%H")
-OS="LINUX"
-sed -i "/\/\*${OS}_VERSION/c\\/\*${OS}_VERSION\*\/ const ${OS}_VERSION = \"$COMMIT\";" $VERSIONS_FILE
+
 cd build
 mkdir monero_build
 mkdir wownero_build
@@ -25,4 +17,3 @@ cd $WOWNERO_BUILD
 cmake ../../cmakefiles/wownero/${TYPES_OF_BUILD}
 make -j$(nproc)
 cp libcw_wownero.so ../
-
