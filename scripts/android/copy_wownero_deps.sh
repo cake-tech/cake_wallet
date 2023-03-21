d78e9a4d1db95a08dcc897fd85c3381b87541533
sneurlax <sneurlax@gmail.com>
2023-03-21 16:46:30 -0500

    typofix

--- scripts/android/copy_wownero_deps.sh
+++ scripts/android/copy_wownero_deps.sh
@@ -2,10 +2,10 @@
 
 WORKDIR="$(pwd)/"build
 CW_DIR="$(pwd)"/../../../flutter_libmonero
-CW_EXRTERNAL_DIR=${CW_DIR}/cw_shared_external/ios/External/android
+CW_EXTERNAL_DIR=${CW_DIR}/cw_shared_external/ios/External/android
 CW_WOWNERO_EXTERNAL_DIR=${CW_DIR}/cw_wownero/ios/External/android
 
 mkdir -p ${CW_WOWNERO_EXTERNAL_DIR}/include
 
-cp $CW_EXRTERNAL_DIR/x86/include/wownero/wallet2_api.h ${CW_WOWNERO_EXTERNAL_DIR}/include
-cp -R $CW_EXRTERNAL_DIR/x86/include/wownero_seed ${CW_WOWNERO_EXTERNAL_DIR}/include
+cp $CW_EXTERNAL_DIR/x86/include/wownero/wallet2_api.h ${CW_WOWNERO_EXTERNAL_DIR}/include
+cp -R $CW_EXTERNAL_DIR/x86/include/wownero_seed ${CW_WOWNERO_EXTERNAL_DIR}/include
