505e407229e6b6b0c8333b6ab6847339df7ca4c1
Serhii <borodenko.sv@gmail.com>
2023-03-24 12:15:33 +0200

    Allow copy the Amount and Address

--- lib/src/screens/unspent_coins/unspent_coins_details_page.dart
+++ lib/src/screens/unspent_coins/unspent_coins_details_page.dart
@@ -2,12 +2,14 @@ import 'package:cake_wallet/src/screens/transaction_details/textfield_list_item.
 import 'package:cake_wallet/src/screens/transaction_details/widgets/textfield_list_row.dart';
 import 'package:cake_wallet/src/screens/unspent_coins/widgets/unspent_coins_switch_row.dart';
 import 'package:cake_wallet/src/widgets/standard_list.dart';
+import 'package:cake_wallet/utils/show_bar.dart';
 import 'package:cake_wallet/view_model/unspent_coins/unspent_coins_details_view_model.dart';
 import 'package:cake_wallet/view_model/unspent_coins/unspent_coins_switch_item.dart';
 import 'package:flutter/material.dart';
 import 'package:cake_wallet/src/widgets/list_row.dart';
 import 'package:cake_wallet/src/screens/transaction_details/standart_list_item.dart';
 import 'package:cake_wallet/src/screens/base_page.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:cake_wallet/generated/i18n.dart';
 
@@ -30,9 +32,16 @@ class UnspentCoinsDetailsPage extends BasePage {
           final item = unspentCoinsDetailsViewModel.items[index];
 
           if (item is StandartListItem) {
-            return ListRow(
-                title: '${item.title}:',
-                value: item.value);
+
+            return GestureDetector(
+              onTap: () {
+                Clipboard.setData(ClipboardData(text: item.value));
+                showBar<void>(context,
+                    S.of(context).transaction_details_copied(item.title));
+              },
+              child:
+              ListRow(title: '${item.title}:', value: item.value),
+            );
           }
 
           if (item is TextFieldListItem) {
