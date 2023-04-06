fd6f94ef2d80ea777b850612bd2f67896a4365f0
Blazebrain <davidadegoke16@gmail.com>
2023-04-06 15:46:59 +0100

    chore: Setup codebase to work on local

--- lib/wallet_type_utils.dart
+++ lib/wallet_type_utils.dart
@@ -2,28 +2,27 @@ import 'package:cw_core/wallet_type.dart';
 import 'package:cake_wallet/wallet_types.g.dart';
 
 bool get isMoneroOnly {
-    return availableWalletTypes.length == 1
-     	&& availableWalletTypes.first == WalletType.monero;
+  return availableWalletTypes.length == 1 &&
+      availableWalletTypes.first == WalletType.monero;
 }
 
 bool get isHaven {
-    return availableWalletTypes.length == 1
-        && availableWalletTypes.first == WalletType.haven;
+  return availableWalletTypes.length == 1 &&
+      availableWalletTypes.first == WalletType.haven;
 }
 
-
 bool get isSingleCoin {
-     return availableWalletTypes.length == 1;
+  return availableWalletTypes.length == 1;
 }
 
 String get approximatedAppName {
-    if (isMoneroOnly) {
-        return 'Monero.com';   
-    }
+  if (isMoneroOnly) {
+    return 'Monero.com';
+  }
 
-    if (isHaven) {
-        return 'Haven';
-    }
-     
-    return 'Cake Wallet';
+  if (isHaven) {
+    return 'Haven';
+  }
+
+  return 'Cake Wallet';
 }
