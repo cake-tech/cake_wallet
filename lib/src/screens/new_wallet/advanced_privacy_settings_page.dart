aeb88485c72ed1f1c93c1abe4a89fdd2fa3a59bc
Serhii <borodenko.sv@gmail.com>
2023-04-12 15:17:41 +0300

    don't add existing node

--- lib/src/screens/new_wallet/advanced_privacy_settings_page.dart
+++ lib/src/screens/new_wallet/advanced_privacy_settings_page.dart
@@ -105,7 +105,7 @@ class _AdvancedPrivacySettingsBodyState extends State<AdvancedPrivacySettingsBod
                     return;
                   }
 
-                  widget.nodeViewModel.save(saveAsCurrent: true);
+                  widget.nodeViewModel.save();
                 }
 
                 Navigator.pop(context);
