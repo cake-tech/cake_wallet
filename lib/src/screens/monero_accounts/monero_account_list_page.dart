b5f7f35529281141d9c79ba730da9a7656d3dbf3
OmarHatem <omarh.ismail1@gmail.com>
2023-04-25 15:44:12 +0200

    Fix Accounts Popup UI

--- lib/src/screens/monero_accounts/monero_account_list_page.dart
+++ lib/src/screens/monero_accounts/monero_account_list_page.dart
@@ -1,9 +1,7 @@
-import 'dart:ui';
 import 'package:cake_wallet/src/widgets/section_divider.dart';
+import 'package:cake_wallet/utils/responsive_layout_util.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:cake_wallet/palette.dart';
 import 'package:cake_wallet/routes.dart';
 import 'package:cake_wallet/generated/i18n.dart';
 import 'package:cake_wallet/view_model/monero_account_list/monero_account_list_view_model.dart';
@@ -36,131 +34,138 @@ class MoneroAccountListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertBackground(
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(left: 24, right: 24),
-                child: Text(
-                  S.of(context).choose_account,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Lato',
-                      decoration: TextDecoration.none,
-                      color: Colors.white
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 24, right: 24, top: 24),
-                child: GestureDetector(
-                  onTap: () => null,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(14)),
-                    child: Container(
-                      height: 296,
-                      color: Theme.of(context).textTheme!.headline1!.decorationColor!,
-                      child: Column(
-                        children: <Widget>[
-                          Expanded(
-                              child: Observer(
-                                  builder: (_) {
-                                    final accounts = accountListViewModel.accounts;
-                                    isAlwaysShowScrollThumb = accounts == null
-                                        ? false
-                                        : accounts.length > 3;
-
-                                    return Stack(
-                                      alignment: Alignment.center,
-                                      children: <Widget>[
-                                        ListView.separated(
-                                          padding: EdgeInsets.zero,
-                                          controller: controller,
-                                          separatorBuilder: (context, index) =>
-                                          const SectionDivider(),
-                                          itemCount: accounts.length ?? 0,
-                                          itemBuilder: (context, index) {
-                                            final account = accounts[index];
-
-                                            return AccountTile(
-                                                isCurrent: account.isSelected,
-                                                accountName: account.label,
-                                                onTap: () {
-                                                  if (account.isSelected) {
-                                                    return;
-                                                  }
-
-                                                  accountListViewModel
-                                                      .select(account);
-                                                  Navigator.of(context).pop();
-                                                },
-                                                onEdit: () async =>
-                                                await Navigator.of(context)
-                                                    .pushNamed(
-                                                    Routes.accountCreation,
-                                                    arguments: account));
-                                          },
-                                        ),
-                                        isAlwaysShowScrollThumb
-                                            ? CakeScrollbar(
-                                            backgroundHeight: backgroundHeight,
-                                            thumbHeight: thumbHeight,
-                                            fromTop: accountListViewModel
-                                                .scrollOffsetFromTop
-                                        )
-                                            : Offstage(),
-                                      ],
-                                    );
-                                  }
-                              )
-                          ),
-                          GestureDetector(
-                            onTap: () async => await Navigator.of(context)
-                                .pushNamed(Routes.accountCreation),
-                            child: Container(
-                              height: 62,
-                              color: Theme.of(context).cardColor,
-                              padding: EdgeInsets.only(left: 24, right: 24),
-                              child: Center(
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: <Widget>[
-                                    Icon(
-                                      Icons.add,
-                                      color: Colors.white,
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.only(left: 5),
-                                      child: Text(
-                                        S.of(context).create_new_account,
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w600,
-                                          fontFamily: 'Lato',
-                                          color: Colors.white,
-                                          decoration: TextDecoration.none,
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
+      child: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(left: 24, right: 24),
+                      child: Text(
+                        S.of(context).choose_account,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Lato',
+                            decoration: TextDecoration.none,
+                            color: Colors.white
+                        ),
                       ),
                     ),
-                  ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 24, right: 24, top: 24),
+                      child: GestureDetector(
+                        onTap: () => null,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(14)),
+                          child: Container(
+                            height: 296,
+                            color: Theme.of(context).textTheme!.headline1!.decorationColor!,
+                            child: Column(
+                              children: <Widget>[
+                                Expanded(
+                                    child: Observer(
+                                        builder: (_) {
+                                          final accounts = accountListViewModel.accounts;
+                                          isAlwaysShowScrollThumb = accounts == null
+                                              ? false
+                                              : accounts.length > 3;
+
+                                          return Stack(
+                                            alignment: Alignment.center,
+                                            children: <Widget>[
+                                              ListView.separated(
+                                                padding: EdgeInsets.zero,
+                                                controller: controller,
+                                                separatorBuilder: (context, index) =>
+                                                const SectionDivider(),
+                                                itemCount: accounts.length ?? 0,
+                                                itemBuilder: (context, index) {
+                                                  final account = accounts[index];
+
+                                                  return AccountTile(
+                                                      isCurrent: account.isSelected,
+                                                      accountName: account.label,
+                                                      onTap: () {
+                                                        if (account.isSelected) {
+                                                          return;
+                                                        }
+
+                                                        accountListViewModel
+                                                            .select(account);
+                                                        Navigator.of(context).pop();
+                                                      },
+                                                      onEdit: () async =>
+                                                      await Navigator.of(context)
+                                                          .pushNamed(
+                                                          Routes.accountCreation,
+                                                          arguments: account));
+                                                },
+                                              ),
+                                              isAlwaysShowScrollThumb
+                                                  ? CakeScrollbar(
+                                                  backgroundHeight: backgroundHeight,
+                                                  thumbHeight: thumbHeight,
+                                                  fromTop: accountListViewModel
+                                                      .scrollOffsetFromTop
+                                              )
+                                                  : Offstage(),
+                                            ],
+                                          );
+                                        }
+                                    )
+                                ),
+                                GestureDetector(
+                                  onTap: () async => await Navigator.of(context)
+                                      .pushNamed(Routes.accountCreation),
+                                  child: Container(
+                                    height: 62,
+                                    color: Theme.of(context).cardColor,
+                                    padding: EdgeInsets.only(left: 24, right: 24),
+                                    child: Center(
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: <Widget>[
+                                          Icon(
+                                            Icons.add,
+                                            color: Colors.white,
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.only(left: 5),
+                                            child: Text(
+                                              S.of(context).create_new_account,
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w600,
+                                                fontFamily: 'Lato',
+                                                color: Colors.white,
+                                                decoration: TextDecoration.none,
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
+                SizedBox(height: ResponsiveLayoutUtil.kPopupSpaceHeight),
+                AlertCloseButton()
+              ],
+            ),
           ),
-          AlertCloseButton()
         ],
       ),
     );
