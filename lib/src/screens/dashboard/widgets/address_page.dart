6ff554e997870e359c8ac5feeb9241a785fc0e6e
Rafael Saes <git@saes.io>
2023-05-02 16:29:30 -0300

    fix: revert changes & use currentTheme.type to use proper theme style

--- lib/src/screens/dashboard/widgets/address_page.dart
+++ lib/src/screens/dashboard/widgets/address_page.dart
@@ -60,7 +60,43 @@ class AddressPage extends BasePage {
   bool effectsInstalled = false;
 
   @override
-  bool get lightTitleColor => true;
+  Color? get titleColor =>
+      currentTheme.type == ThemeType.bright ? Colors.white : null;
+
+  @override
+  Widget? leading(BuildContext context) {
+    final _backButton = Icon(
+      Icons.arrow_back_ios,
+      color: titleColor,
+      size: 16,
+    );
+    final _closeButton = currentTheme.type == ThemeType.dark
+        ? closeButtonImageDarkTheme
+        : closeButtonImage;
+
+    bool isMobileView = ResponsiveLayoutUtil.instance.isMobile(context);
+
+    return MergeSemantics(
+      child: SizedBox(
+        height: isMobileView ? 37 : 45,
+        width: isMobileView ? 37 : 45,
+        child: ButtonTheme(
+          minWidth: double.minPositive,
+          child: Semantics(
+            label: !isMobileView ? 'Close' : 'Back',
+            child: TextButton(
+              style: ButtonStyle(
+                overlayColor: MaterialStateColor.resolveWith(
+                    (states) => Colors.transparent),
+              ),
+              onPressed: () => onClose(context),
+              child: !isMobileView ? _closeButton : _backButton,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget middle(BuildContext context) =>
--- lib/src/screens/receive/receive_page.dart
+++ lib/src/screens/receive/receive_page.dart
@@ -53,7 +53,8 @@ class ReceivePage extends BasePage {
   final FocusNode _cryptoAmountFocus;
 
   @override
-  bool get lightTitleColor => true;
+  Color? get titleColor =>
+      currentTheme.type == ThemeType.bright ? Colors.white : null;
 
   @override
   Widget middle(BuildContext context) {
