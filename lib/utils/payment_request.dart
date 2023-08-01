ca3bf2f3f5f3775fe5e20512ea5305284798a30b
fosse <matt.cfosse@gmail.com>
2023-08-01 15:56:34 -0400

    fix for scanning QR codes

--- lib/utils/payment_request.dart
+++ lib/utils/payment_request.dart
@@ -1,3 +1,5 @@
+import 'package:cw_nano/nano_util.dart';
+
 class PaymentRequest {
   PaymentRequest(this.address, this.amount, this.note, this.scheme);
 
@@ -10,9 +12,14 @@ class PaymentRequest {
     if (uri != null) {
       address = uri.path;
       amount = uri.queryParameters['tx_amount'] ?? uri.queryParameters['amount'] ?? "";
-      note = uri.queryParameters['tx_description']
-          ?? uri.queryParameters['message'] ?? "";
+      note = uri.queryParameters['tx_description'] ?? uri.queryParameters['message'] ?? "";
       scheme = uri.scheme;
+    }
+
+    if (address.contains("nano")) {
+      amount = NanoUtil.getRawAsUsableString(amount, NanoUtil.rawPerNano);
+    } else if (address.contains("ban")) {
+      amount = NanoUtil.getRawAsUsableString(amount, NanoUtil.rawPerBanano);
     }
 
     return PaymentRequest(address, amount, note, scheme);
