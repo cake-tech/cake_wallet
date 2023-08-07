d1684f5be8e1a11408bc7d179cc6bdcde7178b4e
Omar Hatem <omarh.ismail1@gmail.com>
2023-08-07 14:43:04 +0300

    v4.8.0 and v1.5.0 (#1021)
    * Update versions
    Add Ethereum to MacOS
    Fix Ethereum config when building Monero.com
    
    * Fix Restore from backup issue
    
    * Update app build versions
    Fix minor UI issue

--- tool/configure.dart
+++ tool/configure.dart
@@ -477,8 +477,6 @@ Future<void> generateEthereum(bool hasImplementation) async {
 
   final outputFile = File(ethereumOutputPath);
   const ethereumCommonHeaders = """
-""";
-  const ethereumCWHeaders = """
 import 'package:cake_wallet/view_model/send/output.dart';
 import 'package:cw_core/crypto_amount_format.dart';
 import 'package:cw_core/crypto_currency.dart';
@@ -490,6 +488,9 @@ import 'package:cw_core/wallet_base.dart';
 import 'package:cw_core/wallet_credentials.dart';
 import 'package:cw_core/wallet_info.dart';
 import 'package:cw_core/wallet_service.dart';
+import 'package:hive/hive.dart';
+""";
+  const ethereumCWHeaders = """
 import 'package:cw_ethereum/ethereum_formatter.dart';
 import 'package:cw_ethereum/ethereum_mnemonics.dart';
 import 'package:cw_ethereum/ethereum_transaction_credentials.dart';
@@ -498,7 +499,6 @@ import 'package:cw_ethereum/ethereum_wallet.dart';
 import 'package:cw_ethereum/ethereum_wallet_creation_credentials.dart';
 import 'package:cw_ethereum/ethereum_wallet_service.dart';
 import 'package:cw_ethereum/ethereum_transaction_priority.dart';
-import 'package:hive/hive.dart';
 """;
   const ethereumCwPart = "part 'cw_ethereum.dart';";
   const ethereumContent = """
