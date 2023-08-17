faca80a86eec61cedd0c0c28f7e89f07576474ca
Konstantin Ullrich <konstantinullrich12@gmail.com>
2023-08-17 15:14:41 +0200

    CW-228 Add more translation Tools

--- tool/append_translation.dart
+++ tool/append_translation.dart
@@ -1,15 +1,5 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:translator/translator.dart';
-
-const defaultLang = "en";
-const langs = [
-  "ar", "bg", "cs", "de", "en", "es", "fr", "ha", "hi", "hr", "id", "it",
-  "ja", "ko", "my", "nl", "pl", "pt", "ru", "th", "tr", "uk", "ur", "yo",
-  "zh-cn" // zh, but Google Translate uses zh-cn for Chinese (Simplified)
-];
-final translator = GoogleTranslator();
+import 'utils/translation/translation_constants.dart';
+import 'utils/translation/translation_utils.dart';
 
 void main(List<String> args) async {
   if (args.length != 2) {
@@ -23,44 +13,7 @@ void main(List<String> args) async {
   print('Appending "$name": "$text"');
 
   for (var lang in langs) {
-    final fileName = getFileName(lang);
-    final translation = await getTranslation(text, lang);
-
-    appendArbFile(fileName, name, translation);
+    appendTranslation(lang, name, text);
   }
 }
 
-void appendArbFile(String fileName, String name, String text) {
-  final file = File(fileName);
-  final inputContent = file.readAsStringSync();
-  final arbObj = json.decode(inputContent) as Map<String, dynamic>;
-
-  if (arbObj.containsKey(name)) {
-    print("String $name already exists in $fileName!");
-    return;
-  }
-
-  arbObj.addAll({name: text});
-
-  final outputContent = json
-      .encode(arbObj)
-      .replaceAll('","', '",\n  "')
-      .replaceAll('{"', '{\n  "')
-      .replaceAll('"}', '"\n}')
-      .replaceAll('":"', '": "');
-
-  file.writeAsStringSync(outputContent);
-}
-
-
-Future<String> getTranslation(String text, String lang) async {
-  if (lang == defaultLang) return text;
-  return (await translator.translate(text, from: defaultLang, to: lang)).text;
-}
-
-String getFileName(String lang) {
-  final shortLang = lang
-      .split("-")
-      .first;
-  return "./res/values/strings_$shortLang.arb";
-}
