aab58ecd624af9a0c4b288a9fb2e1c0a8e4c585f
Omar Hatem <omarh.ismail1@gmail.com>
2023-08-29 00:02:46 +0300

    Update build_haven.sh

--- scripts/android/build_haven.sh
+++ scripts/android/build_haven.sh
@@ -5,7 +5,7 @@ HAVEN_VERSION=tags/v3.0.7
 HAVEN_SRC_DIR=${WORKDIR}/haven
 
 git clone https://github.com/haven-protocol-org/haven-main.git ${HAVEN_SRC_DIR}
-git fetch --all --tags --prune
+git fetch --tags
 git checkout ${HAVEN_VERSION}
 cd $HAVEN_SRC_DIR
 git submodule init
