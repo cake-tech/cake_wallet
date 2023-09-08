94f256997f083271e3e540ef734a31c3784cf414
Omar Hatem <omarh.ismail1@gmail.com>
2023-09-08 17:15:28 +0300

    New versions (#1073)

--- lib/src/screens/settings/display_settings_page.dart
+++ lib/src/screens/settings/display_settings_page.dart
@@ -21,64 +21,66 @@ class DisplaySettingsPage extends BasePage {
 
   @override
   Widget body(BuildContext context) {
-    return Observer(builder: (_) {
-      return Container(
-        padding: EdgeInsets.only(top: 10),
-        child: Column(
-          children: [
-            SettingsSwitcherCell(
-                title: S.current.settings_display_balance,
-                value: _displaySettingsViewModel.shouldDisplayBalance,
+    return SingleChildScrollView(
+      child: Observer(builder: (_) {
+        return Container(
+          padding: EdgeInsets.only(top: 10),
+          child: Column(
+            children: [
+              SettingsSwitcherCell(
+                  title: S.current.settings_display_balance,
+                  value: _displaySettingsViewModel.shouldDisplayBalance,
+                  onValueChange: (_, bool value) {
+                    _displaySettingsViewModel.setShouldDisplayBalance(value);
+                  }),
+              SettingsSwitcherCell(
+                title: S.current.show_market_place,
+                value: _displaySettingsViewModel.shouldShowMarketPlaceInDashboard,
                 onValueChange: (_, bool value) {
-                  _displaySettingsViewModel.setShouldDisplayBalance(value);
-                }),
-            SettingsSwitcherCell(
-              title: S.current.show_market_place,
-              value: _displaySettingsViewModel.shouldShowMarketPlaceInDashboard,
-              onValueChange: (_, bool value) {
-                _displaySettingsViewModel.setShouldShowMarketPlaceInDashbaord(value);
-              },
-            ),
-            //if (!isHaven) it does not work correctly
-            if (!_displaySettingsViewModel.disabledFiatApiMode)
-              SettingsPickerCell<FiatCurrency>(
-                title: S.current.settings_currency,
-                searchHintText: S.current.search_currency,
-                items: FiatCurrency.all,
-                selectedItem: _displaySettingsViewModel.fiatCurrency,
-                onItemSelected: (FiatCurrency currency) =>
-                    _displaySettingsViewModel.setFiatCurrency(currency),
-                images: FiatCurrency.all
-                    .map((e) => Image.asset("assets/images/flags/${e.countryCode}.png"))
-                    .toList(),
-                isGridView: true,
-                matchingCriteria: (FiatCurrency currency, String searchText) {
-                  return currency.title.toLowerCase().contains(searchText) ||
-                      currency.fullName.toLowerCase().contains(searchText);
+                  _displaySettingsViewModel.setShouldShowMarketPlaceInDashbaord(value);
                 },
               ),
-            SettingsPickerCell<String>(
-              title: S.current.settings_change_language,
-              searchHintText: S.current.search_language,
-              items: LanguageService.list.keys.toList(),
-              displayItem: (dynamic code) {
-                return LanguageService.list[code] ?? '';
-              },
-              selectedItem: _displaySettingsViewModel.languageCode,
-              onItemSelected: _displaySettingsViewModel.onLanguageSelected,
-              images: LanguageService.list.keys
-                  .map((e) => Image.asset(
-                      "assets/images/flags/${LanguageService.localeCountryCode[e]}.png"))
-                  .toList(),
-              matchingCriteria: (String code, String searchText) {
-                return LanguageService.list[code]?.toLowerCase().contains(searchText) ?? false;
-              },
-            ),
-            if (ResponsiveLayoutUtil.instance.isMobile && DeviceInfo.instance.isMobile)
-              SettingsThemeChoicesCell(_displaySettingsViewModel),
-          ],
-        ),
-      );
-    });
+              //if (!isHaven) it does not work correctly
+              if (!_displaySettingsViewModel.disabledFiatApiMode)
+                SettingsPickerCell<FiatCurrency>(
+                  title: S.current.settings_currency,
+                  searchHintText: S.current.search_currency,
+                  items: FiatCurrency.all,
+                  selectedItem: _displaySettingsViewModel.fiatCurrency,
+                  onItemSelected: (FiatCurrency currency) =>
+                      _displaySettingsViewModel.setFiatCurrency(currency),
+                  images: FiatCurrency.all
+                      .map((e) => Image.asset("assets/images/flags/${e.countryCode}.png"))
+                      .toList(),
+                  isGridView: true,
+                  matchingCriteria: (FiatCurrency currency, String searchText) {
+                    return currency.title.toLowerCase().contains(searchText) ||
+                        currency.fullName.toLowerCase().contains(searchText);
+                  },
+                ),
+              SettingsPickerCell<String>(
+                title: S.current.settings_change_language,
+                searchHintText: S.current.search_language,
+                items: LanguageService.list.keys.toList(),
+                displayItem: (dynamic code) {
+                  return LanguageService.list[code] ?? '';
+                },
+                selectedItem: _displaySettingsViewModel.languageCode,
+                onItemSelected: _displaySettingsViewModel.onLanguageSelected,
+                images: LanguageService.list.keys
+                    .map((e) => Image.asset(
+                        "assets/images/flags/${LanguageService.localeCountryCode[e]}.png"))
+                    .toList(),
+                matchingCriteria: (String code, String searchText) {
+                  return LanguageService.list[code]?.toLowerCase().contains(searchText) ?? false;
+                },
+              ),
+              if (ResponsiveLayoutUtil.instance.isMobile && DeviceInfo.instance.isMobile)
+                SettingsThemeChoicesCell(_displaySettingsViewModel),
+            ],
+          ),
+        );
+      }),
+    );
   }
 }
