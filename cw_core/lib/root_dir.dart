9e8698816af64bcfd560f57db8154c16c8258f58
Rafael Saes <git@saes.io>
2023-09-14 15:56:36 -0300

    fix: errors trying to open documents file

--- cw_core/lib/root_dir.dart
+++ cw_core/lib/root_dir.dart
@@ -3,24 +3,24 @@ import 'package:path_provider/path_provider.dart';
 
 String? _rootDirPath;
 
-void setRootDirFromEnv()
-	=> _rootDirPath = Platform.environment['CAKE_WALLET_DIR'];
+void setRootDirFromEnv() => _rootDirPath = Platform.environment['CAKE_WALLET_DIR'];
 
 Future<Directory> getAppDir({String appName = 'cake_wallet'}) async {
-	Directory dir;
+  Directory dir;
 
-	if (_rootDirPath != null && _rootDirPath!.isNotEmpty) {
-		dir = Directory.fromUri(Uri.file(_rootDirPath!));
-		dir.create(recursive: true);
-	} else {
-		dir = await getApplicationDocumentsDirectory();
+  if (_rootDirPath != null && _rootDirPath!.isNotEmpty) {
+    dir = Directory.fromUri(Uri.file(_rootDirPath!));
+    dir.create(recursive: true);
+  } else {
+    if (Platform.isLinux) {
+      final appDirPath = '/home/${Platform.environment['USER']}/.$appName';
+      dir = Directory.fromUri(Uri.file(appDirPath));
+      await dir.create(recursive: true);
+    } else {
+      dir = await getApplicationDocumentsDirectory();
+    }
+  }
 
-		if (Platform.isLinux) {
-			final appDirPath = '${dir.path}/$appName';
-			dir = Directory.fromUri(Uri.file(appDirPath));
-			await dir.create(recursive: true);
-		}
-	}
-
-	return dir;
+  return dir;
 }
+
