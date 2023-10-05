e53717195d0a8dfa488f5cff92c025d00948e30b
Blazebrain <davidadegoke16@gmail.com>
2023-10-05 12:41:36 +0100

    fix: Wallet type check when registering key service

--- lib/core/wallet_connect/wallet_connect_key_service.dart
+++ lib/core/wallet_connect/wallet_connect_key_service.dart
@@ -4,6 +4,7 @@ import 'package:cw_core/balance.dart';
 import 'package:cw_core/transaction_history.dart';
 import 'package:cw_core/transaction_info.dart';
 import 'package:cw_core/wallet_base.dart';
+import 'package:cw_core/wallet_type.dart';
 
 abstract class WalletConnectKeyService {
   /// Returns a list of all the keys.
@@ -32,10 +33,9 @@ class KeyServiceImpl implements WalletConnectKeyService {
               'eip155:42161',
               'eip155:80001',
             ],
-            privateKey: ethereum!.getPrivateKey(wallet),
-            publicKey: ethereum!.getPublicKey(wallet),
+            privateKey: _getPrivateKey(wallet),
+            publicKey: _getPublicKey(wallet),
           ),
-          
         ];
 
   late final WalletBase<Balance, TransactionHistoryBase<TransactionInfo>, TransactionInfo> wallet;
@@ -68,5 +68,17 @@ class KeyServiceImpl implements WalletConnectKeyService {
       }
     }
     return accounts;
+  }
+
+  static String _getPrivateKey(WalletBase wallet) {
+    if (wallet.type != WalletType.ethereum) return '';
+
+    return ethereum!.getPrivateKey(wallet);
+  }
+
+  static String _getPublicKey(WalletBase wallet) {
+    if (wallet.type != WalletType.ethereum) return '';
+
+    return ethereum!.getPublicKey(wallet);
   }
 }
