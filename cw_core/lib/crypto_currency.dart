e15a4fc23ad8e45310d3d2eee579830a651b4c19
Serhii <borodenko.sv@gmail.com>
2023-10-05 14:05:49 +0300

    merge fixes

--- cw_core/lib/crypto_currency.dart
+++ cw_core/lib/crypto_currency.dart
@@ -200,7 +200,7 @@ class CryptoCurrency extends EnumerableItem<int> with Serializable<int> implemen
   static const zrx = CryptoCurrency(title: 'ZRX', tag: 'ETH', fullName: '0x Protocol', raw: 83, name: 'zrx', iconPath: 'assets/images/crypto_assets/zrx_icon.svg');
   static const dydx = CryptoCurrency(title: 'DYDX', tag: 'ETH', fullName: 'dYdX', raw: 84, name: 'dydx', iconPath: 'assets/images/crypto_assets/dydx_icon.svg');
   static const steth = CryptoCurrency(title: 'STETH', tag: 'ETH', fullName: 'Lido Staked Ethereum', raw: 85, name: 'steth', iconPath: 'assets/images/crypto_assets/steth_icon.svg');
-  static const banano = CryptoCurrency(title: 'BAN', raw: 86, name: 'banano', iconPath: 'assets/images/nano_icon.png');
+  static const banano = CryptoCurrency(title: 'BAN', raw: 86, name: 'banano', iconPath: 'assets/images/crypto_assets/ban_icon.svg');
 
   static final Map<int, CryptoCurrency> _rawCurrencyMap =
     [...all, ...havenCurrencies].fold<Map<int, CryptoCurrency>>(<int, CryptoCurrency>{}, (acc, item) {
--- lib/src/screens/dashboard/widgets/menu_widget.dart
+++ lib/src/screens/dashboard/widgets/menu_widget.dart
@@ -30,8 +30,8 @@ class MenuWidgetState extends State<MenuWidget> {
       this.litecoinIcon = Image.asset('assets/images/litecoin_menu.png'),
       this.havenIcon = Image.asset('assets/images/haven_menu.png'),
       this.ethereumIcon = Image.asset('assets/images/eth_icon.png'),
-      this.nanoIcon = Image.asset('assets/images/nano_icon.png'),
-      this.bananoIcon = Image.asset('assets/images/nano_icon.png');
+      this.nanoIcon = Image.asset('assets/images/nano_menu.png'),
+      this.bananoIcon = Image.asset('assets/images/nano_menu.png');
 
 
   final largeScreen = 731;
