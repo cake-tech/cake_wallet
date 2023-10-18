e7fe64ee1fb88b843de37f9f55467c4688d78633
fosse <matt.cfosse@gmail.com>
2023-10-18 13:33:29 -0400

    merge fixes

--- lib/bitcoin/cw_bitcoin.dart
+++ lib/bitcoin/cw_bitcoin.dart
@@ -2,9 +2,6 @@ part of 'bitcoin.dart';
 
 class CWBitcoin extends Bitcoin {
   @override
-  TransactionPriority getMediumTransactionPriority() => BitcoinTransactionPriority.medium;
-
-  @override
   WalletCredentials createBitcoinRestoreWalletFromSeedCredentials({
     required String name,
     required String mnemonic,
@@ -19,113 +16,14 @@ class CWBitcoin extends Bitcoin {
           derivationType: derivationType,
           derivationPath: derivationPath);
 
-  String bitcoinTransactionPriorityWithLabel(TransactionPriority priority, int rate) =>
-      (priority as BitcoinTransactionPriority).labelWithRate(rate);
-
-  void updateUnspents(Object wallet) async {
-    final bitcoinWallet = wallet as ElectrumWallet;
-    await bitcoinWallet.updateUnspent();
-  }
-
-	@override
-	List<TransactionPriority> getLitecoinTransactionPriorities()
-		=> LitecoinTransactionPriority.all;
-
-	@override
-	TransactionPriority deserializeBitcoinTransactionPriority(int raw)
-		=> BitcoinTransactionPriority.deserialize(raw: raw);
-
-	@override
-	TransactionPriority deserializeLitecoinTransactionPriority(int raw)
-		=> LitecoinTransactionPriority.deserialize(raw: raw);
-
-	@override
-	int getFeeRate(Object wallet, TransactionPriority priority) {
-		final bitcoinWallet = wallet as ElectrumWallet;
-		return bitcoinWallet.feeRate(priority);
-	}
-
-	@override
-	Future<void> generateNewAddress(Object wallet) async {
-		final bitcoinWallet = wallet as ElectrumWallet;
-		await bitcoinWallet.walletAddresses.generateNewAddress();
-	}
-	
-	@override
-	Object createBitcoinTransactionCredentials(List<Output> outputs, {required TransactionPriority priority, int? feeRate})
-		=> BitcoinTransactionCredentials(
-			outputs.map((out) => OutputInfo(
-					fiatAmount: out.fiatAmount,
-					cryptoAmount: out.cryptoAmount,
-					address: out.address,
-					note: out.note,
-					sendAll: out.sendAll,
-					extractedAddress: out.extractedAddress,
-					isParsedAddress: out.isParsedAddress,
-					formattedCryptoAmount: out.formattedCryptoAmount))
-			.toList(),
-			priority: priority as BitcoinTransactionPriority,
-			feeRate: feeRate);
-
-	@override
-	Object createBitcoinTransactionCredentialsRaw(List<OutputInfo> outputs, {TransactionPriority? priority, required int feeRate})
-		=> BitcoinTransactionCredentials(
-				outputs,
-				priority: priority != null ? priority as BitcoinTransactionPriority : null,
-				feeRate: feeRate);
-
-	@override
-	List<String> getAddresses(Object wallet) {
-		final bitcoinWallet = wallet as ElectrumWallet;
-		return bitcoinWallet.walletAddresses.addresses
-			.map((BitcoinAddressRecord addr) => addr.address)
-			.toList();
-	}
-
-	@override
-	String getAddress(Object wallet) {
-		final bitcoinWallet = wallet as ElectrumWallet;
-		return bitcoinWallet.walletAddresses.address;
-	}
-
-	@override
-	String formatterBitcoinAmountToString({required int amount})
-		=> bitcoinAmountToString(amount: amount);
-
-	@override	
-	double formatterBitcoinAmountToDouble({required int amount})
-		=> bitcoinAmountToDouble(amount: amount);
-
-	@override	
-	int formatterStringDoubleToBitcoinAmount(String amount)
-		=> stringDoubleToBitcoinAmount(amount);
-
   @override
-  String bitcoinTransactionPriorityWithLabel(TransactionPriority priority, int rate)
-    => (priority as BitcoinTransactionPriority).labelWithRate(rate);
-
-	@override
-	List<BitcoinUnspent> getUnspents(Object wallet) {
-		final bitcoinWallet = wallet as ElectrumWallet;
-		return bitcoinWallet.unspentCoins;
-	}
-
-	void updateUnspents(Object wallet) async {
-		final bitcoinWallet = wallet as ElectrumWallet;
-		await bitcoinWallet.updateUnspent();
-	}
-
-	WalletService createBitcoinWalletService(Box<WalletInfo> walletInfoSource, Box<UnspentCoinsInfo> unspentCoinSource) {
-		return BitcoinWalletService(walletInfoSource, unspentCoinSource);
-	}
-
-	WalletService createLitecoinWalletService(Box<WalletInfo> walletInfoSource, Box<UnspentCoinsInfo> unspentCoinSource) {
-		return LitecoinWalletService(walletInfoSource, unspentCoinSource);
-	}
-  
-  @override
-  TransactionPriority getBitcoinTransactionPriorityMedium()
-    => BitcoinTransactionPriority.medium;
+  WalletCredentials createBitcoinRestoreWalletFromWIFCredentials(
+          {required String name,
+          required String password,
+          required String wif,
+          WalletInfo? walletInfo}) =>
+      BitcoinRestoreWalletFromWIFCredentials(
+          name: name, password: password, wif: wif, walletInfo: walletInfo);
 
   @override
   WalletCredentials createBitcoinNewWalletCredentials(
@@ -146,19 +44,6 @@ class CWBitcoin extends Bitcoin {
       'publicKey': keys.publicKey
     };
   }
-
-  @override
-  List<TransactionPriority> getTransactionPriorities() => BitcoinTransactionPriority.all;
-
-  List<TransactionPriority> getLitecoinTransactionPriorities() => LitecoinTransactionPriority.all;
-
-  @override
-  TransactionPriority deserializeBitcoinTransactionPriority(int raw) =>
-      BitcoinTransactionPriority.deserialize(raw: raw);
-
-  @override
-  TransactionPriority deserializeLitecoinTransactionPriority(int raw) =>
-      LitecoinTransactionPriority.deserialize(raw: raw);
 
   @override
   int getFeeRate(Object wallet, TransactionPriority priority) {
@@ -223,18 +108,43 @@ class CWBitcoin extends Bitcoin {
   int formatterStringDoubleToBitcoinAmount(String amount) => stringDoubleToBitcoinAmount(amount);
 
   @override
-  List<Unspent> getUnspents(Object wallet) {
+  String bitcoinTransactionPriorityWithLabel(TransactionPriority priority, int rate) =>
+      (priority as BitcoinTransactionPriority).labelWithRate(rate);
+
+  @override
+  List<BitcoinUnspent> getUnspents(Object wallet) {
     final bitcoinWallet = wallet as ElectrumWallet;
-    return bitcoinWallet.unspentCoins
-        .map((BitcoinUnspent bitcoinUnspent) => Unspent(bitcoinUnspent.address.address,
-            bitcoinUnspent.hash, bitcoinUnspent.value, bitcoinUnspent.vout, null))
-        .toList();
+    return bitcoinWallet.unspentCoins;
+  }
+
+  void updateUnspents(Object wallet) async {
+    final bitcoinWallet = wallet as ElectrumWallet;
+    await bitcoinWallet.updateUnspent();
   }
 
   WalletService createBitcoinWalletService(
       Box<WalletInfo> walletInfoSource, Box<UnspentCoinsInfo> unspentCoinSource) {
     return BitcoinWalletService(walletInfoSource, unspentCoinSource);
   }
+
+  WalletService createLitecoinWalletService(
+      Box<WalletInfo> walletInfoSource, Box<UnspentCoinsInfo> unspentCoinSource) {
+    return LitecoinWalletService(walletInfoSource, unspentCoinSource);
+  }
+
+  @override
+  List<TransactionPriority> getTransactionPriorities() => BitcoinTransactionPriority.all;
+
+  @override
+  List<TransactionPriority> getLitecoinTransactionPriorities() => LitecoinTransactionPriority.all;
+
+  @override
+  TransactionPriority deserializeBitcoinTransactionPriority(int raw) =>
+      BitcoinTransactionPriority.deserialize(raw: raw);
+
+  @override
+  TransactionPriority deserializeLitecoinTransactionPriority(int raw) =>
+      LitecoinTransactionPriority.deserialize(raw: raw);
 
   @override
   TransactionPriority getBitcoinTransactionPriorityMedium() => BitcoinTransactionPriority.medium;
@@ -247,6 +157,9 @@ class CWBitcoin extends Bitcoin {
 
   @override
   TransactionPriority getLitecoinTransactionPrioritySlow() => LitecoinTransactionPriority.slow;
+
+  @override
+  TransactionPriority getMediumTransactionPriority() => BitcoinTransactionPriority.medium;
 
   @override
   Future<List<DerivationType>> compareDerivationMethods(
