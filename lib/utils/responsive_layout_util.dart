402a98dc999a608fc78e31110935b25e600429e6
Serhii <borodenko.sv@gmail.com>
2023-10-23 13:25:48 +0300

    Update responsive_layout_util.dart

--- lib/utils/responsive_layout_util.dart
+++ lib/utils/responsive_layout_util.dart
@@ -5,7 +5,7 @@ part 'responsive_layout_util.g.dart';
 
 class _ResponsiveLayoutUtil = ResponsiveLayoutUtilBase with _$_ResponsiveLayoutUtil;
 
-abstract class ResponsiveLayoutUtilBase with Store {
+abstract class ResponsiveLayoutUtilBase with Store, WidgetsBindingObserver {
   static const double _kMobileThreshold = 550;
   static const double kDesktopMaxWidthConstraint = 400;
   static const double kDesktopMaxDashBoardWidthConstraint = 900;
@@ -13,8 +13,15 @@ abstract class ResponsiveLayoutUtilBase with Store {
   static const double kPopupSpaceHeight = 100;
 
   ResponsiveLayoutUtilBase() {
+    WidgetsBinding.instance.addObserver(this);
     final initialMediaQuery = MediaQueryData.fromView(WidgetsBinding.instance!.window);
     updateDeviceInfo(initialMediaQuery);
+  }
+
+  @override
+  void didChangeMetrics() {
+    final mediaQuery = MediaQueryData.fromView(WidgetsBinding.instance!.window);
+    updateDeviceInfo(mediaQuery);
   }
 
   @observable
