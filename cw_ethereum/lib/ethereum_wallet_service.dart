51fd3a826024e4a73d259c530379a309730b584d
Serhii <borodenko.sv@gmail.com>
2023-11-15 15:02:28 +0200

    eth seed lengh option

--- cw_ethereum/lib/ethereum_wallet_service.dart
+++ cw_ethereum/lib/ethereum_wallet_service.dart
@@ -20,7 +20,14 @@ class EthereumWalletService extends WalletService<EthereumNewWalletCredentials,
 
   @override
   Future<EthereumWallet> create(EthereumNewWalletCredentials credentials) async {
-    final mnemonic = bip39.generateMnemonic();
+
+    final strength = (credentials.seedPhraseLength == 12)
+        ? 128
+        : (credentials.seedPhraseLength == 24)
+        ? 256
+        : 128;
+
+    final mnemonic = bip39.generateMnemonic(strength: strength);
     final wallet = EthereumWallet(
       walletInfo: credentials.walletInfo!,
       mnemonic: mnemonic,
