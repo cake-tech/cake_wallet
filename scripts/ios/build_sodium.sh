de5a06407939a45a6dce479fb0812fdd817d5a81
fosse <matt.cfosse@gmail.com>
2023-11-30 11:17:52 -0500

    fix

--- scripts/ios/build_sodium.sh
+++ scripts/ios/build_sodium.sh
@@ -9,7 +9,9 @@ echo "============================ SODIUM ============================"
 
 echo "Cloning SODIUM from - $SODIUM_URL"
 git clone $SODIUM_URL $SODIUM_PATH --branch stable
+git checkout 675149b9b8b66ff44152553fb3ebf9858128363d
 cd $SODIUM_PATH
+./autogen.sh
 ./dist-build/ios.sh
 
 mv ${SODIUM_PATH}/libsodium-ios/include/* $EXTERNAL_IOS_INCLUDE_DIR
