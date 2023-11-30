cd6ec8f6ac453bbcbead061c2d47351f678101ae
Serhii <borodenko.sv@gmail.com>
2023-11-30 11:45:33 +0200

    update localization files

--- lib/src/screens/buy/buy_options_page.dart
+++ lib/src/screens/buy/buy_options_page.dart
@@ -19,7 +19,8 @@ class BuyOptionsPage extends BasePage {
   final iconLightRobinhood = 'assets/images/robinhood_light.png';
   final iconDarkOnramper = 'assets/images/onramper_dark.png';
   final iconLightOnramper = 'assets/images/onramper_light.png';
-  final iconDFXImage = 'assets/images/dfx_icon.png';
+  final iconDarkDFX = 'assets/images/dfx_dark.png';
+  final iconLightDFX = 'assets/images/dfx_light.png';
 
   @override
   String get title => S.current.buy;
@@ -29,18 +30,18 @@ class BuyOptionsPage extends BasePage {
 
   @override
   Widget body(BuildContext context) {
-    final isLightMode = Theme
-        .of(context)
-        .extension<OptionTileTheme>()
-        ?.useDarkImage ?? false;
-    final iconRobinhood =
-    Image.asset(
-        isLightMode ? iconLightRobinhood : iconDarkRobinhood, height: 40,
+    final isLightMode =
+        Theme.of(context).extension<OptionTileTheme>()?.useDarkImage ?? false;
+    final iconRobinhood = Image.asset(
+        isLightMode ? iconLightRobinhood : iconDarkRobinhood,
+        height: 40,
         width: 40);
-    final iconOnramper =
-    Image.asset(isLightMode ? iconLightOnramper : iconDarkOnramper, height: 40,
+    final iconOnramper = Image.asset(
+        isLightMode ? iconLightOnramper : iconDarkOnramper,
+        height: 40,
         width: 40);
-    final iconDFX = Image.asset(iconDFXImage, height: 40, width: 40);
+    final iconDFX = Image.asset(isLightMode ? iconLightDFX : iconDarkDFX,
+        height: 40, width: 40);
     final availableProviders = dashboardViewModel.availableProviders;
 
     return Container(
@@ -50,56 +51,52 @@ class BuyOptionsPage extends BasePage {
           child: Column(
             children: [
               if (availableProviders.contains(BuyProviderType.Onramper))
-              Padding(
-                padding: EdgeInsets.only(top: 24),
-                child: OptionTile(
-                  image: iconOnramper,
-                  title: "Onramper",
-                  description: S
-                      .of(context)
-                      .onramper_option_description,
-                  onPressed: () async =>
-                  await getIt.get<OnRamperBuyProvider>().launchProvider(
-                      context),
+                Padding(
+                  padding: EdgeInsets.only(top: 24),
+                  child: OptionTile(
+                    image: iconOnramper,
+                    title: "Onramper",
+                    description: S.of(context).onramper_option_description,
+                    onPressed: () async => await getIt
+                        .get<OnRamperBuyProvider>()
+                        .launchProvider(context),
+                  ),
                 ),
-              ),
               if (availableProviders.contains(BuyProviderType.Robinhood))
-              Padding(
-                padding: EdgeInsets.only(top: 24),
-                child: OptionTile(
-                  image: iconRobinhood,
-                  title: "Robinhood Connect",
-                  description: S
-                      .of(context)
-                      .robinhood_option_description,
-                  onPressed: () async =>
-                  await getIt.get<RobinhoodBuyProvider>().launchProvider(
-                      context),
+                Padding(
+                  padding: EdgeInsets.only(top: 24),
+                  child: OptionTile(
+                    image: iconRobinhood,
+                    title: "Robinhood Connect",
+                    description: S.of(context).robinhood_option_description,
+                    onPressed: () async => await getIt
+                        .get<RobinhoodBuyProvider>()
+                        .launchProvider(context),
+                  ),
                 ),
-              ),
               if (availableProviders.contains(BuyProviderType.DFX))
-              Padding(
-                padding: EdgeInsets.only(top: 24),
-                child: OptionTile(
-                  image: iconDFX,
-                  title: "DFX Connect",
-                  description: 'Buy crypto with EUR & CHF. Up to 990€ without additional KYC. For retail and corporate customers in Europe',
-                  onPressed: () async =>
-                  await getIt.get<DFXBuyProvider>().launchProvider(context),
+                Padding(
+                  padding: EdgeInsets.only(top: 24),
+                  child: OptionTile(
+                    image: iconDFX,
+                    title: "DFX Connect",
+                    description: S.of(context).dfx_option_description,
+                    onPressed: () async => await getIt
+                        .get<DFXBuyProvider>()
+                        .launchProvider(context),
+                  ),
                 ),
-              ),
               Spacer(),
               Padding(
                 padding: EdgeInsets.fromLTRB(24, 24, 24, 32),
                 child: Text(
-                  S
-                      .of(context)
-                      .select_buy_provider_notice,
+                  S.of(context).select_buy_provider_notice,
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 14,
                     fontWeight: FontWeight.normal,
-                    color: Theme.of(context).extension<TransactionTradeTheme>()!
+                    color: Theme.of(context)
+                        .extension<TransactionTradeTheme>()!
                         .detailsTitlesColor,
                   ),
                 ),
