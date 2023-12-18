0dcefa850f33d7d511e50dca88e884bf579f1f8b
OmarHatem <omarh.ismail1@gmail.com>
2023-12-18 15:55:16 +0200

    increment build number

--- lib/src/screens/dashboard/dashboard_page.dart
+++ lib/src/screens/dashboard/dashboard_page.dart
@@ -6,9 +6,8 @@ import 'package:cake_wallet/entities/main_actions.dart';
 import 'package:cake_wallet/src/screens/dashboard/desktop_widgets/desktop_sidebar_wrapper.dart';
 import 'package:cake_wallet/src/screens/dashboard/pages/market_place_page.dart';
 import 'package:cake_wallet/src/screens/wallet_connect/widgets/modals/bottom_sheet_listener.dart';
-import 'package:cake_wallet/src/widgets/alert_background.dart';
-import 'package:cake_wallet/src/widgets/alert_close_button.dart';
 import 'package:cake_wallet/src/widgets/gradient_background.dart';
+import 'package:cake_wallet/src/widgets/vulnerable_seeds_popup.dart';
 import 'package:cake_wallet/themes/extensions/sync_indicator_theme.dart';
 import 'package:cake_wallet/utils/device_info.dart';
 import 'package:cake_wallet/utils/version_comparator.dart';
@@ -370,88 +369,7 @@ class _DashboardPageView extends BasePage {
           showPopUp<void>(
             context: context,
             builder: (BuildContext context) {
-              return Stack(
-                alignment: Alignment.center,
-                children: [
-                  AlertBackground(
-                    child: AlertDialog(
-                      insetPadding: EdgeInsets.only(left: 16, right: 16, bottom: 48),
-                      elevation: 0.0,
-                      contentPadding: EdgeInsets.zero,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
-                      content: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30.0),
-                            gradient: LinearGradient(colors: [
-                              Theme.of(context)
-                                  .extension<DashboardPageTheme>()!
-                                  .firstGradientBackgroundColor,
-                              Theme.of(context)
-                                  .extension<DashboardPageTheme>()!
-                                  .secondGradientBackgroundColor,
-                            ], begin: Alignment.centerLeft, end: Alignment.centerRight)),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                          child: Stack(
-                            children: [
-                              SingleChildScrollView(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(top: 16.0),
-                                  child: Container(
-                                    alignment: Alignment.bottomCenter,
-                                    child: DefaultTextStyle(
-                                      style: TextStyle(
-                                        decoration: TextDecoration.none,
-                                        fontSize: 24.0,
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: 'Lato',
-                                        color: Theme.of(context)
-                                            .extension<DashboardPageTheme>()!
-                                            .textColor,
-                                      ),
-                                      child: Text("Emergency Notice"),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SingleChildScrollView(
-                                child: Padding(
-                                  padding: EdgeInsets.only(top: 48, bottom: 16),
-                                  child: Container(
-                                    width: double.maxFinite,
-                                    child: Column(
-                                      children: <Widget>[
-                                        ConstrainedBox(
-                                          constraints: BoxConstraints(
-                                            maxHeight: MediaQuery.of(context).size.height * 0.7,
-                                          ),
-                                          child: Text(
-                                            "Your Bitcoin wallet(s) below use a legacy seed format that is vulnerable, which MAY result in you losing money from these wallet(s) if no action is taken.\nWe recommend that you IMMEDIATELY create wallet(s) in Cake Wallet and immediately transfer the funds to these wallet(s).\nVulnerable wallet name(s):\n\n[${affectedWalletNames.join(", ")}]\n\nFor assistance, please use the in-app support or email support@cakewallet.com",
-                                            style: TextStyle(
-                                              decoration: TextDecoration.none,
-                                              fontSize: 16.0,
-                                              fontFamily: 'Lato',
-                                              color: Theme.of(context)
-                                                  .extension<DashboardPageTheme>()!
-                                                  .textColor,
-                                            ),
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  AlertCloseButton(bottom: 30)
-                ],
-              );
+              return VulnerableSeedsPopup(affectedWalletNames);
             },
           );
         },
