fe2e26f14600b19125a3bce2369fb61a742ceda1
Matthew Fosse <matt@fosse.co>
2024-01-11 15:00:41 -0800

    Minor nano fixes + abstract nano utils out into package (#1250)
    * fixes
    
    * refactors
    
    * fixes
    
    * more fixes
    
    * fixes for monero.com
    
    * update confirmed balance to receivable balance
    
    * fix balance display bug
    
    * remove print statements
    
    * prevent unnecessary writes
    
    * review fixes
    
    * forgot to add pubspec changes
    
    * fix
    
    * fix sync status desyncing on rpc fail
    
    * fix
    
    * update translations + txDetails fixes
    
    * squash balance bug
    
    * add source address on tx info
    
    * fix

--- cw_nano/lib/nano_balance.dart
+++ cw_nano/lib/nano_balance.dart
@@ -1,34 +1,35 @@
 import 'package:cw_core/balance.dart';
-import 'package:cw_nano/nano_util.dart';
+import 'package:nanoutil/nanoutil.dart';
 
 BigInt stringAmountToBigInt(String amount) {
-  return BigInt.parse(NanoUtil.getAmountAsRaw(amount, NanoUtil.rawPerNano));
+  return BigInt.parse(NanoAmounts.getAmountAsRaw(amount, NanoAmounts.rawPerNano));
 }
 
 class NanoBalance extends Balance {
   final BigInt currentBalance;
   final BigInt receivableBalance;
-  late String formattedCurrentBalance;
-  late String formattedReceivableBalance;
 
-  NanoBalance({required this.currentBalance, required this.receivableBalance}) : super(0, 0) {
-    this.formattedCurrentBalance = "";
-    this.formattedReceivableBalance = "";
-  }
+  NanoBalance({required this.currentBalance, required this.receivableBalance}) : super(0, 0);
 
-  NanoBalance.fromString(
-      {required this.formattedCurrentBalance, required this.formattedReceivableBalance})
+  NanoBalance.fromFormattedString(
+      {required String formattedCurrentBalance, required String formattedReceivableBalance})
       : currentBalance = stringAmountToBigInt(formattedCurrentBalance),
         receivableBalance = stringAmountToBigInt(formattedReceivableBalance),
         super(0, 0);
 
+  NanoBalance.fromRawString(
+      {required String currentBalance, required String receivableBalance})
+      : currentBalance = BigInt.parse(currentBalance),
+        receivableBalance = BigInt.parse(receivableBalance),
+        super(0, 0);
+
   @override
   String get formattedAvailableBalance {
-    return NanoUtil.getRawAsUsableString(currentBalance.toString(), NanoUtil.rawPerNano);
+    return NanoAmounts.getRawAsUsableString(currentBalance.toString(), NanoAmounts.rawPerNano);
   }
 
   @override
   String get formattedAdditionalBalance {
-    return NanoUtil.getRawAsUsableString(receivableBalance.toString(), NanoUtil.rawPerNano);
+    return NanoAmounts.getRawAsUsableString(receivableBalance.toString(), NanoAmounts.rawPerNano);
   }
 }
