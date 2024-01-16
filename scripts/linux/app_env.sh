c10e4692b63414c3c8858d932661be52d29ef0aa
OmarHatem <omarh.ismail1@gmail.com>
2024-01-16 02:37:19 +0200

    Update app version [skip ci]

--- scripts/linux/app_env.sh
+++ scripts/linux/app_env.sh
@@ -14,8 +14,8 @@ if [ -n "$1" ]; then
 fi
 
 CAKEWALLET_NAME="Cake Wallet"
-CAKEWALLET_VERSION="1.3.1"
-CAKEWALLET_BUILD_NUMBER=12
+CAKEWALLET_VERSION="1.4.0"
+CAKEWALLET_BUILD_NUMBER=13
 
 if ! [[ " ${TYPES[*]} " =~ " ${APP_LINUX_TYPE} " ]]; then
     echo "Wrong app type."
