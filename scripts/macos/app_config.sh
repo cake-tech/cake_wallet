d8f1cdc7ef06dd1cc70ecf0176902109929345ed
Omar Hatem <omarh.ismail1@gmail.com>
2024-01-16 02:02:10 +0200

    v4.13.0 and v1.10.0 (#1265)
    * New versions
    
    * Minor fix [skip ci]
    
    * Minor UI enhancement for balance card
    
    * Minor UI enhancement
    
    * Add Monero build configs for MacOS [skip ci]
    
    * Add rest of Macos Configs [skip ci]
    
    * Delete empty file
    Save and report restore from backup exceptions [skip ci]
    
    * Minor fixes

--- scripts/macos/app_config.sh
+++ scripts/macos/app_config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+MONERO_COM="monero.com"
 CAKEWALLET="cakewallet"
 DIR=`pwd`
 
@@ -10,6 +11,8 @@ fi
 
 cd ../.. # go to root
 cp -rf ./macos/Runner/InfoBase.plist ./macos/Runner/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName ${APP_MACOS_NAME}" ./macos/Runner/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleName ${APP_MACOS_NAME}" ./macos/Runner/Info.plist
 /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier ${APP_MACOS_BUNDLE_ID}" ./macos/Runner/Info.plist
 /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${APP_MACOS_VERSION}" ./macos/Runner/Info.plist
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${APP_MACOS_BUILD_NUMBER}" ./macos/Runner/Info.plist
@@ -17,11 +20,16 @@ cp -rf ./macos/Runner/InfoBase.plist ./macos/Runner/Info.plist
 # Fill entitlements Bundle ID
 cp -rf ./macos/Runner/DebugProfileBase.entitlements ./macos/Runner/DebugProfile.entitlements
 cp -rf ./macos/Runner/ReleaseBase.entitlements ./macos/Runner/Release.entitlements
+cp -rf ./macos/Runner/Configs/AppInfoBase.xcconfig ./macos/Runner/Configs/AppInfo.xcconfig
 sed -i '' "s/\${BUNDLE_ID}/${APP_MACOS_BUNDLE_ID}/g" ./macos/Runner/DebugProfile.entitlements
 sed -i '' "s/\${BUNDLE_ID}/${APP_MACOS_BUNDLE_ID}/g" ./macos/Runner/Release.entitlements
+sed -i '' "s/\${PRODUCT_NAME}/${APP_MACOS_NAME}/g" ./macos/Runner/Configs/AppInfo.xcconfig
+sed -i '' "s/\${PRODUCT_BUNDLE_IDENTIFIER}/${APP_MACOS_BUNDLE_ID}/g" ./macos/Runner/Configs/AppInfo.xcconfig
 CONFIG_ARGS=""
 
 case $APP_MACOS_TYPE in
+        $MONERO_COM)
+		CONFIG_ARGS="--monero";;
         $CAKEWALLET)
 		CONFIG_ARGS="--monero --bitcoin --ethereum --polygon --nano --bitcoinCash";; #--haven
 esac
@@ -32,3 +40,4 @@ flutter pub run tool/generate_pubspec.dart
 flutter pub get
 flutter packages pub run tool/configure.dart $CONFIG_ARGS
 cd $DIR
+$DIR/app_icon.sh
