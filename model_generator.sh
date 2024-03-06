26b224516b0670910445186945f89fbf0c0504e2
Matthew Fosse <matt@fosse.co>
2024-03-06 10:18:28 -0800

    update build scripts to be more modular and useful in dev

--- model_generator.sh
+++ model_generator.sh
@@ -1,11 +1,32 @@
-cd cw_core && flutter pub get && flutter packages pub run build_runner build --delete-conflicting-outputs && cd ..
-cd cw_evm && flutter pub get && flutter packages pub run build_runner build --delete-conflicting-outputs && cd ..
-cd cw_monero && flutter pub get && flutter packages pub run build_runner build --delete-conflicting-outputs && cd ..
-cd cw_bitcoin && flutter pub get && flutter packages pub run build_runner build --delete-conflicting-outputs && cd ..
-cd cw_haven && flutter pub get && flutter packages pub run build_runner build --delete-conflicting-outputs && cd ..
-cd cw_nano && flutter pub get && flutter packages pub run build_runner build --delete-conflicting-outputs && cd ..
-cd cw_bitcoin_cash && flutter pub get && flutter packages pub run build_runner build --delete-conflicting-outputs && cd ..
-cd cw_solana && flutter pub get && flutter packages pub run build_runner build --delete-conflicting-outputs && cd ..
-cd cw_ethereum && flutter pub get && cd ..
-cd cw_polygon && flutter pub get && cd ..
+CURRENCIES=(
+  "core"
+  "evm"
+  "monero"
+  "bitcoin"
+  "haven"
+  "nano"
+  "bitcoin_cash"
+  "lightning"
+  "solana"
+  "ethereum"
+  "polygon"
+)
+
+CURRENCY=$1
+
+if [ "$CURRENCY" == "all" ]; then
+  for dir in "${CURRENCIES[@]}"; do
+    echo "Processing cw_$dir"
+    cd "cw_$dir" && flutter pub get && flutter packages pub run build_runner build --delete-conflicting-outputs && cd ..
+  done
+else
+  if [[ " ${CURRENCIES[@]} " =~ " $CURRENCY " ]]; then
+    echo "Processing cw_$CURRENCY"
+    cd "cw_$CURRENCY" && flutter pub get && flutter packages pub run build_runner build --delete-conflicting-outputs && cd ..
+  else
+    echo "Invalid currency type. Available options: all, ${CURRENCIES[@]}"
+    exit 1
+  fi
+fi
+
 flutter packages pub run build_runner build --delete-conflicting-outputs
