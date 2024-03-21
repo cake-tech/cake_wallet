9f782956caaf1fb58e4187c9b2844d4daf06d1ba
Matthew Fosse <matt@fosse.co>
2024-03-21 09:18:48 -0700

    just set hard cap on invoice max

--- lib/view_model/lightning_invoice_page_view_model.dart
+++ lib/view_model/lightning_invoice_page_view_model.dart
@@ -84,7 +84,7 @@ abstract class LightningInvoicePageViewModelBase with Store {
         state = FailureState('Amount is too small');
         return;
       }
-      if (maximum != null && maximum != 0 && amountInCrypto > maximum!) {
+      if (amountInCrypto > 4000000) {
         state = FailureState('Amount is too big');
         return;
       }
