4cd1162241c5a7638846baabe0bccf3b5e609a3c
Blazebrain <davidadegoke16@gmail.com>
2024-03-26 08:36:31 +0100

    fix: Fetch read implementation

--- lib/core/secure_storage.dart
+++ lib/core/secure_storage.dart
@@ -1,46 +1,49 @@
 import 'dart:async';
-import 'dart:developer';
-
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
 // For now, we can create a utility function to handle this.
 //
 // However, we could look into abstracting the entire FlutterSecureStorage package
 // so the app doesn't depend on the package directly but an absraction.
 // It'll make these kind of modifications to read/write come from a single point.
+
 Future<String?> readSecureStorage(FlutterSecureStorage secureStorage, String key) async {
   const timeoutDurationInSeconds = 3;
 
-  String? result;
+  Completer<String?> completer = Completer<String?>();
+  Stopwatch stopwatch = Stopwatch();
+  stopwatch.start();
+  bool isRetrying = false;
+  late Timer retryTimer;
 
-  result = await secureStorage.read(key: key);
+  void retry() {
+    if (!isRetrying) {
+      isRetrying = true;
+      retryTimer = Timer.periodic(
+        Duration(milliseconds: 100),
+        (timer) async {
+          String? result = await secureStorage.read(key: key);
 
-  if (result == null) {
-    Stopwatch stopwatch = Stopwatch();
+          if (result != null) {
+            timer.cancel();
+            stopwatch.stop();
+            print('We\'ve gotten a valid response from secure storage');
+            completer.complete(result);
+            isRetrying = false;
+          }
 
-    stopwatch.start();
-
-    await Timer.periodic(
-      Duration(milliseconds: 100),
-      (timer) async {
-        print(timer.tick);
-
-        result = await secureStorage.read(key: key);
-
-        if (result != null) {
-          timer.cancel();
-          stopwatch.stop();
-          log('We\'ve gotten a valid response from secure storage');
-        }
-
-        if (stopwatch.elapsed.inSeconds >= timeoutDurationInSeconds) {
-          timer.cancel();
-          stopwatch.stop();
-          log('Timer cancelled due to timeout.');
-        }
-      },
-    );
+          if (stopwatch.elapsed.inSeconds >= timeoutDurationInSeconds) {
+            timer.cancel();
+            stopwatch.stop();
+            print('Timer cancelled due to timeout.');
+            completer.complete(null);
+            isRetrying = false;
+          }
+        },
+      );
+    }
   }
-  
-  return result;
+
+  retry(); // Start the first attempt
+
+  return completer.future;
 }
