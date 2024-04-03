17e0d7351b023e83c1fdcceda2b6b5313fa14892
Blazebrain <davidadegoke16@gmail.com>
2024-04-03 15:24:08 +0100

    Sync with remote

---
+++ cw_core/lib/exceptions.dart
@@ -0,0 +1,30 @@
+import 'package:cw_core/crypto_currency.dart';
+
+class TransactionWrongBalanceException implements Exception {
+  TransactionWrongBalanceException(this.currency);
+
+  final CryptoCurrency currency;
+}
+
+class TransactionNoInputsException implements Exception {}
+
+class TransactionNoFeeException implements Exception {}
+
+class TransactionNoDustException implements Exception {}
+
+class TransactionNoDustOnChangeException implements Exception {
+  TransactionNoDustOnChangeException(this.max, this.min);
+
+  final String max;
+  final String min;
+}
+
+class TransactionCommitFailed implements Exception {}
+
+class TransactionCommitFailedDustChange implements Exception {}
+
+class TransactionCommitFailedDustOutput implements Exception {}
+
+class TransactionCommitFailedDustOutputSendAll implements Exception {}
+
+class TransactionCommitFailedVoutNegative implements Exception {}
--- lib/view_model/exchange/exchange_view_model.dart
+++ lib/view_model/exchange/exchange_view_model.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:collection';
 import 'dart:convert';
 
+import 'package:bitcoin_base/bitcoin_base.dart';
 import 'package:cake_wallet/bitcoin_cash/bitcoin_cash.dart';
 import 'package:cake_wallet/bitcoin/bitcoin.dart';
 import 'package:cake_wallet/core/wallet_change_listener_view_model.dart';
@@ -9,6 +10,7 @@ import 'package:cake_wallet/entities/exchange_api_mode.dart';
 import 'package:cake_wallet/entities/preferences_key.dart';
 import 'package:cake_wallet/entities/wallet_contact.dart';
 import 'package:cake_wallet/ethereum/ethereum.dart';
+import 'package:cake_wallet/exchange/exchange_provider_description.dart';
 import 'package:cake_wallet/exchange/exchange_template.dart';
 import 'package:cake_wallet/exchange/exchange_trade_state.dart';
 import 'package:cake_wallet/exchange/limits.dart';
@@ -18,6 +20,7 @@ import 'package:cake_wallet/exchange/provider/exchange_provider.dart';
 import 'package:cake_wallet/exchange/provider/exolix_exchange_provider.dart';
 import 'package:cake_wallet/exchange/provider/sideshift_exchange_provider.dart';
 import 'package:cake_wallet/exchange/provider/simpleswap_exchange_provider.dart';
+import 'package:cake_wallet/exchange/provider/thorchain_exchange.provider.dart';
 import 'package:cake_wallet/exchange/provider/trocador_exchange_provider.dart';
 import 'package:cake_wallet/exchange/trade.dart';
 import 'package:cake_wallet/exchange/trade_request.dart';
@@ -96,7 +99,7 @@ abstract class ExchangeViewModelBase extends WalletChangeListenerViewModel with 
 
     /// if the provider is not in the user settings (user's first time or newly added provider)
     /// then use its default value decided by us
-    selectedProviders = ObservableList.of(providersForCurrentPair()
+    selectedProviders = ObservableList.of(providerList
         .where((element) => exchangeProvidersSelection[element.title] == null
             ? element.isEnabled
             : (exchangeProvidersSelection[element.title] as bool))
@@ -148,6 +151,7 @@ abstract class ExchangeViewModelBase extends WalletChangeListenerViewModel with 
         SimpleSwapExchangeProvider(),
         TrocadorExchangeProvider(
             useTorOnly: _useTorOnly, providerStates: _settingsStore.trocadorProviderStates),
+        ThorChainExchangeProvider(tradesStore: trades),
         if (FeatureFlag.isExolixEnabled) ExolixExchangeProvider(),
       ];
 
@@ -466,6 +470,18 @@ abstract class ExchangeViewModelBase extends WalletChangeListenerViewModel with 
 
   @action
   Future<void> createTrade() async {
+    if (isSendAllEnabled) {
+      await calculateDepositAllAmount();
+      final amount = double.tryParse(depositAmount);
+
+      if (limits.min != null && amount != null && amount < limits.min!) {
+        tradeState = TradeIsCreatedFailure(
+            title: S.current.trade_not_created,
+            error: S.current.amount_is_below_minimum_limit(limits.min!.toString()));
+        return;
+      }
+    }
+
     try {
       for (var provider in _sortedAvailableProviders.values) {
         if (!(await provider.checkIsAvailable())) continue;
@@ -492,12 +508,23 @@ abstract class ExchangeViewModelBase extends WalletChangeListenerViewModel with 
           else {
             try {
               tradeState = TradeIsCreating();
-              final trade =
-                  await provider.createTrade(request: request, isFixedRateMode: isFixedRateMode);
+              final trade = await provider.createTrade(
+                request: request,
+                isFixedRateMode: isFixedRateMode,
+                isSendAll: isSendAllEnabled,
+              );
               trade.walletId = wallet.id;
               trade.fromWalletAddress = wallet.walletAddresses.address;
+
+              if (!isCanCreateTrade(trade)) {
+                tradeState = TradeIsCreatedFailure(
+                    title: S.current.trade_not_created,
+                    error: S.current.thorchain_taproot_address_not_supported);
+                return;
+              }
+
               tradesStore.setTrade(trade);
-              await trades.add(trade);
+              if (trade.provider != ExchangeProviderDescription.thorChain) await trades.add(trade);
               tradeState = TradeIsCreatedSuccessfully(trade: trade);
 
               /// return after the first successful trade
@@ -539,25 +566,24 @@ abstract class ExchangeViewModelBase extends WalletChangeListenerViewModel with 
   @action
   void enableSendAllAmount() {
     isSendAllEnabled = true;
+    isFixedRateMode = false;
     calculateDepositAllAmount();
   }
 
   @action
+  void enableFixedRateMode() {
+    isSendAllEnabled = false;
+    isFixedRateMode = true;
+  }
+
+  @action
   Future<void> calculateDepositAllAmount() async {
-    if (wallet.type == WalletType.litecoin || wallet.type == WalletType.bitcoinCash) {
-      final availableBalance = wallet.balance[wallet.currency]!.available;
-      final priority = _settingsStore.priority[wallet.type]!;
-      final fee = wallet.calculateEstimatedFee(priority, null);
-
-      if (availableBalance < fee || availableBalance == 0) return;
-
-      final amount = availableBalance - fee;
-      changeDepositAmount(amount: bitcoin!.formatterBitcoinAmountToString(amount: amount));
-    } else if (wallet.type == WalletType.bitcoin) {
+    if (wallet.type == WalletType.litecoin ||
+        wallet.type == WalletType.bitcoin ||
+        wallet.type == WalletType.bitcoinCash) {
       final priority = _settingsStore.priority[wallet.type]!;
 
-      final amount = await bitcoin!.estimateFakeSendAllTxAmount(
-          wallet, bitcoin!.deserializeBitcoinTransactionPriority(priority.raw));
+      final amount = await bitcoin!.estimateFakeSendAllTxAmount(wallet, priority);
 
       changeDepositAmount(amount: bitcoin!.formatterBitcoinAmountToString(amount: amount));
     }
@@ -749,4 +775,17 @@ abstract class ExchangeViewModelBase extends WalletChangeListenerViewModel with 
   int get depositMaxDigits => depositCurrency.decimals;
 
   int get receiveMaxDigits => receiveCurrency.decimals;
+
+  bool isCanCreateTrade(Trade trade) {
+    if (trade.provider == ExchangeProviderDescription.thorChain) {
+      final payoutAddress = trade.payoutAddress ?? '';
+      final fromWalletAddress = trade.fromWalletAddress ?? '';
+      final tapRootPattern = RegExp(P2trAddress.regex.pattern);
+
+      if (tapRootPattern.hasMatch(payoutAddress) || tapRootPattern.hasMatch(fromWalletAddress)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
