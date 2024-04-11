8004d07d8bcc97850edff5761ebd80695dc39997
Matthew Fosse <matt@fosse.co>
2024-04-11 16:27:46 -0700

    more minor code cleanup

--- cw_core/lib/n2_node.dart
+++ cw_core/lib/n2_node.dart
@@ -1,7 +1,3 @@
-double? _toDouble(num v) {
-  return double.tryParse(v.toString());
-}
-
 class N2Node {
   N2Node({
     this.weight,
@@ -17,20 +13,19 @@ class N2Node {
   String? account;
   String? alias;
 
+  factory N2Node.fromJson(Map<String, dynamic> json) => N2Node(
+        weight: double.tryParse((json['weight'] as num).toString()),
+        uptime: json['uptime'] as String?,
+        score: json['score'] as int?,
+        account: json['rep_address'] as String?,
+        alias: json['alias'] as String?,
+      );
 
-  Nfactory N2Node.fromJson(Map<String, dynamic> json) => N2Node(
-    weight: _toDouble(json['weight'] as num),
-    uptime: json['uptime'] as String?,
-    score: json['score'] as int?,
-    account: json['rep_address'] as String?,
-    alias: json['alias'] as String?,
-  );
-  
   Map<String, dynamic> toJson() => <String, dynamic>{
-    'uptime': instance.uptime,
-    'weight': instance.weight,
-    'score': instance.score,
-    'rep_address': instance.account,
-    'alias': instance.alias,
-  };
+        'uptime': uptime,
+        'weight': weight,
+        'score': score,
+        'rep_address': account,
+        'alias': alias,
+      };
 }
