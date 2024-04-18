6a93877bbbf5bddd3aaf21b55cfebf08532c337c
Matthew Fosse <matt@fosse.co>
2024-04-18 12:15:16 -0700

    remove comment and change duration from 2 to 0

--- lib/entities/biometric_auth.dart
+++ lib/entities/biometric_auth.dart
@@ -18,8 +18,7 @@ class BiometricAuth {
     bool canAuthenticate;
     try {
       canAuthenticate = await _flutterLocalAuthenticationPlugin.canAuthenticate();
-      // Setup TouchID Allowable Reuse duration
-      await _flutterLocalAuthenticationPlugin.setTouchIDAuthenticationAllowableReuseDuration(2);
+      await _flutterLocalAuthenticationPlugin.setTouchIDAuthenticationAllowableReuseDuration(0);
     } catch (error) {
       print("Exception checking support. $error");
       canAuthenticate = false;
