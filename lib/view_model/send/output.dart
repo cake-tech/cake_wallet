7d8ff6378a2d2752c33f73059566a790042b3725
Serhii <borodenko.sv@gmail.com>
2024-04-22 17:40:46 +0300

    Update output.dart

--- lib/view_model/send/output.dart
+++ lib/view_model/send/output.dart
@@ -126,8 +126,8 @@ abstract class OutputBase with Store {
 
       if (_wallet.type == WalletType.bitcoin) {
         if (_settingsStore.priority[_wallet.type] == bitcoin!.getBitcoinTransactionPriorityCustom()) {
-          fee = bitcoin!.getFeeAmountWithFeeRate(
-              _settingsStore.customBitcoinFeeRate, formattedCryptoAmount, 1, 1);
+          fee = bitcoin!.getFeeAmountWithFeeRate(_wallet,
+              _settingsStore.customBitcoinFeeRate, 1, 0);
         }
 
         return bitcoin!.formatterBitcoinAmountToDouble(amount: fee);
