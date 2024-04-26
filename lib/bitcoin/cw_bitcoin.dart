7fcf48f91da3d82121704bf25c530549a24c9322
Serhii <borodenko.sv@gmail.com>
2024-04-26 19:18:26 +0300

    Fix estimated fee calculation for customs fee rate (#1406)
    * Update output.dart
    
    * fix estimated fee calculation
    
    * Update bitcoin_transaction_priority.dart

--- lib/bitcoin/cw_bitcoin.dart
+++ lib/bitcoin/cw_bitcoin.dart
@@ -282,13 +282,14 @@ class CWBitcoin extends Bitcoin {
   }
 
   @override
-  int getFeeAmountWithFeeRate(Object wallet, int feeRate, int inputsCount, int outputsCount,
-      {int? size}) {
+  int getEstimatedFeeWithFeeRate(Object wallet, int feeRate, int? amount,
+      {int? outputsCount, int? size}) {
     final bitcoinWallet = wallet as ElectrumWallet;
-    return bitcoinWallet.feeAmountWithFeeRate(
+    return bitcoinWallet.calculateEstimatedFeeWithFeeRate(
       feeRate,
-      inputsCount,
-      outputsCount,
+      amount,
+      outputsCount: outputsCount,
+      size: size,
     );
   }
 
