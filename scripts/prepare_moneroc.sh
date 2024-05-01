a0fd73ac37166439cfd1a8bc746ffc8fad4ce2b0
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-05-01 17:47:11 +0200

    add android build script

---
+++ scripts/prepare_moneroc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -x -e
+
+cd "$(dirname "$0")"
+
+
+
+if [[ ! -d "monero_c" ]];
+then
+    git clone https://github.com/mrcyjanek/monero_c --branch rewrite-wip
+    cd monero_c
+    git checkout cd90f3bcd0349759030751ec7ce84eec6ee80c43
+    git reset --hard
+    git submodule update --init --force --recursive
+    ./apply_patches.sh monero
+    ./apply_patches.sh wownero
+else
+    cd monero_c
+fi
+
+if [[ ! -f "monero/.patch-applied" ]];
+then
+    ./apply_patches.sh monero
+fi
+
+if [[ ! -f "wownero/.patch-applied" ]];
+then
+    ./apply_patches.sh wownero
+fi
+cd ..
+
+echo "monero_c source prepared".
