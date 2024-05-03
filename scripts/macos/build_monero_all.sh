ac30e14352d9b235f81673bb1c3e02ba855a4515
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-05-04 00:26:08 +0200

    monero build script

--- scripts/macos/build_monero_all.sh
+++ scripts/macos/build_monero_all.sh
@@ -1,20 +1,30 @@
 #!/bin/sh
+set -x -e
 
-ARCH=`uname -m`
+cd "$(dirname "$0")"
 
-. ./config.sh
+NPROC="-j$(nproc)"
 
-case $ARCH in
-	arm64)
-		./build_openssl_arm64.sh
-		./build_boost_arm64.sh;;
-	x86_64)
-		./build_openssl_x86_64.sh
-		./build_boost_x86_64.sh;;
-esac
+../prepare_moneroc.sh
 
-./build_zmq.sh
-./build_expat.sh
-./build_unbound.sh
-./build_sodium.sh
-./build_monero.sh
+# NOTE: -j1 is intentional. Otherwise you will run into weird behaviour on macos
+if [[ ! "x$USE_DOCKER" == "x" ]];
+then
+    for COIN in monero;
+    do
+        pushd ../monero_c
+            echo "unsupported!"
+            exit 1
+        popd
+    done
+else
+    for COIN in monero;
+    do
+        pushd ../monero_c
+            ./build_single.sh ${COIN} host-apple-darwin $NPROC
+        popd
+    done
+fi
+
+unxz -f ../monero_c/release/monero/host-apple-darwin_libwallet2_api_c.dylib.xz
+# unxz -f ../monero_c/release/wownero/host-apple-darwin_libwallet2_api_c.dylib.xz
