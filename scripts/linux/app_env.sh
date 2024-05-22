556b79bdcdc78467b84a0473725fe030b8a2bdb4
OmarHatem <omarh.ismail1@gmail.com>
2024-05-22 04:22:01 +0300

    Add Tron Update Linux version

--- scripts/linux/app_env.sh
+++ scripts/linux/app_env.sh
@@ -14,8 +14,8 @@ if [ -n "$1" ]; then
 fi
 
 CAKEWALLET_NAME="Cake Wallet"
-CAKEWALLET_VERSION="1.7.2"
-CAKEWALLET_BUILD_NUMBER=27
+CAKEWALLET_VERSION="1.8.0"
+CAKEWALLET_BUILD_NUMBER=28
 
 if ! [[ " ${TYPES[*]} " =~ " ${APP_LINUX_TYPE} " ]]; then
     echo "Wrong app type."
