26510736f59a9782f7ee1e0c0d336ba7d8d5b15a
Matthew Fosse <matt@fosse.co>
2024-05-24 11:00:43 -0700

    working

--- cw_mweb/ios/Classes/CwMwebPlugin.swift
+++ cw_mweb/ios/Classes/CwMwebPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import Mwebd
 
 public class CwMwebPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -8,10 +9,42 @@ public class CwMwebPlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  private static var server: MwebdServer?
+  private static var port: Int = 0
+    
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
+      switch call.method {
+      case "getPlatformVersion":
+          result("iOS " + UIDevice.current.systemVersion)
+      case "start":
+          let args = call.arguments as? [String: String]
+          print("args: \(args)")
+          let dataDir = args?["dataDir"]
+          var error: NSError?
+          
+          if CwMwebPlugin.server == nil {
+              CwMwebPlugin.server = MwebdNewServer("", dataDir, "", &error)
+              
+              if let server = CwMwebPlugin.server {
+                  do {
+                      print("starting server \(CwMwebPlugin.port)")
+                      try server.start(0, ret0_: &CwMwebPlugin.port)
+                      result(CwMwebPlugin.port)
+                  } catch let startError as NSError {
+                      result(FlutterError(code: "Server Start Error", message: startError.localizedDescription, details: nil))
+                  }
+              } else if let error = error {
+                  result(FlutterError(code: "Server Creation Error", message: error.localizedDescription, details: nil))
+              } else {
+                  result(FlutterError(code: "Unknown Error", message: "Failed to create server", details: nil))
+              }
+          } else {
+//              result(FlutterError(code: "Server Already Running", message: "The server is already running", details: nil))
+              result(CwMwebPlugin.port)
+          }
+      
+      
+      // result(0)
     default:
       result(FlutterMethodNotImplemented)
     }
