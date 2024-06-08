b3e0c0b734d11000ad943e899b40923dd9690b2f
Matthew Fosse <matt@fosse.co>
2024-06-08 15:00:26 +0200

    save [skip ci]

--- scripts/android/build_mwebd.sh
+++ scripts/android/build_mwebd.sh
@@ -1,7 +1,6 @@
 git clone https://github.com/ltcmweb/mwebd
 cd mwebd
 go install github.com/ltcmweb/mwebd/cmd/mwebd@latest
-gomobile bind -target=android -androidapi 19 github.com/ltcmweb/mwebd
-
+gomobile bind -target=android -androidapi 21 github.com/ltcmweb/mwebd
 mkdir -p ../../../cw_mweb/android/libs/
 mv ./mwebd.aar $_
