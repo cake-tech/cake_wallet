5b3161fb2958b6dd0fae0249a35b3979999367f3
Konstantin Ullrich <konstantinullrich12@gmail.com>
2024-06-18 02:00:07 -0400

    Add Recovery Height to Wallet seed page for monero (#1470)

--- tool/configure.dart
+++ tool/configure.dart
@@ -363,6 +363,7 @@ abstract class Monero {
   WalletCredentials createMoneroRestoreWalletFromSeedCredentials({required String name, required String password, required int height, required String mnemonic});
   WalletCredentials createMoneroNewWalletCredentials({required String name, required String language, required bool isPolyseed, String password});
   Map<String, String> getKeys(Object wallet);
+  int? getRestoreHeight(Object wallet);
   Object createMoneroTransactionCreationCredentials({required List<Output> outputs, required TransactionPriority priority});
   Object createMoneroTransactionCreationCredentialsRaw({required List<OutputInfo> outputs, required TransactionPriority priority});
   String formatterMoneroAmountToString({required int amount});
