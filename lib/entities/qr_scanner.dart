415e1aa2120212cc677ab11ee5a16e001ea5fc71
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-07-16 23:08:40 +0200

    replace qr scanner with fast_scanner

--- lib/entities/qr_scanner.dart
+++ lib/entities/qr_scanner.dart
@@ -1,15 +1,60 @@
-import 'package:barcode_scan2/barcode_scan2.dart';
+import 'package:fast_scanner/fast_scanner.dart';
+import 'package:flutter/material.dart';
 
 var isQrScannerShown = false;
 
-Future<String> presentQRScanner() async {
+Future<String> presentQRScanner(BuildContext context) async {
   isQrScannerShown = true;
   try {
-    final result = await BarcodeScanner.scan();
+    final result = await Navigator.of(context).push<Barcode>(
+      MaterialPageRoute(
+        builder:(context) {
+          return BarcodeScannerSimple();
+        },
+      ),
+    );
     isQrScannerShown = false;
-    return result.rawContent.trim();
+    return (result?.rawValue??'').trim();
   } catch (e) {
     isQrScannerShown = false;
     rethrow;
+  }
+}
+
+// https://github.com/MrCyjaneK/fast_scanner/blob/master/example/lib/barcode_scanner_simple.dart
+class BarcodeScannerSimple extends StatefulWidget {
+  const BarcodeScannerSimple({super.key});
+
+  @override
+  State<BarcodeScannerSimple> createState() => _BarcodeScannerSimpleState();
+}
+
+class _BarcodeScannerSimpleState extends State<BarcodeScannerSimple> {
+  Barcode? _barcode;
+
+  void _handleBarcode(BarcodeCapture barcodes) {
+    if (mounted) {
+      setState(() {
+        _barcode = barcodes.barcodes.firstOrNull;
+      });
+      if (_barcode != null) {
+        Navigator.of(context).pop(_barcode);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Scan')),
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          MobileScanner(
+            onDetect: _handleBarcode,
+          ),
+        ],
+      ),
+    );
   }
 }
