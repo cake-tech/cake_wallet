3ec61d8baad944c5dab84c87680083595ae0e49b
Blazebrain <davidadegoke16@gmail.com>
2024-08-09 08:12:25 +0100

    fix: Disable send all for solana wallets

--- lib/view_model/send/send_view_model.dart
+++ lib/view_model/send/send_view_model.dart
@@ -118,7 +118,7 @@ abstract class SendViewModelBase extends WalletChangeListenerViewModel with Stor
   @computed
   bool get isBatchSending => outputs.length > 1;
 
-  bool get shouldDisplaySendALL => walletType != WalletType.solana || walletType != WalletType.tron;
+  bool get shouldDisplaySendALL => walletType != WalletType.solana;
 
   @computed
   String get pendingTransactionFiatAmount {
