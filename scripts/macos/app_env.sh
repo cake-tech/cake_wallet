19c600ba10c4d72c984388073b0820e362c12bc6
OmarHatem <omarh.ismail1@gmail.com>
2024-08-18 17:15:23 +0300

    update macos build number minor fixes

--- scripts/macos/app_env.sh
+++ scripts/macos/app_env.sh
@@ -22,7 +22,7 @@ MONERO_COM_BUNDLE_ID="com.cakewallet.monero"
 
 CAKEWALLET_NAME="Cake Wallet"
 CAKEWALLET_VERSION="1.12.4"
-CAKEWALLET_BUILD_NUMBER=84
+CAKEWALLET_BUILD_NUMBER=85
 CAKEWALLET_BUNDLE_ID="com.fotolockr.cakewallet"
 
 if ! [[ " ${TYPES[*]} " =~ " ${APP_MACOS_TYPE} " ]]; then
