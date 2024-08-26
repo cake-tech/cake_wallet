f8ede25edf32fdba03bc0f696b8a8bae9a264f16
Tay8NWWFKpz9JT4NXU0w <andysmith1367596@proton.me>
2024-08-26 16:20:57 +0000

    Bump Haven version to 4.1.0

--- scripts/docker/build_haven.sh
+++ scripts/docker/build_haven.sh
@@ -2,7 +2,7 @@
 set -x -e
 
 . ./config.sh
-HAVEN_VERSION=tags/v4.0.2
+HAVEN_VERSION=tags/v4.1.0
 HAVEN_SRC_DIR=${WORKDIR}/haven
 
 git clone https://github.com/haven-protocol-org/haven-main.git ${HAVEN_SRC_DIR}
