8d0f57e98d4572a057196a5b68b2d16188b175c8
Matthew Fosse <matt@fosse.co>
2024-09-17 21:06:10 -0700

    re-add support button

--- lib/src/screens/dashboard/widgets/menu_widget.dart
+++ lib/src/screens/dashboard/widgets/menu_widget.dart
@@ -97,12 +97,11 @@ class MenuWidgetState extends State<MenuWidget> {
 
   @override
   Widget build(BuildContext context) {
-    int itemCount = SettingActions.all.length;
     List<SettingActions> items = SettingActions.all;
     if (!widget.dashboardViewModel.hasSilentPayments) {
-      itemCount--;
       items.removeWhere((element) => element.name(context) == S.of(context).silent_payments_settings);
     }
+    int itemCount = items.length;
     // if (!widget.dashboardViewModel.hasMweb) {
     //   itemCount--;
     //   items.removeWhere((element) => element.name(context) == S.of(context).litecoin_mweb_settings);
