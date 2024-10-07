c59692d1cee7ea79f67325ce246d744736e38d81
Matthew Fosse <matt@fosse.co>
2024-10-07 12:20:16 -0700

    additional logging and minor fixes

--- scripts/ios/build_mwebd.sh
+++ scripts/ios/build_mwebd.sh
@@ -12,7 +12,7 @@ fi
 # build mwebd:
 git clone https://github.com/ltcmweb/mwebd
 cd mwebd
-git reset --hard f6ea8a9e3d348b01bb44f03a1cc4ad65b0abe935
+git reset --hard c880e276e4b06645ad8427972e8a788fe2fd0557
 gomobile bind -target=ios .
 mv -fn ./Mwebd.xcframework ../../../ios/
 # cleanup:
