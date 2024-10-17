93dde0bab11953ff115afb355633df762a9b3691
cyan <cyjan@mrcyjanek.net>
2024-10-17 11:06:57 +0000

    revert local change

--- scripts/android/pubspec_gen.sh
+++ scripts/android/pubspec_gen.sh
@@ -19,7 +19,6 @@ case $APP_ANDROID_TYPE in
                 CONFIG_ARGS="--haven"
                 ;;
 esac
-CONFIG_ARGS="--monero --ethereum --polygon --nano --solana --tron --wownero"
 
 cd ../..
 cp -rf pubspec_description.yaml pubspec.yaml
