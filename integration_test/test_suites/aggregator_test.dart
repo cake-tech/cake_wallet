7ee60f2a13e31941ebd107af6464863bd54b181d
Blazebrain <davidadegoke16@gmail.com>
2024-11-06 15:42:06 +0100

    test: Another trial at getting combined tests running

--- integration_test/test_suites/aggregator_test.dart
+++ integration_test/test_suites/aggregator_test.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
 import 'send_flow_test.dart' as send_flow_test;
 import 'exchange_flow_test.dart' as exchange_flow_test;
 import 'create_wallet_flow_test.dart' as create_wallet_flow_test;
@@ -6,6 +9,13 @@ import 'transaction_history_flow_test.dart' as transaction_history_flow_test;
 import 'restore_wallet_through_seeds_flow_test.dart' as restore_wallet_through_seeds_flow_test;
 
 void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+  });
+
   send_flow_test.main();
   exchange_flow_test.main();
   restore_wallet_through_seeds_flow_test.main();
