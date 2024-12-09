315a1c2edac8773818f5e98ce8bb548f8012be71
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-12-09 11:27:56 -0500

    update monero_c hash

--- scripts/prepare_moneroc.sh
+++ scripts/prepare_moneroc.sh
@@ -8,7 +8,7 @@ if [[ ! -d "monero_c" ]];
 then
     git clone https://github.com/mrcyjanek/monero_c --branch master
     cd monero_c
-    git checkout c41c4dad9aa5003a914cfb2c528c76386f952665
+    git checkout aadd5acb1a6e4888733446f1e66dc1e53ee0f7de
     git reset --hard
     git submodule update --init --force --recursive
     ./apply_patches.sh monero
