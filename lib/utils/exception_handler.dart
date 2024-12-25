2fc7d40e53410db337414935365df33b29dca921
OmarHatem <omarh.ismail1@gmail.com>
2024-12-25 04:53:17 +0200

    potential fix for transactions not cleared correctly [skip ci]

--- lib/utils/exception_handler.dart
+++ lib/utils/exception_handler.dart
@@ -221,7 +221,7 @@ class ExceptionHandler {
     // just ignoring until we find a solution to this issue or migrate from flutter secure storage
     "core/auth_service.dart:64",
     "core/key_service.dart:14",
-    "core/wallet_loading_service.dart:133",
+    "core/wallet_loading_service.dart:131",
   ];
 
   static Future<void> _addDeviceInfo(File file) async {
