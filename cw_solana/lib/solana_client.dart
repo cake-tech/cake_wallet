1367588bed0913a8a029798a62041c99585650f8
OmarHatem <omarh.ismail1@gmail.com>
2024-12-27 19:11:33 +0200

    revert associated recipient account removal

--- cw_solana/lib/solana_client.dart
+++ cw_solana/lib/solana_client.dart
@@ -502,22 +502,22 @@ class SolanaWalletClient {
       throw SolanaNoAssociatedTokenAccountException(ownerKeypair.address, mint.toBase58());
     }
 
-    // try {
-    //   associatedRecipientAccount ??= await _client!.createAssociatedTokenAccount(
-    //     mint: mint,
-    //     owner: destinationOwner,
-    //     funder: ownerKeypair,
-    //   );
-    // } catch (e) {
-    //   throw SolanaCreateAssociatedTokenAccountException(e.toString());
-    // }
+    try {
+      associatedRecipientAccount ??= await _client!.createAssociatedTokenAccount(
+        mint: mint,
+        owner: destinationOwner,
+        funder: ownerKeypair,
+      );
+    } catch (e) {
+      throw SolanaCreateAssociatedTokenAccountException(e.toString());
+    }
 
     // Input by the user
     final amount = (inputAmount * math.pow(10, tokenDecimals)).toInt();
 
     final instruction = TokenInstruction.transfer(
       source: Ed25519HDPublicKey.fromBase58(associatedSenderAccount.pubkey),
-      destination: destinationOwner,
+      destination: Ed25519HDPublicKey.fromBase58(associatedRecipientAccount.pubkey),
       owner: ownerKeypair.publicKey,
       amount: amount,
     );
