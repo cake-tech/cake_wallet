ef0054b057726cde7ca954e502b615451a73ab1b
Serhii <borodenko.sv@gmail.com>
2024-12-31 00:55:38 +0200

    fix:node QR Code URI parsing

--- lib/view_model/node_list/node_create_or_edit_view_model.dart
+++ lib/view_model/node_list/node_create_or_edit_view_model.dart
@@ -219,28 +219,21 @@ abstract class NodeCreateOrEditViewModelBase with Store {
         throw Exception('Unexpected scan QR code value: value is empty');
       }
 
+      if (!code.contains('://')) {
+        code = 'tcp://$code';
+      }
+
       final uri = Uri.tryParse(code);
-
-      if (uri == null) {
-        throw Exception('Unexpected scan QR code value: Value is invalid');
+      if (uri == null || uri.host.isEmpty) {
+        throw Exception('Invalid QR code: Unable to parse or missing host.');
       }
 
-      final userInfo = uri.userInfo.split(':');
-
-      if (userInfo.length < 2) {
-        throw Exception('Unexpected scan QR code value: Value is invalid');
-      }
-
-      final rpcUser = userInfo[0];
-      final rpcPassword = userInfo[1];
       final ipAddress = uri.host;
-      final port = uri.port.toString();
+      final port = uri.hasPort ? uri.port.toString() : '';
       final path = uri.path;
 
       setAddress(ipAddress);
       setPath(path);
-      setPassword(rpcPassword);
-      setLogin(rpcUser);
       setPort(port);
     } on Exception catch (e) {
       connectionState = FailureState(e.toString());
