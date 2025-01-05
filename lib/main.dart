ad1f91cd51e9d72c3d0d41124f6167668a031bb6
OmarHatem <omarh.ismail1@gmail.com>
2025-01-05 05:06:46 +0200

    fix initial migration version

--- lib/main.dart
+++ lib/main.dart
@@ -215,7 +215,7 @@ Future<void> initializeAppConfigs() async {
     secureStorage: secureStorage,
     anonpayInvoiceInfo: anonpayInvoiceInfo,
     havenSeedStore: havenSeedStore,
-    initialMigrationVersion: 46,
+    initialMigrationVersion: 47,
   );
 }
 
