3ca0334c0fe9488b0e616f5e0c351253da63b1e4
Blazebrain <davidadegoke16@gmail.com>
2025-01-16 14:38:05 +0100

    chore: Try switching test commands

--- integration_test_runner.sh
+++ integration_test_runner.sh
@@ -18,9 +18,7 @@ do
         rm -rf ~/.local/share/com.example.cake_wallet ~/Documents/cake_wallet
     fi
     echo "Running test: $target"
-    if flutter drive \
-      --driver=test_driver/integration_test.dart \
-      --target="$target"; then
+    if flutter test $target; then
         echo "✅ Test passed: $target"
         passed_tests+=("$target")
     else
