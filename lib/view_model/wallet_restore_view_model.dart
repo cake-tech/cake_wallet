f0c7e4a7c38898db73fc7e57e872757fbad250e7
OmarHatem <omarh.ismail1@gmail.com>
2025-01-21 16:56:19 +0200

    minor merge leftover [skip ci]

--- lib/view_model/wallet_restore_view_model.dart
+++ lib/view_model/wallet_restore_view_model.dart
@@ -80,8 +80,6 @@ abstract class WalletRestoreViewModelBase extends WalletCreationVM with Store {
   final bool hasBlockchainHeightLanguageSelector;
   final bool hasRestoreFromPrivateKey;
 
-  bool get hasPassphrase => [WalletType.bitcoin, WalletType.litecoin, WalletType.bitcoinCash].contains(type);
-
   @observable
   WalletRestoreMode mode;
 
