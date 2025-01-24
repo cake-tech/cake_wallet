199ada3fa9dbc6c5e6da4f7c7f2c8cfef9692674
Matthew Fosse <matt@fosse.co>
2025-01-24 10:09:48 -0800

    Mweb fixes (#1972)
    * quick fix [still investigating]
    
    * cleaner quick fix
    
    * cleaner quick fix
    
    * log fix
    
    * save to downloads would require an additional permission
    
    * change address handling updates [skip ci]
    
    * should change address not updating
    
    * change address handling updates
    
    * remove unnecessary code + don't reset change address index to 0

--- cw_bitcoin/lib/bitcoin_wallet_addresses.dart
+++ cw_bitcoin/lib/bitcoin_wallet_addresses.dart
@@ -2,6 +2,7 @@ import 'package:bitcoin_base/bitcoin_base.dart';
 import 'package:blockchain_utils/bip/bip/bip32/bip32.dart';
 import 'package:cw_bitcoin/electrum_wallet_addresses.dart';
 import 'package:cw_bitcoin/utils.dart';
+import 'package:cw_core/unspent_coin_type.dart';
 import 'package:cw_core/wallet_info.dart';
 import 'package:mobx/mobx.dart';
 
@@ -26,7 +27,10 @@ abstract class BitcoinWalletAddressesBase extends ElectrumWalletAddresses with S
 
   @override
   String getAddress(
-      {required int index, required Bip32Slip10Secp256k1 hd, BitcoinAddressType? addressType}) {
+      {required int index,
+      required Bip32Slip10Secp256k1 hd,
+      BitcoinAddressType? addressType,
+      UnspentCoinType coinTypeToSpendFrom = UnspentCoinType.any}) {
     if (addressType == P2pkhAddressType.p2pkh)
       return generateP2PKHAddress(hd: hd, index: index, network: network);
 
