e4fe1f1faa4b417d502d9b38fc342a47aa74ec99
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-02-03 11:40:29 +0100

    add tor proxy support to cw_evm, cw_tron and cw_polygon [run tests]

--- cw_ethereum/lib/ethereum_client.dart
+++ cw_ethereum/lib/ethereum_client.dart
@@ -19,7 +19,7 @@ class EthereumClient extends EVMChainClient {
   Future<List<EVMChainTransactionModel>> fetchTransactions(String address,
       {String? contractAddress}) async {
     try {
-      final response = await httpClient.get(Uri.https("api.etherscan.io", "/api", {
+      final response = await client.get(Uri.https("api.etherscan.io", "/api", {
         "module": "account",
         "action": contractAddress != null ? "tokentx" : "txlist",
         if (contractAddress != null) "contractaddress": contractAddress,
@@ -50,7 +50,7 @@ class EthereumClient extends EVMChainClient {
   @override
   Future<List<EVMChainTransactionModel>> fetchInternalTransactions(String address) async {
     try {
-      final response = await httpClient.get(Uri.https("api.etherscan.io", "/api", {
+      final response = await client.get(Uri.https("api.etherscan.io", "/api", {
         "module": "account",
         "action": "txlistinternal",
         "address": address,
