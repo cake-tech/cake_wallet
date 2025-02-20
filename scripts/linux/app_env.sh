e8fdf33a79a858f4f79c292d9ffd36e6b5e8c7b1
Omar Hatem <omarh.ismail1@gmail.com>
2025-02-20 20:24:59 +0200

    v4.23.2 Release Candidate (#2039)
    * v4.23.2 Release Candidate
    
    * adjust bottom nav bar height
    
    * Adjust gradient and padding
    
    * fix blur

--- scripts/linux/app_env.sh
+++ scripts/linux/app_env.sh
@@ -14,8 +14,8 @@ if [ -n "$1" ]; then
 fi
 
 CAKEWALLET_NAME="Cake Wallet"
-CAKEWALLET_VERSION="1.13.0"
-CAKEWALLET_BUILD_NUMBER=46
+CAKEWALLET_VERSION="1.13.2"
+CAKEWALLET_BUILD_NUMBER=47
 
 if ! [[ " ${TYPES[*]} " =~ " ${APP_LINUX_TYPE} " ]]; then
     echo "Wrong app type."
