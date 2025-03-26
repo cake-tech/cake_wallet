2f0cb28fb204229a847c5fdcac98a0c34edbd02d
cyan <cyjan@mrcyjanek.net>
2025-03-26 02:56:24 +0100

    properly await multDest transactino so it won't freeze the UI (#2116)

--- cw_monero/lib/monero_wallet.dart
+++ cw_monero/lib/monero_wallet.dart
@@ -402,6 +402,7 @@ abstract class MoneroWalletBase extends WalletBase<MoneroBalance,
               outputs: moneroOutputs,
               priorityRaw: _credentials.priority.serialize(),
               accountIndex: walletAddresses.account!.id,
+              paymentId: "",
               preferredInputs: inputs);
     } else {
       final output = outputs.first;
