705bf76a4bfa4712c2166b175c54fe137a4bc3f2
Konstantin Ullrich <konstantinullrich12@gmail.com>
2025-03-28 12:08:26 +0100

    bug: fix wrong naming in CI

--- cw_monero/test/utils/setup_monero_c.dart
+++ cw_monero/test/utils/setup_monero_c.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 File getMoneroCBinary() {
   if (Platform.isWindows)
     return File(
-        '../scripts/monero_c/release/monero/x86_64-w64-mingw32-monero_libwallet2_api_c.dll');
+        '../scripts/monero_c/release/monero/x86_64-w64-mingw32_libwallet2_api_c.dll');
   if (Platform.isMacOS) return File('../macos/monero_libwallet2_api_c.dylib');
-  return File('../scripts/monero_c/release/monero/x86_64-linux-gnu-monero_libwallet2_api_c.so');
+  return File('../scripts/monero_c/release/monero/x86_64-linux-gnu_libwallet2_api_c.so');
 }
 
 String get moneroCBinaryName {
