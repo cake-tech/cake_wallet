c4fb2c43557963819a43a66368dade3d56d0141a
OmarHatem <omarh.ismail1@gmail.com>
2025-03-31 05:04:02 +0200

    change UI of potential scam text

--- lib/src/screens/dashboard/pages/balance/balance_row_widget.dart
+++ lib/src/screens/dashboard/pages/balance/balance_row_widget.dart
@@ -137,17 +137,6 @@ class BalanceRowWidget extends StatelessWidget {
                                             .extension<BalancePageTheme>()!
                                             .labelTextColor),
                                   ),
-                                if (currency.isPotentialScam)
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                                    child: Text(
-                                      "(${S.of(context).potential_scam})",
-                                      style: TextStyle(
-                                        color: Colors.red[800],
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                    ),
-                                  ),
                               ],
                             ),
                           ),
@@ -229,7 +218,33 @@ class BalanceRowWidget extends StatelessWidget {
                       ),
                     ],
                   ),
-                  //),
+                  if (currency.isPotentialScam)
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                      margin: EdgeInsets.only(top: 4),
+                      decoration: BoxDecoration(
+                        color: Colors.red[800],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.warning_amber_outlined,
+                            size: 16,
+                            color: Colors.white,
+                          ),
+                          const SizedBox(width: 4),
+                          Text(
+                            S.of(context).potential_scam,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   if (frozenBalance.isNotEmpty)
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
