eb6b1de45b7f1cdfb4d638647818ee7f90c2f0a9
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-03-31 12:46:05 +0200

    don't throw unhandled exceptions in unawaited async code

--- cw_monero/lib/monero_unspent.dart
+++ cw_monero/lib/monero_unspent.dart
@@ -8,7 +8,7 @@ class MoneroUnspent extends Unspent {
       String address, String hash, String keyImage, int value, bool isFrozen, this.isUnlocked)
       : super(address, hash, value, 0, keyImage) {
     getCoinByKeyImage(keyImage).then((coinId) {
-      if (coinId == null) throw Exception("Unable to find a coin for address $address");
+      if (coinId == null) return;
       getCoin(coinId).then((coin) {
         _frozen = monero.CoinsInfo_frozen(coin);
       });
@@ -21,7 +21,7 @@ class MoneroUnspent extends Unspent {
   set isFrozen(bool freeze) {
     printV("set isFrozen: $freeze ($keyImage): $freeze");
     getCoinByKeyImage(keyImage!).then((coinId) async {
-      if (coinId == null) throw Exception("Unable to find a coin for address $address");
+      if (coinId == null) return;
       if (freeze) {
         await freezeCoin(coinId);
         _frozen = true;
