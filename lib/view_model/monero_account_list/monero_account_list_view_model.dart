5e54010256a446245cf3510707399e98a559a04b
Matthew Fosse <matt@fosse.co>
2025-04-09 22:09:11 -0700

    finally found a fix

--- lib/view_model/monero_account_list/monero_account_list_view_model.dart
+++ lib/view_model/monero_account_list/monero_account_list_view_model.dart
@@ -8,8 +8,7 @@ import 'package:cake_wallet/monero/monero.dart';
 
 part 'monero_account_list_view_model.g.dart';
 
-class MoneroAccountListViewModel = MoneroAccountListViewModelBase
-    with _$MoneroAccountListViewModel;
+class MoneroAccountListViewModel = MoneroAccountListViewModelBase with _$MoneroAccountListViewModel;
 
 abstract class MoneroAccountListViewModelBase with Store {
   MoneroAccountListViewModelBase(this._wallet) : scrollOffsetFromTop = 0;
@@ -27,25 +26,89 @@ abstract class MoneroAccountListViewModelBase with Store {
   @computed
   List<AccountListItem> get accounts {
     if (_wallet.type == WalletType.monero) {
-      return monero
-        !.getAccountList(_wallet)
-        .accounts.map((acc) => AccountListItem(
-            label: acc.label,
+      var accList = monero!
+          .getAccountList(_wallet)
+          .accounts
+          .map((acc) => AccountListItem(
+              label: acc.label,
+              id: acc.id,
+              balance: acc.balance,
+              isSelected: acc.id == monero!.getCurrentAccount(_wallet).id))
+          .toList();
+
+      final subList = monero!
+          .getSubaddressList(_wallet)
+          .subaddresses
+          .map((acc) => AccountListItem(
+              label: acc.label,
+              id: acc.id,
+              balance: '0',
+              isSelected: acc.id == monero!.getCurrentAccount(_wallet).id))
+          .toList();
+
+      // override the labels of the accList with the labels of the subList
+      // this is needed because the function to update the labels doesn't update the same labels in the account list
+      // and there doesn't appear to be an equivalent account label function
+      final newAccList = accList.map((acc) {
+        try {
+          var subLabel = subList.firstWhere((sub) => sub.id == acc.id).label;
+          // remove everything before the first space:
+          subLabel = subLabel.split(' ').last;
+          return AccountListItem(
+            label: subLabel,
             id: acc.id,
             balance: acc.balance,
-            isSelected: acc.id == monero!.getCurrentAccount(_wallet).id))
-        .toList();
+            isSelected: acc.id == monero!.getCurrentAccount(_wallet).id,
+          );
+        } catch (e) {
+          return acc;
+        }
+      }).toList();
+
+      return newAccList;
     }
 
     if (_wallet.type == WalletType.wownero) {
-      return wownero
-        !.getAccountList(_wallet)
-        .accounts.map((acc) => AccountListItem(
-            label: acc.label,
+      var accList = wownero!
+          .getAccountList(_wallet)
+          .accounts
+          .map((acc) => AccountListItem(
+              label: acc.label,
+              id: acc.id,
+              balance: acc.balance,
+              isSelected: acc.id == wownero!.getCurrentAccount(_wallet).id))
+          .toList();
+
+      final subList = wownero!
+          .getSubaddressList(_wallet)
+          .subaddresses
+          .map((acc) => AccountListItem(
+              label: acc.label,
+              id: acc.id,
+              balance: '0',
+              isSelected: acc.id == wownero!.getCurrentAccount(_wallet).id))
+          .toList();
+
+      // override the labels of the accList with the labels of the subList
+      // this is needed because the function to update the labels doesn't update the same labels in the account list
+      // and there doesn't appear to be an equivalent account label function
+      final newAccList = accList.map((acc) {
+        try {
+          var subLabel = subList.firstWhere((sub) => sub.id == acc.id).label;
+          // remove everything before the first space:
+          subLabel = subLabel.split(' ').last;
+          return AccountListItem(
+            label: subLabel,
             id: acc.id,
             balance: acc.balance,
-            isSelected: acc.id == wownero!.getCurrentAccount(_wallet).id))
-        .toList();
+            isSelected: acc.id == wownero!.getCurrentAccount(_wallet).id,
+          );
+        } catch (e) {
+          return acc;
+        }
+      }).toList();
+
+      return newAccList;
     }
 
     throw Exception('Unexpected wallet type: ${_wallet.type}');
@@ -60,7 +123,7 @@ abstract class MoneroAccountListViewModelBase with Store {
         item.id,
         item.label,
         item.balance,
-        );
+      );
     }
 
     if (_wallet.type == WalletType.wownero) {
@@ -69,7 +132,7 @@ abstract class MoneroAccountListViewModelBase with Store {
         item.id,
         item.label,
         item.balance,
-        );
+      );
     }
   }
 }
