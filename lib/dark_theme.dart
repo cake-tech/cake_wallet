708969f3b47d8931863094f98661aa6e1a0fcdcd
tuxsudo <tuxsudo@tux.pizza>
2025-04-30 02:11:00 -0400

    Typos

--- lib/dark_theme.dart
+++ lib/dark_theme.dart
@@ -17,16 +17,16 @@ class BaseThemeV2 {
       errorContainer: Color(0xB71919),
       onErrorContainer: Color(0xFFDAD6),
       background: Color(0x1B284A),
-      backgroundGradient: Color(0x171E30)
+      backgroundGradient: Color(0x171E30),
       surface: Color(0x1D2541),
       onSurface: Color(0xD7E2F7),
       surfaceVariant: Color(0x3B3F59),
       onSurfaceVariant: Color(0xA3AFB9),
-      surfaceContainerLowest: Color(0x171C30)
-      surfaceContainerLow: Color(0x2D385C)
-      surfaceContainer: Color(0x24335B)
-      surfaceContainerHigh: Color(0x212C47)
-      surfaceContainerHighest: Color(0x2A3B67)
+      surfaceContainerLowest: Color(0x171C30),
+      surfaceContainerLow: Color(0x2D385C),
+      surfaceContainer: Color(0x24335B),
+      surfaceContainerHigh: Color(0x212C47),
+      surfaceContainerHighest: Color(0x2A3B67),
     ),
   );
 }
--- lib/light_theme.dart
+++ lib/light_theme.dart
@@ -17,16 +17,16 @@ class BaseThemeV2 {
       errorContainer: Color(0xFFBDBD),
       onErrorContainer: Color(0xE43D3D),
       background: Color(0xEFEFF8),
-      backgroundGradient: Color(0xDFE3E7)
+      backgroundGradient: Color(0xDFE3E7),
       surface: Color(0xEFEFF8),
       onSurface: Color(0x312938),
       surfaceVariant: Color(0xE8E0E7),
       onSurfaceVariant: Color(0x6C6772),
-      surfaceContainerLowest: Color(0xFFFFFF)
-      surfaceContainerLow: Color(0xF2F3F7)
-      surfaceContainer: Color(0xFBFBFD)
-      surfaceContainerHigh: Color(0xFDFDFE)
-      surfaceContainerHighest: Color(0xFFFFFF)
+      surfaceContainerLowest: Color(0xFFFFFF),
+      surfaceContainerLow: Color(0xF2F3F7),
+      surfaceContainer: Color(0xFBFBFD),
+      surfaceContainerHigh: Color(0xFDFDFE),
+      surfaceContainerHighest: Color(0xFFFFFF),
     ),
   );
 }
