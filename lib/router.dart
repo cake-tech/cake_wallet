9c8d22842ad064d580273b1538c7bfbf0d3a7dcb
Blazebrain <davidadegoke16@gmail.com>
2025-05-08 05:37:29 +0100

    fix(generic-fixes): Wallet Grouping Bug
    Add ability to export hash change logs

--- lib/router.dart
+++ lib/router.dart
@@ -36,7 +36,7 @@ import 'package:cake_wallet/src/screens/dashboard/pages/address_page.dart';
 import 'package:cake_wallet/src/screens/dashboard/pages/nft_details_page.dart';
 import 'package:cake_wallet/src/screens/dashboard/pages/transactions_page.dart';
 import 'package:cake_wallet/src/screens/dashboard/sign_page.dart';
-import 'package:cake_wallet/src/screens/dev/hash_change_log.dart';
+import 'package:cake_wallet/src/screens/dev/hash_change_logs_page.dart';
 import 'package:cake_wallet/src/screens/dev/monero_background_sync.dart';
 import 'package:cake_wallet/src/screens/dev/moneroc_call_profiler.dart';
 import 'package:cake_wallet/src/screens/dev/print_verbose_logs_page.dart';
@@ -923,7 +923,7 @@ Route<dynamic> createRoute(RouteSettings settings) {
       );
     
     case Routes.devHashChangeLogs:
-      return MaterialPageRoute<void>(builder: (_) => HashChangeLogViewerPage());
+      return MaterialPageRoute<void>(builder: (_) => HashChangeLogsPage());
 
     case Routes.devPrintVerbose:
       return MaterialPageRoute<void>(builder: (_) => PrintVerboseLogsPage());
