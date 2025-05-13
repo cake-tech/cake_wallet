2c97530ff2d2c76c531def21355c26799109aea8
OmarHatem <omarh.ismail1@gmail.com>
2025-05-13 17:29:45 +0300

    - Re-enable SwapTrade - Hide Keyboard on opening fee selection

--- lib/view_model/exchange/exchange_view_model.dart
+++ lib/view_model/exchange/exchange_view_model.dart
@@ -35,6 +35,7 @@ import 'package:cake_wallet/exchange/limits_state.dart';
 import 'package:cake_wallet/exchange/provider/changenow_exchange_provider.dart';
 import 'package:cake_wallet/exchange/provider/exchange_provider.dart';
 import 'package:cake_wallet/exchange/provider/exolix_exchange_provider.dart';
+import 'package:cake_wallet/exchange/provider/swaptrade_exchange_provider.dart';
 import 'package:cake_wallet/exchange/provider/sideshift_exchange_provider.dart';
 import 'package:cake_wallet/exchange/provider/thorchain_exchange.provider.dart';
 import 'package:cake_wallet/exchange/provider/trocador_exchange_provider.dart';
@@ -180,7 +181,7 @@ abstract class ExchangeViewModelBase extends WalletChangeListenerViewModel with 
         ThorChainExchangeProvider(tradesStore: trades),
         ChainflipExchangeProvider(tradesStore: trades),
         if (FeatureFlag.isExolixEnabled) ExolixExchangeProvider(),
-        // SwapTradeExchangeProvider(),
+        SwapTradeExchangeProvider(),
         LetsExchangeExchangeProvider(),
         StealthExExchangeProvider(),
         XOSwapExchangeProvider(),
