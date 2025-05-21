11e7a4fb4db96d5a69e5d0f7f79c79041631607f
JohnnyLawDGB <johnnylaw2021@protonmail.com>
2025-05-21 05:38:07 -0500

    Add DigiByte wallet package

---
+++ cw_digibyte/lib/cw_digibyte.dart
@@ -0,0 +1,4 @@
+library cw_digibyte;
+
+export 'digibyte_wallet.dart';
+export 'digibyte_wallet_service.dart';
---
+++ cw_digibyte/lib/digibyte_wallet_service.dart
@@ -0,0 +1,183 @@
+import 'dart:io';
+
+import 'package:bitcoin_base/bitcoin_base.dart';
+import 'package:cw_bitcoin/bitcoin_mnemonics_bip39.dart';
+import 'package:cw_bitcoin/litecoin_wallet.dart';
+import 'package:cw_bitcoin/mnemonic_is_incorrect_exception.dart';
+import 'package:cw_core/encryption_file_utils.dart';
+import 'package:cw_core/pathForWallet.dart';
+import 'package:cw_core/unspent_coins_info.dart';
+import 'package:cw_core/wallet_base.dart';
+import 'package:cw_core/wallet_info.dart';
+import 'package:cw_core/wallet_service.dart';
+import 'package:cw_core/wallet_type.dart';
+import 'package:hive/hive.dart';
+import 'package:collection/collection.dart';
+
+import 'digibyte_wallet.dart';
+
+class DigibyteWalletService extends WalletService<
+    BitcoinNewWalletCredentials,
+    BitcoinRestoreWalletFromSeedCredentials,
+    BitcoinRestoreWalletFromWIFCredentials,
+    BitcoinRestoreWalletFromHardware> {
+  DigibyteWalletService(
+      this.walletInfoSource, this.unspentCoinsInfoSource, this.alwaysScan, this.isDirect);
+
+  final Box<WalletInfo> walletInfoSource;
+  final Box<UnspentCoinsInfo> unspentCoinsInfoSource;
+  final bool alwaysScan;
+  final bool isDirect;
+
+  @override
+  WalletType getType() => WalletType.bitcoin;
+
+  @override
+  Future<DigibyteWallet> create(BitcoinNewWalletCredentials credentials, {bool? isTestnet}) async {
+    final String mnemonic;
+    switch (credentials.walletInfo?.derivationInfo?.derivationType) {
+      case DerivationType.bip39:
+        final strength = credentials.seedPhraseLength == 24 ? 256 : 128;
+        mnemonic = credentials.mnemonic ?? await MnemonicBip39.generate(strength: strength);
+        break;
+      case DerivationType.electrum:
+      default:
+        mnemonic = await generateElectrumMnemonic();
+        break;
+    }
+
+    final wallet = await DigibyteWalletBase(
+      mnemonic: mnemonic,
+      password: credentials.password!,
+      passphrase: credentials.passphrase,
+      walletInfo: credentials.walletInfo!,
+      unspentCoinsInfo: unspentCoinsInfoSource,
+      encryptionFileUtils: encryptionFileUtilsFor(isDirect),
+    );
+    await wallet.save();
+    await wallet.init();
+
+    return wallet;
+  }
+
+  @override
+  Future<bool> isWalletExit(String name) async =>
+      File(await pathForWallet(name: name, type: getType())).existsSync();
+
+  @override
+  Future<DigibyteWallet> openWallet(String name, String password) async {
+    final walletInfo = walletInfoSource.values
+        .firstWhereOrNull((info) => info.id == WalletBase.idFor(name, getType()))!;
+
+    try {
+      final wallet = await DigibyteWalletBase.open(
+        password: password,
+        name: name,
+        walletInfo: walletInfo,
+        unspentCoinsInfo: unspentCoinsInfoSource,
+        alwaysScan: alwaysScan,
+        encryptionFileUtils: encryptionFileUtilsFor(isDirect),
+      );
+      await wallet.init();
+      saveBackup(name);
+      return wallet;
+    } catch (_) {
+      await restoreWalletFilesFromBackup(name);
+      final wallet = await DigibyteWalletBase.open(
+        password: password,
+        name: name,
+        walletInfo: walletInfo,
+        unspentCoinsInfo: unspentCoinsInfoSource,
+        alwaysScan: alwaysScan,
+        encryptionFileUtils: encryptionFileUtilsFor(isDirect),
+      );
+      await wallet.init();
+      return wallet;
+    }
+  }
+
+  @override
+  Future<void> remove(String wallet) async {
+    File(await pathForWalletDir(name: wallet, type: getType())).delete(recursive: true);
+    final walletInfo = walletInfoSource.values
+        .firstWhereOrNull((info) => info.id == WalletBase.idFor(wallet, getType()))!;
+    await walletInfoSource.delete(walletInfo.key);
+
+    final unspentCoinsToDelete = unspentCoinsInfoSource.values
+        .where((unspentCoin) => unspentCoin.walletId == walletInfo.id)
+        .toList();
+
+    final keysToDelete = unspentCoinsToDelete.map((unspentCoin) => unspentCoin.key).toList();
+
+    if (keysToDelete.isNotEmpty) {
+      await unspentCoinsInfoSource.deleteAll(keysToDelete);
+    }
+  }
+
+  @override
+  Future<void> rename(String currentName, String password, String newName) async {
+    final currentWalletInfo = walletInfoSource.values
+        .firstWhereOrNull((info) => info.id == WalletBase.idFor(currentName, getType()))!;
+    final currentWallet = await DigibyteWalletBase.open(
+      password: password,
+      name: currentName,
+      walletInfo: currentWalletInfo,
+      unspentCoinsInfo: unspentCoinsInfoSource,
+      alwaysScan: alwaysScan,
+      encryptionFileUtils: encryptionFileUtilsFor(isDirect),
+    );
+
+    await currentWallet.renameWalletFiles(newName);
+    await saveBackup(newName);
+
+    final newWalletInfo = currentWalletInfo;
+    newWalletInfo.id = WalletBase.idFor(newName, getType());
+    newWalletInfo.name = newName;
+
+    await walletInfoSource.put(currentWalletInfo.key, newWalletInfo);
+  }
+
+  @override
+  Future<DigibyteWallet> restoreFromHardwareWallet(
+    BitcoinRestoreWalletFromHardware credentials, {
+    bool? isTestnet,
+  }) async {
+    final network = isTestnet == true ? DigibyteNetwork.testnet : DigibyteNetwork.mainnet;
+    credentials.walletInfo?.network = network.value;
+    credentials.walletInfo?.derivationInfo?.derivationPath = credentials.hwAccountData.derivationPath;
+
+    final wallet = await DigibyteWallet(
+      password: credentials.password!,
+      xpub: credentials.hwAccountData.xpub,
+      walletInfo: credentials.walletInfo!,
+      unspentCoinsInfo: unspentCoinsInfoSource,
+      encryptionFileUtils: encryptionFileUtilsFor(isDirect),
+    );
+    await wallet.save();
+    await wallet.init();
+    return wallet;
+  }
+
+  @override
+  Future<DigibyteWallet> restoreFromKeys(BitcoinRestoreWalletFromWIFCredentials credentials, {bool? isTestnet}) async =>
+      throw UnimplementedError();
+
+  @override
+  Future<DigibyteWallet> restoreFromSeed(BitcoinRestoreWalletFromSeedCredentials credentials, {bool? isTestnet}) async {
+    if (!validateMnemonic(credentials.mnemonic)) {
+      throw LitecoinMnemonicIsIncorrectException();
+    }
+
+    final wallet = await DigibyteWalletBase(
+      password: credentials.password!,
+      passphrase: credentials.passphrase,
+      mnemonic: credentials.mnemonic,
+      walletInfo: credentials.walletInfo!,
+      unspentCoinsInfo: unspentCoinsInfoSource,
+      encryptionFileUtils: encryptionFileUtilsFor(isDirect),
+    );
+    await wallet.save();
+    await wallet.init();
+    return wallet;
+  }
+}
