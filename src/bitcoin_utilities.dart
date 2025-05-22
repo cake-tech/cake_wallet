7c3a9669dd5948b334119ecce07b114b8f2dccda
JohnnyLawDGB <johnnylaw2021@protonmail.com>
2025-05-22 18:34:25 -0500

    refactor: use bitcoin utilities wrapper for Digibyte

---
+++ src/bitcoin_utilities.dart
@@ -0,0 +1,6 @@
+// src/bitcoin_utilities.dart
+export 'package:cw_bitcoin/electrum_derivations.dart'
+    show DerivationType, electrum_path;
+export 'package:cw_bitcoin/bitcoin_wallet_creation_credentials.dart';
+export 'package:cw_bitcoin/bitcoin_transaction_priority.dart';
+export 'package:cw_bitcoin/bitcoin_transaction_credentials.dart';
