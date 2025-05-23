52d7689538012261807c9515473a8868b44a75ac
rafael_xmr <g.pi8ax@slmail.me>
2025-05-23 16:02:29 -0300

    fix: methods2

--- cw_bitcoin/lib/address_from_output.dart
+++ cw_bitcoin/lib/address_from_output.dart
@@ -17,21 +17,16 @@ BitcoinBaseAddress addressFromScript(Script script,
 
   switch (addressType) {
     case P2pkhAddressType.p2pkh:
-      return P2pkhAddress.fromScriptPubkey(
-          script: script, network: BitcoinNetwork.mainnet);
+      return P2pkhAddress.fromScriptPubkey(script: script);
     case P2shAddressType.p2pkhInP2sh:
     case P2shAddressType.p2pkInP2sh:
-      return P2shAddress.fromScriptPubkey(
-          script: script, network: BitcoinNetwork.mainnet);
+      return P2shAddress.fromScriptPubkey(script: script);
     case SegwitAddressType.p2wpkh:
-      return P2wpkhAddress.fromScriptPubkey(
-          script: script, network: BitcoinNetwork.mainnet);
+      return P2wpkhAddress.fromScriptPubkey(script: script);
     case SegwitAddressType.p2wsh:
-      return P2wshAddress.fromScriptPubkey(
-          script: script, network: BitcoinNetwork.mainnet);
+      return P2wshAddress.fromScriptPubkey(script: script);
     case SegwitAddressType.p2tr:
-      return P2trAddress.fromScriptPubkey(
-          script: script, network: BitcoinNetwork.mainnet);
+      return P2trAddress.fromScriptPubkey(script: script);
   }
 
   throw ArgumentError("Invalid script");
