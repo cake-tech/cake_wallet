93445f29bd3e9eca1502b2288b60e23025c2606e
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-05-27 10:32:31 +0200

    fix: derusting tor implementation
    Instead of rust-based Arti I've moved back to the
    OG C++ tor implementation.
    This fixed all issues we had with Tor.
    - onion services now work
    - all requests are going through without random errors
    - we don't have to navigate a maze of multiple forks of multiple packages
    - fully working `torrc` config file (probably will be needed for Tari).
    - logging for Tor client
    - and so on.
    
    feat: network logging tab
    feat: use built-in proxy on Tails - this should resolve all issues for Tails users (needs testing though)

--- lib/utils/feature_flag.dart
+++ lib/utils/feature_flag.dart
@@ -5,7 +5,7 @@ class FeatureFlag {
   static const bool isCakePayEnabled = false;
   static const bool isExolixEnabled = true;
   static const bool isBackgroundSyncEnabled = true;
-  static final bool isInAppTorEnabled = (Platform.isAndroid || Platform.isIOS);
+  static final bool isInAppTorEnabled = (Platform.isAndroid);
   static const int verificationWordsCount = kDebugMode ? 0 : 2;
   static const bool hasDevOptions = bool.fromEnvironment('hasDevOptions', defaultValue: kDebugMode);
 }
