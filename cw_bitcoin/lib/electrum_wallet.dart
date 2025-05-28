02cbae52a5b3334b387b87102a6cb5f6ee590ad3
Konstantin Ullrich <konstantinullrich12@gmail.com>
2025-05-28 13:19:33 +0200

    feat: add unsignedPsbt to PendingBitcoinTransaction

--- cw_bitcoin/lib/electrum_wallet.dart
+++ cw_bitcoin/lib/electrum_wallet.dart
@@ -1125,7 +1125,8 @@ abstract class ElectrumWalletBase
           network: network,
           hasChange: estimatedTx.hasChange,
           isSendAll: estimatedTx.isSendAll,
-          hasTaprootInputs: false, // ToDo: (Konsti) Support Taproot
+          hasTaprootInputs: false, // ToDo: (Konsti) Support Taproot,
+          isViewOnly: false,
         )..addListener((transaction) async {
             transactionHistory.addOne(transaction);
             await updateBalance();
@@ -1196,18 +1197,21 @@ abstract class ElectrumWalletBase
         }
       });
 
-      return PendingBitcoinTransaction(transaction, type,
-          electrumClient: electrumClient,
-          amount: estimatedTx.amount,
-          fee: estimatedTx.fee,
-          feeRate: feeRateInt.toString(),
-          network: network,
-          hasChange: estimatedTx.hasChange,
-          isSendAll: estimatedTx.isSendAll,
-          hasTaprootInputs: hasTaprootInputs,
-          utxos: estimatedTx.utxos,
-          publicKeys: estimatedTx.publicKeys)
-        ..addListener((transaction) async {
+      return PendingBitcoinTransaction(
+        transaction,
+        type,
+        electrumClient: electrumClient,
+        amount: estimatedTx.amount,
+        fee: estimatedTx.fee,
+        feeRate: feeRateInt.toString(),
+        network: network,
+        hasChange: estimatedTx.hasChange,
+        isSendAll: estimatedTx.isSendAll,
+        hasTaprootInputs: hasTaprootInputs,
+        utxos: estimatedTx.utxos,
+        publicKeys: estimatedTx.publicKeys,
+        isViewOnly: keys.privateKey.isEmpty,
+      )..addListener((transaction) async {
           transactionHistory.addOne(transaction);
           if (estimatedTx.spendsSilentPayment) {
             transactionHistory.transactions.values.forEach((tx) {
@@ -1868,6 +1872,7 @@ abstract class ElectrumWalletBase
         network: network,
         hasChange: changeOutputs.isNotEmpty,
         feeRate: newFee.toString(),
+        isViewOnly: keys.privateKey.isEmpty,
       )..addListener((transaction) async {
           transactionHistory.transactions.values.forEach((tx) {
             if (tx.id == hash) {
