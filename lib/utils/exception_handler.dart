d00b272c2c46fa8da08fe458b4c2cc0cb1faedb4
OmarHatem <omarh.ismail1@gmail.com>
2025-07-03 18:21:58 +0300

    make it more specific [skip ci]

--- lib/utils/exception_handler.dart
+++ lib/utils/exception_handler.dart
@@ -286,7 +286,7 @@ class ExceptionHandler {
     // just ignoring until we find a solution to this issue or migrate from flutter secure storage
     "core/auth_service.dart:64",
     "core/key_service.dart:14",
-    "core/wallet_loading_service.dart:142",
+    "Wallet is null",
     "Wrong Device Status: 0x5515 (UNKNOWN)",
   ];
 
