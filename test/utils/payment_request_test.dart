f14144d979c60b0561a72b8a381756264b103310
Konstantin Ullrich <konstantinullrich12@gmail.com>
2025-07-11 13:43:45 +0200

    test: add unit test for EIP-681 URI handling with contract and no chainId [skip-ci]

--- test/utils/payment_request_test.dart
+++ test/utils/payment_request_test.dart
@@ -40,6 +40,15 @@ void main() {
         expect(paymentRequest.amount, "");
       });
 
+      test("extract address and amount from EIP681 Uri with contract and no chainId", () {
+        final uri = Uri.parse(
+            "ethereum:0x1234567890abcdef1234567890abcdef12345678/transfer?address=0xabcdef1234567890abcdef1234567890abcdef12&uint256=1000000000000000000");
+        final paymentRequest = PaymentRequest.fromUri(uri);
+
+        expect(paymentRequest.address, "0xabcdef1234567890abcdef1234567890abcdef12");
+        expect(paymentRequest.amount, "1");
+      });
+
       test("extract address from minimal EIP681 Uri", () {
         final uri = Uri.parse("ethereum:0xCfc1650da7C961FD82998e7e30ca5f699D0aBf41");
         final paymentRequest = PaymentRequest.fromUri(uri);
