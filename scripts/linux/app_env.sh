f1a93a9b83c923dbf1cf48ee2963f4da4e18afe7
OmarHatem <omarh.ismail1@gmail.com>
2025-07-12 20:55:16 +0300

    update build numbers

--- scripts/linux/app_env.sh
+++ scripts/linux/app_env.sh
@@ -15,7 +15,7 @@ fi
 
 CAKEWALLET_NAME="Cake Wallet"
 CAKEWALLET_VERSION="5.1.2"
-CAKEWALLET_BUILD_NUMBER=60
+CAKEWALLET_BUILD_NUMBER=61
 
 if ! [[ " ${TYPES[*]} " =~ " ${APP_LINUX_TYPE} " ]]; then
     echo "Wrong app type."
--- scripts/macos/app_env.sh
+++ scripts/macos/app_env.sh
@@ -22,7 +22,7 @@ MONERO_COM_BUNDLE_ID="com.cakewallet.monero"
 
 CAKEWALLET_NAME="Cake Wallet"
 CAKEWALLET_VERSION="5.1.2"
-CAKEWALLET_BUILD_NUMBER=121
+CAKEWALLET_BUILD_NUMBER=122
 CAKEWALLET_BUNDLE_ID="com.fotolockr.cakewallet"
 
 if ! [[ " ${TYPES[*]} " =~ " ${APP_MACOS_TYPE} " ]]; then
