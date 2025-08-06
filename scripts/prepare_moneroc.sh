7d6faaf00bc97a09566b82c19f4c413caff965e7
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-08-06 10:10:24 -0400

    feat: monero cache debugging feat: better JSON explorer for network traffic

--- scripts/prepare_moneroc.sh
+++ scripts/prepare_moneroc.sh
@@ -9,7 +9,7 @@ then
     rm -rf monero_c
     git clone https://github.com/mrcyjanek/monero_c --branch master monero_c
     cd monero_c
-    git checkout b576312e4d466569cd03482b61c597b39a9f4dc3
+    git checkout ac98e9b7d3f696f7db59f8485d33b2c113fcfe75
     git reset --hard
     git submodule update --init --force --recursive
     ./apply_patches.sh monero
