8ce4d0f523b5e71da8791282efec71637eae6448
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-08-14 23:37:22 -0400

    fix: use of invalid thread api on iOS

--- scripts/prepare_torch.sh
+++ scripts/prepare_torch.sh
@@ -5,8 +5,9 @@ cd "$(dirname "$0")"
 # IMPORTANT: Make sure to update action 'Build Torch` in
 # - .github/workflows/pr_test_build_android.yml 
 # - .github/workflows/pr_test_build_linux.yml
-# https://github.com/MrCyjaneK/torch_dart/releases/download/4f987d4-1-ge071fcf/torch_dart-4f987d4-1-ge071fcf.tar.gz
-HASH=e071fcfb454b0378933005853e29afd797a2022e
+# https://github.com/MrCyjaneK/torch_dart/releases/download/4f987d4-1-ge071fcf-1-gc282f09/torch_dart-4f987d4-1-ge071fcf-1-gc282f09.tar.gz
+
+HASH=c282f093ecfb5d38292634830fb2cd19e5adcf62
 
 if [[ ! -d "torch_dart/.git" ]];
 then
@@ -22,3 +23,6 @@ fi
 git reset --hard
 git checkout $HASH
 git reset --hard
+
+# recommended to uncomment during development
+# sed -i.bak 's/go run . -cleanup/#go run . -cleanup/g' build.sh
