3bdf3345d295f9481535a058d04214026dcd7ecd
Konstantin Ullrich <konstantinullrich12@gmail.com>
2025-08-15 10:31:34 +0200

    refactor: remove unused mwebd dependencies and clean up CwMwebPlugin code

--- cw_mweb/android/src/main/kotlin/com/cakewallet/mweb/CwMwebPlugin.kt
+++ cw_mweb/android/src/main/kotlin/com/cakewallet/mweb/CwMwebPlugin.kt
@@ -8,9 +8,6 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-import mwebd.Mwebd
-import mwebd.Server
-
 /** CwMwebPlugin */
 class CwMwebPlugin: FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
@@ -18,8 +15,6 @@ class CwMwebPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
-  private var server: Server? = null
-  private var port: Long? = null
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "cw_mweb")
@@ -27,28 +22,10 @@ class CwMwebPlugin: FlutterPlugin, MethodCallHandler {
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "address") {
-      // val scanSecret: ByteArray = call.argument<ByteArray>("scanSecret") ?: ByteArray(0)
-      // val spendPub: ByteArray = call.argument<ByteArray>("spendPub") ?: ByteArray(0)
-      // val index: Int = call.argument<Int>("index") ?: 0
-      // val res = Mwebd.address(scanSecret, spendPub, index)
-      // result.success(res)
-    } else if (call.method == "addresses") {
-      val scanSecret: ByteArray = call.argument<ByteArray>("scanSecret") ?: ByteArray(0)
-      val spendPub: ByteArray = call.argument<ByteArray>("spendPub") ?: ByteArray(0)
-      val fromIndex: Int = call.argument<Int>("fromIndex") ?: 0
-      val toIndex: Int = call.argument<Int>("toIndex") ?: 0
-      val res = Mwebd.addresses(scanSecret, spendPub, fromIndex, toIndex)
-      result.success(res)
-    } else {
-      result.notImplemented()
-    }
+    result.notImplemented()
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
-    server?.stop()
-    server = null
-    port = null
   }
 }
