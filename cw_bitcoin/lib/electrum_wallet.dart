16f058e9e2336520db942be0b241582be32fbec4
rafael_xmr <g.pi8ax@slmail.me>
2025-08-20 23:06:35 -0300

    sp improv (#2437)
    * fix(sp): proper address on details screen
    
    * fix(sp): fields to update on incoming msg
    
    * Update lib/bitcoin/cw_bitcoin.dart
    
    ---------
    
    Co-authored-by: Omar Hatem <omarh.ismail1@gmail.com>

--- cw_bitcoin/lib/electrum_wallet.dart
+++ cw_bitcoin/lib/electrum_wallet.dart
@@ -403,6 +403,9 @@ abstract class ElectrumWalletBase
               existingTxInfo.confirmations = tx.confirmations;
               existingTxInfo.height = tx.height;
               existingTxInfo.date = tx.date;
+              existingTxInfo.isReceivedSilentPayment = tx.isReceivedSilentPayment;
+              existingTxInfo.direction = tx.direction;
+              existingTxInfo.isPending = tx.isPending;
 
               final newUnspents = tx.unspents!
                   .where((unspent) => !(existingTxInfo.unspents?.any((element) =>
@@ -2057,8 +2060,8 @@ abstract class ElectrumWalletBase
             .where((type) => type != SegwitAddresType.mweb)
             .map((type) => fetchTransactionsForAddressType(historiesWithDetails, type)));
       } else if (type == WalletType.dogecoin) {
-        await Future.wait(DOGECOIN_ADDRESS_TYPES.map(
-            (type) => fetchTransactionsForAddressType(historiesWithDetails, type)));
+        await Future.wait(DOGECOIN_ADDRESS_TYPES
+            .map((type) => fetchTransactionsForAddressType(historiesWithDetails, type)));
       }
 
       transactionHistory.transactions.values.forEach((tx) async {
@@ -2875,6 +2878,23 @@ Future<void> _handleScanSilentPayments(ScanData scanData) async {
       LogLevel.info,
     );
 
+    void endScanningSuccesfully() {
+      if (scanData.isSingleScan)
+        scanData.sendPort.send(SyncResponse(syncHeight, SyncedSyncStatus()));
+      else
+        scanData.sendPort.send(
+          SyncResponse(syncHeight, SyncedTipSyncStatus(scanData.chainTip)),
+        );
+
+      _scanningStream?.close();
+      _scanningStream = null;
+
+      log(
+        "ended: syncHeight: $syncHeight, chainTip: ${scanData.chainTip}, isSingleScan: ${scanData.isSingleScan}",
+        LogLevel.info,
+      );
+    }
+
     void listenFn(Map<String, dynamic> event, ElectrumTweaksSubscribe req) async {
       final response = req.onResponse(event);
 
@@ -2882,9 +2902,6 @@ Future<void> _handleScanSilentPayments(ScanData scanData) async {
         log(
           "ending: response = $response, stream = $_scanningStream",
           LogLevel.error,
-        );
-        scanData.sendPort.send(
-          SyncResponse(scanData.height, LostConnectionSyncStatus()),
         );
         return;
       }
@@ -2896,9 +2913,7 @@ Future<void> _handleScanSilentPayments(ScanData scanData) async {
         if (scanData.isSingleScan) {
           log("ending: noData and isSingleScan", LogLevel.info);
 
-          scanData.sendPort.send(
-            SyncResponse(scanData.height, LostConnectionSyncStatus()),
-          );
+          endScanningSuccesfully();
           return;
         }
 
@@ -3114,22 +3129,7 @@ Future<void> _handleScanSilentPayments(ScanData scanData) async {
       syncHeight = tweakHeight;
 
       if ((tweakHeight >= scanData.chainTip) || scanData.isSingleScan) {
-        if (tweakHeight >= scanData.chainTip)
-          scanData.sendPort.send(
-            SyncResponse(syncHeight, SyncedTipSyncStatus(scanData.chainTip)),
-          );
-
-        if (scanData.isSingleScan) {
-          scanData.sendPort.send(SyncResponse(syncHeight, SyncedSyncStatus()));
-        }
-
-        _scanningStream?.close();
-        _scanningStream = null;
-        log(
-          "ending: syncHeight: $syncHeight, chainTip: ${scanData.chainTip}, isSingleScan: ${scanData.isSingleScan}",
-          LogLevel.info,
-        );
-        return;
+        endScanningSuccesfully();
       }
     }
 
