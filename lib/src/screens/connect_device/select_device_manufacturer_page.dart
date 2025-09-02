5dc6dc3bccefad38d81a744e5ff5224d7569d3c4
Konstantin Ullrich <konstantinullrich12@gmail.com>
2025-09-02 13:27:47 +0200

    feat: add SVG assets for hardware wallet manufacturers (Foundation, Keystone, SeedSigner)

--- lib/src/screens/connect_device/select_device_manufacturer_page.dart
+++ lib/src/screens/connect_device/select_device_manufacturer_page.dart
@@ -1,5 +1,6 @@
 import 'package:cake_wallet/generated/i18n.dart';
 import 'package:cake_wallet/src/screens/base_page.dart';
+import 'package:cake_wallet/src/screens/connect_device/widgets/dotted_divider.dart';
 import 'package:cake_wallet/src/screens/connect_device/widgets/manufacturer_option_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -7,17 +8,46 @@ import 'package:flutter_svg/flutter_svg.dart';
 class SelectDeviceManufacturerPage extends BasePage {
   SelectDeviceManufacturerPage();
 
-  final imageLedger = SvgPicture.asset('assets/images/hardware_wallet/ledger_man.svg', height: 25);
-  final imageBitbox = SvgPicture.asset('assets/images/hardware_wallet/bitbox_man.svg', height: 12);
   final imageTrezor = SvgPicture.asset('assets/images/hardware_wallet/trezor_man.svg', height: 20);
-  final imageColdcard =
-      SvgPicture.asset('assets/images/hardware_wallet/coldcard_man.svg', height: 12);
 
   @override
   String get title => S.current.restore_title_from_hardware_wallet;
 
   @override
   AppBarStyle get appBarStyle => AppBarStyle.regular;
+
+  List<_DeviceManufacturer> get availableManufacturers => [
+        _DeviceManufacturer(
+          image: SvgPicture.asset('assets/images/hardware_wallet/ledger_man.svg', height: 25),
+        ),
+        _DeviceManufacturer(
+          image: SvgPicture.asset('assets/images/hardware_wallet/bitbox_man.svg', height: 25),
+          tag: S.current.new_tag,
+        ),
+        _DeviceManufacturer(
+          image: SvgPicture.asset('assets/images/hardware_wallet/coldcard_man.svg', height: 12),
+          tag: S.current.new_tag,
+        ),
+        _DeviceManufacturer(
+          image: SvgPicture.asset('assets/images/hardware_wallet/seedsigner_man.svg', height: 25),
+          tag: S.current.new_tag,
+        ),
+      ];
+
+  List<_DeviceManufacturer> get comingManufacturers => [
+        _DeviceManufacturer(
+          image: SvgPicture.asset('assets/images/hardware_wallet/trezor_man.svg', height: 20),
+          tag: S.current.coming_soon_tag,
+        ),
+        _DeviceManufacturer(
+          image: SvgPicture.asset('assets/images/hardware_wallet/foundation_man.svg', height: 20),
+          tag: S.current.coming_soon_tag,
+        ),
+        _DeviceManufacturer(
+          image: SvgPicture.asset('assets/images/hardware_wallet/keystone_man.svg', height: 20),
+          tag: S.current.coming_soon_tag,
+        ),
+      ];
 
   @override
   Widget body(BuildContext context) => Container(
@@ -26,42 +56,32 @@ class SelectDeviceManufacturerPage extends BasePage {
             padding: EdgeInsets.only(left: 24, right: 24),
             child: Column(
               children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: ManufacturerOptionTile(
-                    image: imageLedger,
-                    supportedDevices: "Nano S, Nano X, Flex & Stax",
-                    onPressed: () {},
-                    isDarkTheme: currentTheme.isDark,
+                ...availableManufacturers.map(
+                  (manufacturer) => Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: ManufacturerOptionTile(
+                      image: manufacturer.image,
+                      tag: manufacturer.tag,
+                      // supportedDevices: "Nano S, Nano X, Flex & Stax",
+                      onPressed: () {},
+                      isDarkTheme: currentTheme.isDark,
+                    ),
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: ManufacturerOptionTile(
-                    image: imageBitbox,
-                    tag: "Soon",
-                    supportedDevices: "BitBox02 & BitBox02 Nova",
-                    onPressed: () {},
-                    isDarkTheme: currentTheme.isDark,
-                  ),
+                  padding: EdgeInsets.only(top: 20, bottom: 10),
+                  child: DottedDivider(color: Theme.of(context).colorScheme.surfaceContainer),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: ManufacturerOptionTile(
-                    image: imageTrezor,
-                    tag: "Soon",
-                    supportedDevices: "Safe 5, Safe 3 & Model One",
-                    onPressed: () {},
-                    isDarkTheme: currentTheme.isDark,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: ManufacturerOptionTile(
-                    image: imageColdcard,
-                    supportedDevices: "COLDCARD Q",
-                    onPressed: () {},
-                    isDarkTheme: currentTheme.isDark,
+                ...comingManufacturers.map(
+                      (manufacturer) => Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: ManufacturerOptionTile(
+                      image: manufacturer.image,
+                      tag: manufacturer.tag,
+                      onPressed: () {},
+                      isDarkTheme: currentTheme.isDark,
+                      isUnavailable: true,
+                    ),
                   ),
                 ),
               ],
@@ -69,4 +89,11 @@ class SelectDeviceManufacturerPage extends BasePage {
           ),
         ),
       );
+}
+
+class _DeviceManufacturer {
+  final SvgPicture image;
+  final String? tag;
+
+  _DeviceManufacturer({required this.image, this.tag});
 }
--- lib/src/screens/connect_device/widgets/manufacturer_option_tile.dart
+++ lib/src/screens/connect_device/widgets/manufacturer_option_tile.dart
@@ -1,4 +1,3 @@
-import 'package:cake_wallet/generated/i18n.dart';
 import 'package:cake_wallet/themes/utils/custom_theme_colors.dart';
 import 'package:flutter/material.dart';
 
@@ -6,89 +5,75 @@ class ManufacturerOptionTile extends StatelessWidget {
   const ManufacturerOptionTile({
     required this.onPressed,
     required this.image,
-    required this.supportedDevices,
     required this.isDarkTheme,
     this.tag,
+    this.isUnavailable = false,
   });
 
   final VoidCallback onPressed;
   final Widget image;
-  final String supportedDevices;
   final String? tag;
+  final bool isUnavailable;
   final bool isDarkTheme;
 
   @override
   Widget build(BuildContext context) => GestureDetector(
         onTap: onPressed,
-        child: Container(
-          width: double.infinity,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            gradient: LinearGradient(
-              colors: [
-                isDarkTheme
-                    ? CustomThemeColors.cardGradientColorPrimaryDark
-                    : CustomThemeColors.cardGradientColorPrimaryLight,
-                isDarkTheme
-                    ? CustomThemeColors.cardGradientColorSecondaryDark
-                    : CustomThemeColors.cardGradientColorSecondaryLight,
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          padding: EdgeInsets.all(24),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      children: [
-                        Container(height: 25, child: image),
-                        if (tag != null)
-                          Container(
-                              decoration: BoxDecoration(
-                                  border: Border.all(),
-                                  borderRadius: BorderRadius.circular(20),
-                                  color: Theme.of(context).colorScheme.onSurfaceVariant),
-                              padding: EdgeInsets.symmetric(horizontal: 5),
-                              margin: EdgeInsets.only(left: 5),
-                              child: Text(tag!))
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 5),
-                      child: RichText(
-                        text: TextSpan(children: [
-                          TextSpan(
-                            text: '${S.of(context).supported}: ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(
-                            text: supportedDevices,
-                            style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                                  color: Theme.of(context).colorScheme.onSurfaceVariant,
-                                ),
-                          ),
-                        ]),
-                      ),
-                    )
-                  ],
-                ),
+        child: Opacity(
+          opacity: isUnavailable ? 0.5 : 1,
+          child: Container(
+            width: double.infinity,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              gradient: LinearGradient(
+                colors: [
+                  isDarkTheme
+                      ? CustomThemeColors.cardGradientColorPrimaryDark
+                      : CustomThemeColors.cardGradientColorPrimaryLight,
+                  isDarkTheme
+                      ? CustomThemeColors.cardGradientColorSecondaryDark
+                      : CustomThemeColors.cardGradientColorSecondaryLight,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
               ),
-              Icon(
-                Icons.arrow_forward_ios,
-                size: 16,
-                color: Theme.of(context).colorScheme.onSurface,
-              )
-            ],
+            ),
+            padding: EdgeInsets.all(24),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: [
+                          Container(height: 25, child: image),
+                          if (tag != null)
+                            Container(
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.transparent),
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Theme.of(context).colorScheme.surfaceContainerLow),
+                                padding: EdgeInsets.symmetric(horizontal: 5),
+                                margin: EdgeInsets.only(left: 5),
+                                child: Text(tag!))
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 16,
+                  color: Theme.of(context).colorScheme.onSurface,
+                )
+              ],
+            ),
           ),
         ),
       );
