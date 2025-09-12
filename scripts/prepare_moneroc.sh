52e778f2757f380aef710f83fc8bd14b4bedb37a
cyan <cyjan@mrcyjanek.net>
2025-09-12 08:47:19 -0400

    feat: monero cache debugging (#2436)
    * feat: monero cache debugging
    feat: better JSON explorer for network traffic
    
    * frozen coins debugging

--- scripts/prepare_moneroc.sh
+++ scripts/prepare_moneroc.sh
@@ -9,7 +9,7 @@ then
     rm -rf monero_c
     git clone https://github.com/mrcyjanek/monero_c --branch master monero_c
     cd monero_c
-    git checkout b576312e4d466569cd03482b61c597b39a9f4dc3
+    git checkout 5225c47e5033d46bfd60e612e87b24c562f67efa
     git reset --hard
     git submodule update --init --force --recursive
     ./apply_patches.sh monero
