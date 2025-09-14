ec4fc57055fc47056f71190b2202ebec5646262c
Hector Chu <hectorchu@gmail.com>
2025-09-14 13:03:40 +0000

    Don't eat psbt rpc errors

--- cw_mweb/lib/cw_mweb.dart
+++ cw_mweb/lib/cw_mweb.dart
@@ -225,66 +225,31 @@ class CwMweb {
 
   static Future<PsbtResponse> psbtCreate(PsbtCreateRequest request) async {
     log("mweb.psbtCreate() called");
-    try {
-      _rpcClient = await stub();
-      return await _rpcClient!.psbtCreate(request, options: CallOptions(timeout: TIMEOUT_DURATION));
-    } on GrpcError catch (e) {
-      printV('Caught grpc error: ${e.message}');
-    } catch (e) {
-      printV("Error getting psbtCreate: $e");
-    }
-    return PsbtResponse();
+    _rpcClient = await stub();
+    return await _rpcClient!.psbtCreate(request, options: CallOptions(timeout: TIMEOUT_DURATION));
   }
 
   static Future<PsbtResponse> psbtAddInput(PsbtAddInputRequest request) async {
     log("mweb.psbtAddInput() called");
-    try {
-      _rpcClient = await stub();
-      return await _rpcClient!.psbtAddInput(request, options: CallOptions(timeout: TIMEOUT_DURATION));
-    } on GrpcError catch (e) {
-      printV('Caught grpc error: ${e.message}');
-    } catch (e) {
-      printV("Error getting psbtAddInput: $e");
-    }
-    return PsbtResponse();
+    _rpcClient = await stub();
+    return await _rpcClient!.psbtAddInput(request, options: CallOptions(timeout: TIMEOUT_DURATION));
   }
 
   static Future<PsbtResponse> psbtAddRecipient(PsbtAddRecipientRequest request) async {
     log("mweb.psbtAddRecipient() called");
-    try {
-      _rpcClient = await stub();
-      return await _rpcClient!.psbtAddRecipient(request, options: CallOptions(timeout: TIMEOUT_DURATION));
-    } on GrpcError catch (e) {
-      printV('Caught grpc error: ${e.message}');
-    } catch (e) {
-      printV("Error getting psbtAddRecipient: $e");
-    }
-    return PsbtResponse();
+    _rpcClient = await stub();
+    return await _rpcClient!.psbtAddRecipient(request, options: CallOptions(timeout: TIMEOUT_DURATION));
   }
 
   static Future<PsbtGetRecipientsResponse> psbtGetRecipients(PsbtGetRecipientsRequest request) async {
     log("mweb.psbtGetRecipients() called");
-    try {
-      _rpcClient = await stub();
-      return await _rpcClient!.psbtGetRecipients(request, options: CallOptions(timeout: TIMEOUT_DURATION));
-    } on GrpcError catch (e) {
-      printV('Caught grpc error: ${e.message}');
-    } catch (e) {
-      printV("Error getting psbtGetRecipients: $e");
-    }
-    return PsbtGetRecipientsResponse();
+    _rpcClient = await stub();
+    return await _rpcClient!.psbtGetRecipients(request, options: CallOptions(timeout: TIMEOUT_DURATION));
   }
 
   static Future<CreateResponse> psbtExtract(PsbtExtractRequest request) async {
     log("mweb.psbtExtract() called");
-    try {
-      _rpcClient = await stub();
-      return await _rpcClient!.psbtExtract(request, options: CallOptions(timeout: TIMEOUT_DURATION));
-    } on GrpcError catch (e) {
-      printV('Caught grpc error: ${e.message}');
-    } catch (e) {
-      printV("Error getting psbtExtract: $e");
-    }
-    return CreateResponse();
+    _rpcClient = await stub();
+    return await _rpcClient!.psbtExtract(request, options: CallOptions(timeout: TIMEOUT_DURATION));
   }
 }
