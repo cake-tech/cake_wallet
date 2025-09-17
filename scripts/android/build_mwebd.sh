c15fc8842e880d7e67af96051847d8abcf54b095
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-09-17 13:55:12 -0400

    build: 16KB align go builds ci: update action to check for 16KB align [skip ci]

--- scripts/android/build_mwebd.sh
+++ scripts/android/build_mwebd.sh
@@ -25,24 +25,26 @@ export ANDROID_SDK="${HOME}/Library/Android/sdk"
 export NDK_BIN="${ANDROID_HOME}/ndk/${ANDROID_NDK_VERSION}/toolchains/llvm/prebuilt/$(uname | tr '[:upper:]' '[:lower:]')-x86_64/bin"
 
 # Compile for x86_64 architecture and place the binary file in the android/src/main/jniLibs/x86_64 folder
+export CGO_LDFLAGS="-O2 -g -s -w -Wl,-z,max-page-size=16384"
+
 CGO_ENABLED=1 \
 GOOS=android \
 GOARCH=amd64 \
 CC=${NDK_BIN}/x86_64-linux-android21-clang \
-go build -buildmode=c-shared -o ${ANDROID_OUT}/x86_64/libmweb.so .
+go build -v -buildmode=c-shared -o ${ANDROID_OUT}/x86_64/libmweb.so .
 
 # Compile for arm64 architecture and place the binary file in the android/src/main/jniLibs/arm64-v8a folder
 CGO_ENABLED=1 \
 GOOS=android \
 GOARCH=arm64 \
 CC=${NDK_BIN}/aarch64-linux-android21-clang \
-go build -buildmode=c-shared -o ${ANDROID_OUT}/arm64-v8a/libmweb.so .
+go build -v -buildmode=c-shared -o ${ANDROID_OUT}/arm64-v8a/libmweb.so .
 
 # Compile for armv7a architecture and place the binary file in the android/src/main/jniLibs/armeabi-v7a folder
 CGO_ENABLED=1 \
 GOOS=android \
 GOARCH=arm \
 CC=${NDK_BIN}/armv7a-linux-androideabi21-clang \
-go build -buildmode=c-shared -o ${ANDROID_OUT}/armeabi-v7a/libmweb.so .
+go build -v -buildmode=c-shared -o ${ANDROID_OUT}/armeabi-v7a/libmweb.so .
 cd ../
 dart run ffigen --config ffigen_config.yaml
