f748878bdab4921fe4dab6aff36f19d58619083d
Blazebrain <davidadegoke16@gmail.com>
2025-09-29 16:15:59 +0100

    feat: Add BaseTransactionHistory and BaseTransactionInfo classes for handling base cryptocurrency transaction data

---
+++ cw_base/lib/base_transaction_info.dart
@@ -0,0 +1,41 @@
+import 'package:cw_core/transaction_direction.dart';
+import 'package:cw_evm/evm_chain_transaction_info.dart';
+
+class BaseTransactionInfo extends EVMChainTransactionInfo {
+  BaseTransactionInfo({
+    required super.id,
+    required super.height,
+    required super.ethAmount,
+    required super.ethFee,
+    required super.tokenSymbol,
+    required super.direction,
+    required super.isPending,
+    required super.date,
+    required super.confirmations,
+    required super.to,
+    required super.from,
+    super.contractAddress,
+    super.exponent,
+  });
+
+  factory BaseTransactionInfo.fromJson(Map<String, dynamic> data) {
+    return BaseTransactionInfo(
+      id: data['id'] as String,
+      height: data['height'] as int,
+      ethAmount: BigInt.parse(data['amount']),
+      exponent: data['exponent'] as int,
+      ethFee: BigInt.parse(data['fee']),
+      direction: parseTransactionDirectionFromInt(data['direction'] as int),
+      date: DateTime.fromMillisecondsSinceEpoch(data['date'] as int),
+      isPending: data['isPending'] as bool,
+      confirmations: data['confirmations'] as int,
+      tokenSymbol: data['tokenSymbol'] as String,
+      to: data['to'],
+      from: data['from'],
+      contractAddress: data['contractAddress'],
+    );
+  }
+
+  @override
+  String get feeCurrency => 'BASE';
+}
