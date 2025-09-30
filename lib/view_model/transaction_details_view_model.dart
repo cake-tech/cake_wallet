78e426461d5d58a79d371120bca42d6300fbdd9d
Blazebrain <davidadegoke16@gmail.com>
2025-09-30 06:14:22 +0100

    feat: Add base support to transaction details and transaction list item

--- lib/view_model/transaction_details_view_model.dart
+++ lib/view_model/transaction_details_view_model.dart
@@ -73,6 +73,9 @@ abstract class TransactionDetailsViewModelBase with Store {
       case WalletType.polygon:
         _addPolygonListItems(tx, dateFormat);
         break;
+      case WalletType.base:
+        _addBaseListItems(tx, dateFormat);
+        break;
       case WalletType.solana:
         _addSolanaListItems(tx, dateFormat);
         break;
@@ -198,6 +201,8 @@ abstract class TransactionDetailsViewModelBase with Store {
         return 'https://${wallet.isTestnet ? "testnet" : "dcrdata"}.decred.org/tx/${txId.split(':')[0]}';
       case WalletType.dogecoin:
         return 'https://blockchair.com/dogecoin/transaction/${txId}';
+      case WalletType.base:
+        return 'https://basescan.org/tx/${txId}';
       case WalletType.none:
         return '';
     }
@@ -233,6 +238,8 @@ abstract class TransactionDetailsViewModelBase with Store {
         return S.current.view_transaction_on + 'explorer.zano.org';
       case WalletType.decred:
         return S.current.view_transaction_on + 'dcrdata.decred.org';
+      case WalletType.base:
+        return S.current.view_transaction_on + 'basescan.org';
       case WalletType.none:
         return '';
     }
@@ -930,5 +937,55 @@ abstract class TransactionDetailsViewModelBase with Store {
       if (comment != null && comment.isNotEmpty)
         StandartListItem(title: S.current.transaction_details_title, value: comment),
     ]);
+  }
+
+  void _addBaseListItems(TransactionInfo tx, DateFormat dateFormat) {
+    final _items = [
+      StandartListItem(
+        title: S.current.transaction_details_transaction_id,
+        value: tx.txHash,
+        key: ValueKey('standard_list_item_transaction_details_id_key'),
+      ),
+      StandartListItem(
+        title: S.current.transaction_details_date,
+        value: dateFormat.format(tx.date),
+        key: ValueKey('standard_list_item_transaction_details_date_key'),
+      ),
+      StandartListItem(
+        title: S.current.confirmations,
+        value: tx.confirmations.toString(),
+        key: ValueKey('standard_list_item_transaction_confirmations_key'),
+      ),
+      StandartListItem(
+        title: S.current.transaction_details_height,
+        value: '${tx.height}',
+        key: ValueKey('standard_list_item_transaction_details_height_key'),
+      ),
+      StandartListItem(
+        title: S.current.transaction_details_amount,
+        value: tx.amountFormatted(),
+        key: ValueKey('standard_list_item_transaction_details_amount_key'),
+      ),
+      if (tx.feeFormatted()?.isNotEmpty ?? false)
+        StandartListItem(
+          title: S.current.transaction_details_fee,
+          value: tx.feeFormatted()!,
+          key: ValueKey('standard_list_item_transaction_details_fee_key'),
+        ),
+      if (showRecipientAddress && tx.to != null && tx.direction == TransactionDirection.outgoing)
+        StandartListItem(
+          title: S.current.transaction_details_recipient_address,
+          value: tx.to!,
+          key: ValueKey('standard_list_item_transaction_details_recipient_address_key'),
+        ),
+      if (tx.direction == TransactionDirection.incoming && tx.from != null)
+        StandartListItem(
+          title: S.current.transaction_details_source_address,
+          value: tx.from!,
+          key: ValueKey('standard_list_item_transaction_details_source_address_key'),
+        ),
+    ];
+
+    items.addAll(_items);
   }
 }
