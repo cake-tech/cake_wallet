358b520ad54652f5d1c1063e3682ffa423ae9142
cyan <cyjan@mrcyjanek.net>
2025-10-02 14:12:45 +0000

    fix: use bash for bitbox build script

--- scripts/build_bitbox_flutter.sh
+++ scripts/build_bitbox_flutter.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 git clone https://github.com/konstantinullrich/bitbox_flutter
 cd bitbox_flutter
-./build_bindings.sh --dont-install
+bash ./build_bindings.sh --dont-install
 
 FILE=api.aar
 if [ -f "$FILE" ]; then
