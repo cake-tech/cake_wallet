813a9734dfcac3b26c30e4fd617a76bf8ed25129
tuxsudo <tuxsudo@tux.pizza>
2025-10-09 13:41:19 -0400

    Update lib/src/screens/dashboard/desktop_widgets/desktop_wallet_selection_dropdown.dart
    Co-authored-by: Omar Hatem <omarh.ismail1@gmail.com>

--- lib/src/screens/dashboard/desktop_widgets/desktop_wallet_selection_dropdown.dart
+++ lib/src/screens/dashboard/desktop_widgets/desktop_wallet_selection_dropdown.dart
@@ -45,7 +45,7 @@ class _DesktopWalletSelectionDropDownState extends State<DesktopWalletSelectionD
   final wowneroIcon = Image.asset('assets/images/crypto/wownero.png', height: 24, width: 24);
   final zanoIcon = Image.asset('assets/images/crypto/zano.png', height: 24, width: 24);
   final decredIcon = Image.asset('assets/images/crypto/decred.png', height: 24, width: 24);
-  final dogeIcon = Image.asset('assets/images/dogecoin.png', height: 24, width: 24);
+  final dogeIcon = Image.asset('assets/images/crypto/dogecoin.png', height: 24, width: 24);
   final nonWalletTypeIcon = Image.asset('assets/images/close.png', height: 24, width: 24);
 
   Image _newWalletImage(BuildContext context) => Image.asset(
