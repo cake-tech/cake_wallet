66b297b02969a90054cd2451e0991a85b9aa3228
cyan <cyjan@mrcyjanek.net>
2025-10-14 01:33:11 +0000

    ci: don't execute unnecessary steps

--- scripts/build_bitbox_flutter.sh
+++ scripts/build_bitbox_flutter.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/konstantinullrich/bitbox_flutter
 cd bitbox_flutter
-bash ./build_bindings.sh
+bash ./build_bindings.sh --dont-install
 
 FILE=api.aar
 if [ -f "$FILE" ]; then
