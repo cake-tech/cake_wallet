068adeef963a2e7c0a47e2c92c34d0dcbfb1368b
Konstantin Ullrich <konstantinullrich12@gmail.com>
2025-10-30 16:26:56 +0100

    feat: enhance address validation with Lightning Network invoice support for BTC & refactor wallet type/token checks in view model

--- cw_bitcoin/lib/lightning/lightning_addres_type.dart
+++ cw_bitcoin/lib/lightning/lightning_addres_type.dart
@@ -4,6 +4,8 @@ class LightningAddressType implements BitcoinAddressType {
   const LightningAddressType._(this.value);
   static const LightningAddressType p2l = LightningAddressType._("Lightning");
 
+  static const String Bolt11InvoiceMatcher = r'^(lightning:)?(lnbc|lntb|lnbs|lnbcrt)[a-z0-9]+$';
+
   @override
   bool get isP2sh => false;
   @override
--- cw_bitcoin/test/cw_bitcoin_test.dart
+++ cw_bitcoin/test/cw_bitcoin_test.dart
@@ -1,12 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cw_bitcoin/cw_bitcoin.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('lightning matchers', () {
+    final RegExp lightningInvoiceRegex =
+        RegExp(r'^(lightning:)?(lnbc|lntb|lnbs|lnbcrt)[a-z0-9]+$', caseSensitive: false);
+
+    test('Valid invoice', () {
+      final content =
+          "lnbc2500u1pvjluezpp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdq5xysxxatsyp3k7enxv4jsxqzpuaztrnwngzn3kdzw508d6qejxtdg4y5r3zarvary0c5xw7kpqdxssqfsqqqyqqqqlgqqqqqeqqjq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgq9qrsgqfsqqqyqqqqlgqqqqqeqqjq9qrsgq";
+      expect(lightningInvoiceRegex.hasMatch(content), true);
+    });
+    test('Valid invoice with prefix', () {
+      final content =
+          "lightning:lnbc2500u1pvjluezpp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdq5xysxxatsyp3k7enxv4jsxqzpuaztrnwngzn3kdzw508d6qejxtdg4y5r3zarvary0c5xw7kpqdxssqfsqqqyqqqqlgqqqqqeqqjq9qrsgq";
+      expect(lightningInvoiceRegex.hasMatch(content), true);
+    });
+    test('Invalid invoice', () {
+      final content = "bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq"; // This is a Bitcoin address
+      expect(lightningInvoiceRegex.hasMatch(content), false);
+    });
   });
 }
