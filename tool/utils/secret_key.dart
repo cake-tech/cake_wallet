2804227b63b15c5bc4a822e53fd9003b746b3435
OmarHatem <omarh.ismail1@gmail.com>
2025-11-01 13:28:12 +0100

    Add bitcoin secrets config [skip ci]

--- tool/utils/secret_key.dart
+++ tool/utils/secret_key.dart
@@ -108,6 +108,10 @@ class SecretKey {
     SecretKey('tronNowNodesApiKey', () => ''),
   ];
 
+  static final bitcoinSecrets = [
+    SecretKey('breezApiKey', () => ''),
+  ];
+
   final String name;
   final String Function() generate;
 }
