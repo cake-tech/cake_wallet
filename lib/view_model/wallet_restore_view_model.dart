002041ac1aca02914b9e43de25418fb624e229f3
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-11-06 08:54:35 +0100

    wip: decred passphrase

--- lib/view_model/wallet_restore_view_model.dart
+++ lib/view_model/wallet_restore_view_model.dart
@@ -235,6 +235,7 @@ abstract class WalletRestoreViewModelBase extends WalletCreationVM with Store {
               name: name,
               mnemonic: seed,
               password: password,
+              passphrase: passphrase??'',
           );
         case WalletType.none:
         case WalletType.haven:
@@ -297,7 +298,7 @@ abstract class WalletRestoreViewModelBase extends WalletCreationVM with Store {
           return arbitrum!.createArbitrumRestoreWalletFromPrivateKey(
             name: name,
             password: password,
-            privateKey: options['private_key'] as String,
+            privateKey: options?['private_key'] as String,
           );
         case WalletType.solana:
           return solana!.createSolanaRestoreWalletFromPrivateKey(
