aadaba84add9ff1ad574fad6b8500cf9af4be001
Konstantin Ullrich <konstantinullrich12@gmail.com>
2025-11-06 09:53:07 +0100

    feat: improve address formatting for human-readable addresses and update the default LNURL domain

--- cw_bitcoin/lib/bitcoin_wallet.dart
+++ cw_bitcoin/lib/bitcoin_wallet.dart
@@ -98,7 +98,7 @@ abstract class BitcoinWalletBase extends ElectrumWallet with Store {
       lightningWallet = LightningWallet(
         mnemonic: mnemonic,
         apiKey: secrets.breezApiKey,
-        lnurlDomain: "breez.tips",
+        lnurlDomain: "cake.cash",
       );
     } else {
       lightningWallet = null;
