839ed64fa57433823ba81350c1847f00775e11aa
Robert Malikowski <malikowskirobert@gmail.com>
2025-11-16 21:31:42 +0100

    minor layout fixes

--- lib/new-ui/widgets/coins_page/action_row/coin_action_row.dart
+++ lib/new-ui/widgets/coins_page/action_row/coin_action_row.dart
@@ -1,17 +1,4 @@
-import 'package:cake_wallet/bitcoin/bitcoin.dart';
-import 'package:cake_wallet/core/open_crypto_pay/open_cryptopay_service.dart';
-import 'package:cake_wallet/di.dart';
-import 'package:cake_wallet/entities/qr_scanner.dart';
-import 'package:cake_wallet/generated/i18n.dart';
-import 'package:cake_wallet/main.dart';
 import 'package:cake_wallet/new-ui/pages/send_page.dart';
-import 'package:cake_wallet/new-ui/pages/swap_page.dart';
-import 'package:cake_wallet/routes.dart';
-import 'package:cake_wallet/utils/feature_flag.dart';
-import 'package:cake_wallet/utils/payment_request.dart';
-import 'package:cake_wallet/view_model/send/send_view_model.dart';
-import 'package:cake_wallet/view_model/wallet_address_list/wallet_address_list_view_model.dart';
-import 'package:cw_core/unspent_coin_type.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -20,138 +7,59 @@ import '../../../pages/scan_page.dart';
 import 'coin_action_button.dart';
 
 class CoinActionRow extends StatelessWidget {
-  const CoinActionRow({super.key, this.lightningMode = false});
-
-  final bool lightningMode;
+  const CoinActionRow({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18.0),
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
       child: Row(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.center,
         spacing: MediaQuery.of(context).size.width * 0.05,
         children: [
           CoinActionButton(
-            icon: SvgPicture.asset(
-              "assets/new-ui/send.svg",
-              colorFilter: ColorFilter.mode(
-                Theme.of(context).colorScheme.primary,
-                BlendMode.srcIn,
-              ),
-            ),
-            label: S.of(context).send,
+            icon: SvgPicture.asset("assets/new-ui/send.svg"),
+            label: "Send",
             action: () {
-              if (FeatureFlag.hasNewUiExtraPages) {
-                showModalBottomSheet(
-                  context: context,
-                  builder: (context) => SendPage(),
-                );
-              } else {
-                Map<String, dynamic>? args;
-                if (lightningMode) args = {'coinTypeToSpendFrom' : UnspentCoinType.lightning};
-                Navigator.of(context).pushNamed(Routes.send, arguments: args);
-              }
+              showModalBottomSheet(
+                context: context,
+                builder: (context) => SendPage(),
+              );
             },
           ),
           CoinActionButton(
-            icon: SvgPicture.asset(
-              "assets/new-ui/receive.svg",
-              colorFilter: ColorFilter.mode(
-                Theme.of(context).colorScheme.primary,
-                BlendMode.srcIn,
-              ),
-            ),
-            label: S.of(context).receive,
-            action: () async {
-              if (FeatureFlag.hasNewUiExtraPages) {
-                showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  builder: (context) => FractionallySizedBox(
-                    heightFactor: 0.9,
-                    child: ReceivePage(),
-                  ),
-                );
-              } else {
-                // ToDo: (Konsti) refactor as part of the derivation PR (I hate myself for it)
-                if (lightningMode) {
-                  await getIt<WalletAddressListViewModel>().setAddressType(
-                      bitcoin!.getOptionToType(bitcoin!.getBitcoinLightningReceivePageOption()));
-                } else {
-                  await getIt<WalletAddressListViewModel>().setAddressType(
-                      bitcoin!.getOptionToType(bitcoin!.getBitcoinSegwitPageOption()));
-                }
-                Navigator.of(context).pushNamed(Routes.addressPage);
-              }
-            },
-          ),
-          CoinActionButton(
-            icon: SvgPicture.asset(
-              "assets/new-ui/exchange.svg",
-              colorFilter: ColorFilter.mode(
-                Theme.of(context).colorScheme.primary,
-                BlendMode.srcIn,
-              ),
-            ),
-            label: S.of(context).swap,
+            icon: SvgPicture.asset("assets/new-ui/receive.svg"),
+            label: "Receive",
             action: () {
-              if (FeatureFlag.hasNewUiExtraPages) {
-                showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  builder: (context) => FractionallySizedBox(
-                    heightFactor: 0.9,
-                    child: SwapPage(),
-                  ),
-                );
-              } else {
-                Navigator.of(context).pushNamed(Routes.exchange);
-              }
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                builder: (context) => FractionallySizedBox(
+                  heightFactor: 0.9,
+                  child: ReceivePage(),
+                ),
+              );
             },
           ),
           CoinActionButton(
-            icon: SvgPicture.asset(
-              "assets/new-ui/scan.svg",
-              colorFilter: ColorFilter.mode(
-                Theme.of(context).colorScheme.primary,
-                BlendMode.srcIn,
-              ),
-            ),
+            icon: SvgPicture.asset("assets/new-ui/exchange.svg"),
+            label: "Swap",
+            action: () {},
+          ),
+          CoinActionButton(
+            icon: SvgPicture.asset("assets/new-ui/scan.svg"),
             label: "Scan",
-            action: () => _onPressedScan(context),
+            action: () {
+              showModalBottomSheet(
+                context: context,
+
+                builder: (context) => ScanPage(),
+              );
+            },
           ),
         ],
       ),
     );
-  }
-
-  Future<void> _onPressedScan(BuildContext context) async {
-    if (FeatureFlag.hasNewUiExtraPages) {
-      showModalBottomSheet(
-        context: context,
-        isScrollControlled: true,
-        builder: (context) => FractionallySizedBox(
-          heightFactor: 0.9,
-          child: ScanPage(),
-        ),
-      );
-    } else {
-      final code = await presentQRScanner(context);
-
-      if (code == null || code.isEmpty) return;
-
-      if (SendViewModelBase.isNonZeroAmountLightningInvoice(code) ||
-          OpenCryptoPayService.isOpenCryptoPayQR(code)) {
-        Navigator.of(context).pushNamed(Routes.send,
-            arguments: {"paymentRequest": PaymentRequest(code, "", "", "", "")});
-        return;
-      }
-
-      final uri = Uri.tryParse(code);
-      if (uri == null) return;
-      rootKey.currentState?.handleDeepLinking(uri);
-    };
   }
 }
