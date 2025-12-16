16256258732beea924235049fb21705532c4bc5f
Robert Malikowski <malikowskirobert@gmail.com>
2025-12-16 21:15:35 +0100

    move settings page to generic row impl

--- lib/new-ui/pages/settings_page.dart
+++ lib/new-ui/pages/settings_page.dart
@@ -1,7 +1,11 @@
+import 'package:cake_wallet/entities/new_ui_entities/list_item/list_item.dart';
+import 'package:cake_wallet/entities/new_ui_entities/list_item/list_item_regular_row.dart';
 import 'package:cake_wallet/new-ui/widgets/receive_page/receive_top_bar.dart';
 import 'package:cake_wallet/router.dart';
 import 'package:cake_wallet/routes.dart';
+import 'package:cake_wallet/src/widgets/new_list_row/new_list_section.dart';
 import 'package:cake_wallet/src/widgets/section_divider.dart';
+import 'package:cake_wallet/view_model/settings/regular_list_item.dart';
 import 'package:cw_core/utils/print_verbose.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -29,15 +33,19 @@ class SettingsSectionData {
     SettingsListItem("assets/new-ui/settings_row_icons/privacy.svg", "Privacy features", ""),
     SettingsListItem("assets/new-ui/settings_row_icons/seed.svg", "Seed & keys", Routes.seed,
         routeArgs: true),
-    SettingsListItem("assets/new-ui/settings_row_icons/other.svg", "Other", Routes.otherSettingsPage),
+    SettingsListItem(
+        "assets/new-ui/settings_row_icons/other.svg", "Other", Routes.otherSettingsPage),
   ]);
 
   static const SettingsSectionData appSettings =
       SettingsSectionData("App Settings", "assets/new-ui/app_settings.svg", [
-    SettingsListItem("assets/new-ui/settings_row_icons/connections.svg", "Connections", Routes.connectionSync),
+    SettingsListItem(
+        "assets/new-ui/settings_row_icons/connections.svg", "Connections", Routes.connectionSync),
     SettingsListItem("assets/new-ui/settings_row_icons/defaults.svg", "Defaults", ""),
-    SettingsListItem("assets/new-ui/settings_row_icons/display.svg", "Display", Routes.displaySettingsPage),
-    SettingsListItem("assets/new-ui/settings_row_icons/security.svg", "Privacy & Security", Routes.privacyPage),
+    SettingsListItem(
+        "assets/new-ui/settings_row_icons/display.svg", "Display", Routes.displaySettingsPage),
+    SettingsListItem(
+        "assets/new-ui/settings_row_icons/security.svg", "Privacy & Security", Routes.privacyPage),
     SettingsListItem("assets/new-ui/settings_row_icons/backup.svg", "Backup", Routes.backup),
   ]);
 
@@ -58,9 +66,6 @@ class NewSettingsPage extends StatefulWidget {
 
 class _NewSettingsPageState extends State<NewSettingsPage> {
   final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -96,7 +101,7 @@ class _NewSettingsPageState extends State<NewSettingsPage> {
               }
             },
             child: Navigator(
-              key: _navigatorKey,
+                key: _navigatorKey,
                 observers: [HeroController()],
                 onGenerateRoute: (settings) {
                   printV(settings.name);
@@ -119,135 +124,36 @@ class SettingsMainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Map<String, List<ListItem>> sections = Map.fromEntries(SettingsSectionData.all.map((section) =>
+        MapEntry(
+            section.title,
+            section.items
+                .map((item) => ListItemRegularRow(
+                    keyValue: item.title, label: item.title, iconPath: item.iconPath, onTap: (){
+              if (item.route.isNotEmpty) {
+                Navigator.of(context).pushNamed(item.route, arguments: item.routeArgs);
+              }
+
+            }))
+                .toList())));
+
     return SingleChildScrollView(
       physics: ClampingScrollPhysics(),
       controller: ModalScrollController.of(context),
-      child: Column(
-        children: [
-          ModalTopBar(
-            title: "Settings",
-            leadingIcon: Icon(Icons.close),
-            onLeadingPressed: Navigator.of(context, rootNavigator: true).pop,
-            onTrailingPressed: () {},
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20.0),
-            child: Column(
-                spacing: 24.0,
-                children: SettingsSectionData.all
-                    .map((item) => Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                          child: SettingsSection(
-                            data: item,
-                          ),
-                        ))
-                    .toList()),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SettingsSection extends StatelessWidget {
-  const SettingsSection({super.key, required this.data});
-
-  final SettingsSectionData data;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            if (data.titleIconPath.isNotEmpty)
-              SvgPicture.asset(
-                data.titleIconPath,
-                width: 20,
-                height: 20,
-                colorFilter: ColorFilter.mode(
-                    Theme.of(context).colorScheme.onSurfaceVariant, BlendMode.srcIn),
-              ),
-            SizedBox(width: 6),
-            if (data.title.isNotEmpty)
-              Text(
-                data.title,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-              )
-          ],
+      child: Column(children: [
+        ModalTopBar(
+          title: "Settings",
+          leadingIcon: Icon(Icons.close),
+          onLeadingPressed: Navigator.of(context, rootNavigator: true).pop,
+          onTrailingPressed: () {},
         ),
-        if (data.titleIconPath.isNotEmpty && data.title.isNotEmpty) SizedBox(height: 20),
-        ...data.items.map((item) => SettingsRow(
-              item: item,
-              roundedTop: item == data.items.first,
-              roundedBottom: item == data.items.last,
-            ))
-      ],
-    );
-  }
-}
-
-class SettingsRow extends StatelessWidget {
-  const SettingsRow(
-      {super.key, required this.item, required this.roundedTop, required this.roundedBottom});
-
-  final SettingsListItem item;
-  final bool roundedTop;
-  final bool roundedBottom;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 50,
-          child: Material(
-            color: Theme.of(context).colorScheme.surfaceContainer,
-              borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(roundedTop ? 10 : 0),
-                  bottom: Radius.circular(roundedBottom ? 10 : 0)),
-            child: InkWell(
-              onTap: () {
-                if (item.route.isNotEmpty) {
-                  Navigator.of(context).pushNamed(item.route, arguments: item.routeArgs);
-                }
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right: 12.0),
-                          child: SvgPicture.asset(item.iconPath),
-                        ),
-                        Text(item.title)
-                      ],
-                    ),
-                    SvgPicture.asset(
-                      "assets/new-ui/arrow_right.svg",
-                      colorFilter: ColorFilter.mode(
-                          Theme.of(context).colorScheme.onSurfaceVariant, BlendMode.srcIn),
-                    )
-                  ],
-                ),
-              ),
-            ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+          child: NewListSections(
+            sections: sections,
           ),
         ),
-        if (!roundedBottom)
-          Container(
-            color: Theme.of(context).colorScheme.surfaceContainer,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 18.0),
-              child: HorizontalSectionDivider(),
-            ),
-          )
-      ],
+      ]),
     );
   }
 }
