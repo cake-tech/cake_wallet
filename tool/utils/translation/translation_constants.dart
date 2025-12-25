d5cbf09c114e4bc56abf910d40a97628670421b8
Yousef Ebrahimi <yousefbang@gmail.com>
2025-12-25 17:21:11 +0000

    Update translation_constants.dart
    adding fa as a new language (Persian - Farsi)

--- tool/utils/translation/translation_constants.dart
+++ tool/utils/translation/translation_constants.dart
@@ -1,6 +1,6 @@
 const defaultLang = "en";
 const langs = [
-  "ar", "bg", "cs", "de", "en", "es", "fr", "gn", "ha", "hi", "hr", "hy", "id", "it",
+  "ar", "bg", "cs", "de", "en", "es", "fa", "fr", "gn", "ha", "hi", "hr", "hy", "id", "it",
   "ja", "ko", "my", "nl", "pl", "pt", "ru", "th", "tl", "tr", "uk", "ur", "yo",
   "vi", "zh-cn" // zh, but Google Translate uses zh-cn for Chinese (Simplified)
 ];
