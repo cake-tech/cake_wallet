3801892b4cfece38019aedd7f513a1855a9df467
tuxsudo <tuxsudo@tux.pizza>
2026-01-15 13:55:26 -0500

    Return bottom gradient

--- lib/new-ui/new_dashboard.dart
+++ lib/new-ui/new_dashboard.dart
@@ -4,6 +4,7 @@ import 'package:cake_wallet/src/screens/contact/contact_list_page.dart';
 import 'package:cake_wallet/src/screens/dashboard/pages/cake_features_page.dart';
 import 'package:cake_wallet/src/screens/dashboard/widgets/new_main_navbar_widget.dart';
 import 'package:cake_wallet/src/screens/wallet_list/wallet_list_page.dart';
+import 'package:cake_wallet/themes/core/theme_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:progressive_blur/progressive_blur.dart';
 import '../view_model/dashboard/dashboard_view_model.dart';
@@ -32,16 +33,25 @@ class _NewDashboardState extends State<NewDashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        alignment: Alignment.bottomCenter,
         children: [
-          ProgressiveBlurWidget(
-            sigma: 10.0,
-            linearGradientBlur: const LinearGradientBlur(
-              values: [0, 0.5, 1],
-              stops: [0, 0.9, 1],
-              start: Alignment.topCenter,
-              end: Alignment.bottomCenter,
+          widget.dashboardPageWidgets[_selectedPage],
+          Container(
+            height: 150,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[
+                  context.customColors.backgroundGradientColor.withAlpha(5),
+                  context.customColors.backgroundGradientColor.withAlpha(50),
+                  context.customColors.backgroundGradientColor.withAlpha(100),
+                  context.customColors.backgroundGradientColor.withAlpha(150),
+                  context.customColors.backgroundGradientColor.withAlpha(200),
+                  context.customColors.backgroundGradientColor,
+                ],
+              ),
             ),
-            child: widget.dashboardPageWidgets[_selectedPage],
           ),
           NewMainNavBar(
             dashboardViewModel: widget.dashboardViewModel,
