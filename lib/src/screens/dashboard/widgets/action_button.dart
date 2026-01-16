9c180829fe1b62d5465a11c60389a56b1f4b0cd2
tuxsudo <tuxsudo@tux.pizza>
2026-01-15 21:31:08 -0500

    Add haptic feedback to correct ActionButton

--- lib/src/screens/dashboard/widgets/action_button.dart
+++ lib/src/screens/dashboard/widgets/action_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:vibration/vibration.dart';
 
 class ActionButton extends StatelessWidget {
   ActionButton({
@@ -29,7 +28,6 @@ class ActionButton extends StatelessWidget {
         tapTargetSize: MaterialTapTargetSize.shrinkWrap,
       ),
       onPressed: () {
-        HapticFeedback.lightImpact();
         if (route?.isNotEmpty ?? false) {
           Navigator.of(context, rootNavigator: true).pushNamed(route!);
         } else {
