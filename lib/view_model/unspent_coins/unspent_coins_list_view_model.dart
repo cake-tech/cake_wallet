6cb4ea30a1355d2975c530bfbf3bc2b4c6727812
Omar Hatem <omarh.ismail1@gmail.com>
2026-01-23 21:15:32 +0200

    Update lib/view_model/unspent_coins/unspent_coins_list_view_model.dart [skip ci]

--- lib/view_model/unspent_coins/unspent_coins_list_view_model.dart
+++ lib/view_model/unspent_coins/unspent_coins_list_view_model.dart
@@ -128,10 +128,9 @@ abstract class UnspentCoinsListViewModelBase with Store {
       existingInfo.isSending = item.isSending;
       existingInfo.note = item.note;
 
-      await existingInfo.save().then((value){
-        item.isBeingSaved = false;
-        isSavingItems = false;
-      });
+      await existingInfo.save();
+      item.isBeingSaved = false;
+      isSavingItems = false;
       _updateUnspentCoinsInfo();
     } catch (e) {
       printV('Error saving coin info: $e');
