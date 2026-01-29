828e0fb9e9a325607eb6d592562af5acc501d5d4
Robert Malikowski <malikowskirobert@gmail.com>
2026-01-29 16:20:35 +0100

    merge fixes

--- lib/new-ui/widgets/modal_header.dart
+++ lib/new-ui/widgets/modal_header.dart
@@ -11,16 +11,36 @@ class ModalHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-        decoration: BoxDecoration(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            spacing: 10,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SvgPicture.asset(iconPath, width:36,height:36),
-          ),
+    return Container(
+      decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surfaceContainer,
+          borderRadius: BorderRadius.circular(16)),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          spacing: 10,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SvgPicture.asset(iconPath, width: 36, height: 36),
+            Text(
+              title,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Theme.of(context).colorScheme.onSurface),
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: Text(
+                  message,
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      color: Theme.of(context).colorScheme.onSurfaceVariant),
+                  textAlign: TextAlign.center,
+                ))
+          ],
         ),
       ),
     );
