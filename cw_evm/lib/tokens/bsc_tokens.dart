d7cca42323261d89a687a7592b06009ff65f85a1
Blazebrain <davidadegoke16@gmail.com>
2026-02-05 11:31:11 +0100

    Add more default tokens

--- cw_evm/lib/tokens/bsc_tokens.dart
+++ cw_evm/lib/tokens/bsc_tokens.dart
@@ -6,21 +6,21 @@ class BSCTokens {
   static List<Erc20Token> get tokens {
     final tokens = [
       Erc20Token(
-        name: "Binance-Peg USD Coin",
+        name: "USD Coin",
         symbol: "USDC",
         contractAddress: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
         decimal: 18,
         enabled: true,
       ),
       Erc20Token(
-        name: "Binance-Peg Ethereum",
+        name: "Ethereum",
         symbol: "ETH",
         contractAddress: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
         decimal: 18,
         enabled: true,
       ),
       Erc20Token(
-        name: "Binance-Peg Tether USD",
+        name: "Tether USD",
         symbol: "USDT",
         contractAddress: "0x55d398326f99059fF775485246999027B3197955",
         decimal: 18,
@@ -37,6 +37,13 @@ class BSCTokens {
         name: "PancakeSwap Token",
         symbol: "CAKE",
         contractAddress: "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82",
+        decimal: 18,
+        enabled: false,
+      ),
+      Erc20Token(
+        name: "Cardano Token",
+        symbol: "ADA",
+        contractAddress: "0x3EE2200Efb3400fAbB9AacF31297cBdD1d435D47",
         decimal: 18,
         enabled: false,
       ),
