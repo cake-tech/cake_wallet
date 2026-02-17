1f7c1c4685df6612a0d6b3d512837fce70669189
Robert Malikowski <malikowskirobert@gmail.com>
2026-02-17 18:32:12 +0100

    parse -> tryParse

--- cw_core/lib/crypto_amount_format.dart
+++ cw_core/lib/crypto_amount_format.dart
@@ -29,7 +29,7 @@ extension MaxDecimals on String {
     final parts = replaceAll(",", "").split(".");
     if (parts.first.contains("< 0")) parts.first = "0";
 
-    return [formater.format(int.parse(parts.first)), ...parts.sublist(1)]
+    return [formater.format(int.tryParse(parts.first) ?? 0), ...parts.sublist(1)]
         .join(formater.symbols.DECIMAL_SEP);
   }
 }
