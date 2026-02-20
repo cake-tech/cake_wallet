cba557d33e6a102b18c6f65ff31efc18674ec97b
Konstantin Ullrich <konstantinullrich12@gmail.com>
2026-02-20 14:24:24 +0100

    fix: disable Lightning switcher for hardware wallets

--- lib/view_model/exchange/exchange_view_model.dart
+++ lib/view_model/exchange/exchange_view_model.dart
@@ -261,7 +261,7 @@ abstract class ExchangeViewModelBase extends WalletChangeListenerViewModel with 
 
   bool useSameWalletAddress(CryptoCurrency currency) =>
       currency == wallet.currency ||
-      currency == CryptoCurrency.btcln && wallet.currency == CryptoCurrency.btc ||
+      (currency == CryptoCurrency.btcln && wallet.currency == CryptoCurrency.btc && !wallet.isHardwareWallet) ||
       (currency.tag != null && currency.tag == wallet.currency.tag) ||
       currency.tag == wallet.currency.title;
 
