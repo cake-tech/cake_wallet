70b69984e9760068b94757ffb1be32304a4f9fce
Robert Malikowski <malikowskirobert@gmail.com>
2026-02-23 10:11:41 +0100

    fix support chat page

--- lib/src/screens/support_chat/support_chat_page.dart
+++ lib/src/screens/support_chat/support_chat_page.dart
@@ -1,39 +1,49 @@
 import 'package:cake_wallet/core/secure_storage.dart';
 import 'package:cake_wallet/generated/i18n.dart';
-import 'package:cake_wallet/src/screens/base_page.dart';
+import 'package:cake_wallet/new-ui/widgets/receive_page/receive_top_bar.dart';
 import 'package:cake_wallet/src/screens/support_chat/widgets/chatwoot_widget.dart';
 import 'package:cake_wallet/view_model/support_view_model.dart';
 import 'package:flutter/material.dart';
 
-class SupportChatPage extends BasePage {
+class SupportChatPage extends StatelessWidget {
   SupportChatPage(this.supportViewModel, {required this.secureStorage});
 
   final SupportViewModel supportViewModel;
   final SecureStorage secureStorage;
 
-  @override
-  String get title => S.current.settings_support;
 
   @override
-  AppBarStyle get appBarStyle => AppBarStyle.regular;
-
-  @override
-  Widget body(BuildContext context) => FutureBuilder<String>(
-        future: getCookie(),
-        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-          if (snapshot.hasData)
-            return ChatwootWidget(
-              secureStorage,
-              supportUrl: supportViewModel.fetchUrl(authToken: snapshot.data!),
-              appVersion: supportViewModel.appVersion,
-              fiatApiMode: supportViewModel.fiatApiMode,
-              walletType: supportViewModel.walletType,
-              walletSyncState: supportViewModel.walletSyncState,
-              builtInTorState: supportViewModel.builtInTorState,
-            );
-          return Container();
-        },
-      );
+  Widget build(BuildContext context) => Container(
+    color: Theme.of(context).colorScheme.surface,
+    child: SafeArea(
+      child: Column(
+        children: [
+              ModalTopBar(
+                  title: S.current.settings_support,
+                  leadingIcon: Icon(Icons.arrow_back_ios_new),
+                  onLeadingPressed: Navigator.of(context).pop),
+              FutureBuilder<String>(
+                future: getCookie(),
+                builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                  if (snapshot.hasData)
+                    return Expanded(
+                      child: ChatwootWidget(
+                        secureStorage,
+                        supportUrl: supportViewModel.fetchUrl(authToken: snapshot.data!),
+                        appVersion: supportViewModel.appVersion,
+                        fiatApiMode: supportViewModel.fiatApiMode,
+                        walletType: supportViewModel.walletType,
+                        walletSyncState: supportViewModel.walletSyncState,
+                        builtInTorState: supportViewModel.builtInTorState,
+                      ),
+                    );
+                  return Container();
+                },
+              ),
+        ],
+      ),
+    ),
+  );
 
   Future<String> getCookie() async =>
       await secureStorage.read(key: COOKIE_KEY) ?? "";
