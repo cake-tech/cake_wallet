fc94d002bc8adf85cb5e211083c75ef7c66b9417
tuxsudo <tuxsudo@tux.pizza>
2026-02-25 21:44:55 -0500

    Conflict

--- lib/new-ui/widgets/coins_page/top_bar_widget/sync_bar.dart
+++ lib/new-ui/widgets/coins_page/top_bar_widget/sync_bar.dart
@@ -1,12 +1,13 @@
 import 'package:cake_wallet/core/sync_status_title.dart';
+import 'package:cake_wallet/di.dart';
 import 'package:cake_wallet/new-ui/widgets/coins_page/top_bar_widget/pulsing_dot.dart';
-import 'package:cake_wallet/routes.dart';
-import 'package:cake_wallet/src/widgets/cake_image_widget.dart';
+import 'package:cake_wallet/src/screens/settings/manage_nodes_page.dart';
 import 'package:cake_wallet/view_model/dashboard/dashboard_view_model.dart';
 import 'package:cw_core/sync_status.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class SyncBar extends StatelessWidget {
   SyncBar({super.key, required this.dashboardViewModel, required this.isSyncHeavy});
@@ -45,24 +46,18 @@ class SyncBar extends StatelessWidget {
           child: Stack(
             alignment: Alignment.centerLeft,
             children: [
-              if (!_showFullBar())
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  spacing: 6,
-                  children: [
-                    if (dashboardViewModel.isTorEnabled)
-                      CakeImageWidget(
-                        imageUrl: "assets/new-ui/tor.svg",
-                        width: 20,
-                        height: 20,
-                      ),
-                    if (_showDot()) PulsingDot(),
-                  ],
-                ),
+              if (_showDot())
+                PulsingDot(),
               if (_showFullBar())
                 GestureDetector(
                   onTap: () {
-                    Navigator.of(context, rootNavigator: true).pushNamed(Routes.connectionSync);
+                    CupertinoScaffold.showCupertinoModalBottomSheet(
+                      context: context,
+                      barrierColor: Colors.black.withAlpha(85),
+                      builder: (context) => FractionallySizedBox(
+                          child: Material(
+                            child: getIt.get<ManageNodesPage>(param1: false),
+                          )));
                   },
                   child: AnimatedSwitcher(
                     duration: Duration(milliseconds: 100),
@@ -162,7 +157,7 @@ class SyncBar extends StatelessWidget {
       children.add(SvgPicture.asset(
         "assets/new-ui/mweb_sync.svg",
         colorFilter:
-            ColorFilter.mode(Theme.of(context).colorScheme.onSurfaceVariant, BlendMode.srcIn),
+        ColorFilter.mode(Theme.of(context).colorScheme.onSurfaceVariant, BlendMode.srcIn),
       ));
     }
     if (dashboardViewModel.hasSilentPayments) {
