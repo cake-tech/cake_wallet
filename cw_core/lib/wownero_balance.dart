9f00755653214fc99480f8fe025e124af0050d9b
Robert Malikowski <malikowskirobert@gmail.com>
2026-03-03 13:25:47 +0100

    fix wownero balance

--- cw_core/lib/wownero_balance.dart
+++ cw_core/lib/wownero_balance.dart
@@ -4,7 +4,7 @@ import 'package:cw_core/wownero_amount_format.dart';
 class WowneroBalance extends Balance {
   WowneroBalance({required this.fullBalance, required this.unlockedBalance, this.frozenBalance = 0})
       : formattedFrozenBalance = wowneroAmountToString(amount: frozenBalance),
-        super(BigInt.zero, BigInt.zero, frozen: BigInt.zero);
+        super.fromInt(unlockedBalance, fullBalance - unlockedBalance, frozen: frozenBalance);
 
   final int fullBalance;
   final int unlockedBalance;
