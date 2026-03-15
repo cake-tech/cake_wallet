defdd239c955c61dc273499153c9b823d20dc5c1
tuxsudo <tuxsudo@tux.pizza>
2026-03-14 22:39:46 -0400

    Increase top padding slightly more

--- lib/new-ui/widgets/coins_page/assets_history/assets_top_bar.dart
+++ lib/new-ui/widgets/coins_page/assets_history/assets_top_bar.dart
@@ -28,7 +28,7 @@ class AssetsTopBar extends StatelessWidget {
 
     return SliverToBoxAdapter(
       child: Padding(
-        padding: const EdgeInsets.only(top: 24.0, bottom: 0.0, left: 12.0, right: 18.0),
+        padding: const EdgeInsets.only(top: 32.0, bottom: 0.0, left: 12.0, right: 18.0),
         child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
