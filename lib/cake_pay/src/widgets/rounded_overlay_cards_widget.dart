8043dd2fc33bb611a413c46efb8d37354f9132e4
Serhii <17529954+serhii-bor@users.noreply.github.com>
2026-03-19 22:30:02 +0200

    fix Flexible widget issue

--- lib/cake_pay/src/widgets/rounded_overlay_cards_widget.dart
+++ lib/cake_pay/src/widgets/rounded_overlay_cards_widget.dart
@@ -12,35 +12,48 @@ class RoundedOverlayCards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
+
     return ClipRRect(
-      borderRadius:
-      BorderRadius.only(bottomLeft: Radius.circular(25.0), bottomRight: Radius.circular(25.0)),
+      borderRadius: const BorderRadius.only(
+        bottomLeft: Radius.circular(25.0),
+        bottomRight: Radius.circular(25.0),
+      ),
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
+          borderRadius: const BorderRadius.only(
             bottomLeft: Radius.circular(24),
             bottomRight: Radius.circular(24),
           ),
-          color: Theme.of(context).colorScheme.surfaceContainer
+          color: Theme.of(context).colorScheme.surfaceContainer,
         ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            ClipRRect(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(25.0), bottomRight: Radius.circular(25.0)),
+            Flexible(
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(25.0),
+                  bottomRight: Radius.circular(25.0),
+                ),
                 child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(24), bottomRight: Radius.circular(24)),
-                      color: Theme.of(context).colorScheme.surfaceContainerLow,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(24),
+                      bottomRight: Radius.circular(24),
                     ),
-                    constraints: BoxConstraints(
-                      maxHeight: screenHeight * 0.38,
-                    ),
-                    width: double.infinity,
-                    child: Flexible(child: topCardChild))),
-            Flexible(child: bottomCardChild),
+                    color: Theme.of(context).colorScheme.surfaceContainerLow,
+                  ),
+                  constraints: BoxConstraints(
+                    maxHeight: screenHeight * 0.38,
+                  ),
+                  width: double.infinity,
+                  child: topCardChild,
+                ),
+              ),
+            ),
+            Flexible(
+              child: bottomCardChild,
+            ),
           ],
         ),
       ),
