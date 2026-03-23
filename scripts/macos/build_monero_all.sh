e83afcbb4dc378d1422f83341a99d668fc52da44
Czarek Nakamoto <cyjan@mrcyjanek.net>
2026-03-23 15:34:43 +0100

    fix: macos builds

--- scripts/macos/build_monero_all.sh
+++ scripts/macos/build_monero_all.sh
@@ -2,6 +2,7 @@
 set -x -e
 
 cd "$(dirname "$0")"
+../prepare_moneroc.sh
 
 NPROC="-j$(sysctl -n hw.logicalcpu)"
 MONERO_LIBS=""
@@ -9,7 +10,6 @@ WOWNERO_LIBS=""
 monero_c_tag=$(cd ../monero_c/; git describe --tags)
 MONEROC_RELEASE_DIR="../monero_c/release/${monero_c_tag}"
 
-../prepare_moneroc.sh
 
 # NOTE: -j1 is intentional. Otherwise you will run into weird behaviour on macos
 if [[ ! "x$USE_DOCKER" == "x" ]];
@@ -22,29 +22,26 @@ then
         popd
     done
 else
-	ARCHS=(x86_64 arm64)
+    ARCHS=(x86_64 arm64)
     for COIN in monero wownero;
     do
         MONERO_LIBS=""
         WOWNERO_LIBS=""
-	for ARCH in "${ARCHS[@]}";
-	do
-	    if [[ "$ARCH" == "arm64" ]]; then
-		HOST="aarch64-apple-darwin"
-	    else
-		HOST="x86_64-apple-darwin"
-	    fi
-
-            MONERO_LIBS="$MONERO_LIBS -arch ${ARCH} ${MONEROC_RELEASE_DIR}/${HOST}/libmonero_libwallet2_api_c.dylib"
-            WOWNERO_LIBS="$WOWNERO_LIBS -arch ${ARCH} ${MONEROC_RELEASE_DIR}/${HOST}/libwownero_libwallet2_api_c.dylib"
-
-            pushd ../monero_c
+    for ARCH in "${ARCHS[@]}";
+    do
+        if [[ "$ARCH" == "arm64" ]]; then
+            HOST="aarch64-apple-darwin"
+        else
+            HOST="x86_64-apple-darwin"
+        fi
+        MONERO_LIBS="$MONERO_LIBS -arch ${ARCH} ${MONEROC_RELEASE_DIR}/${HOST}/libmonero_wallet2_api_c.dylib"
+        WOWNERO_LIBS="$WOWNERO_LIBS -arch ${ARCH} ${MONEROC_RELEASE_DIR}/${HOST}/libwownero_wallet2_api_c.dylib"
+        pushd ../monero_c
                 ./build_single.sh ${COIN} ${HOST} -j$MAKE_JOB_COUNT
-                unxz -f ./release/${COIN}/${HOST}_libwallet2_api_c.dylib.xz
-	    popd
-	done
+        popd
+    done
     done
 fi
 
-lipo -create ${MONERO_LIBS} -output "${MONEROC_RELEASE_DIR}/host-apple-darwin_libwallet2_api_c.dylib"
-lipo -create ${WOWNERO_LIBS} -output "${WOWNEROC_RELEASE_DIR}/host-apple-darwin_libwallet2_api_c.dylib"
+lipo -create ${MONERO_LIBS} -output "${MONEROC_RELEASE_DIR}/../../../../macos/libmonero_wallet2_api_c.dylib"
+lipo -create ${WOWNERO_LIBS} -output "${MONEROC_RELEASE_DIR}/../../../../macos/libwownero_wallet2_api_c.dylib"
