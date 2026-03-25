90e42a201680dc33a18cd59adb3a120c1cb173a7
Konstantin Ullrich <konstantinullrich12@gmail.com>
2026-03-25 07:46:53 +0100

    fix: remove redundant raw string mark from amount validation pattern

--- lib/core/amount_validator.dart
+++ lib/core/amount_validator.dart
@@ -79,7 +79,7 @@ class DecimalAmountValidator extends TextValidator {
         );
 
   static String _pattern(Currency currency) =>
-      r'^([0-9]+([.\,][0-9]{1,${currency.decimals}})?|[.\,][0-9]{1,${currency.decimals}})\$';
+      '^([0-9]+([.\,][0-9]{1,${currency.decimals}})?|[.\,][0-9]{1,${currency.decimals}})\$';
 }
 
 class AllAmountValidator extends TextValidator {
