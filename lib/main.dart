6eb86d2801675e6e004343f19ffab4475bc3179e
Konstiantin Ullrich <konstantin@cakewallet.com>
2026-05-06 15:59:22 +0200

    fix: Restrict Windows platform from using Zcash and Lightning features; update Lightning availability checks.

--- lib/main.dart
+++ lib/main.dart
@@ -165,12 +165,17 @@ Future<void> runAppWithZone({Key? topLevelKey}) async {
     if (FeatureFlag.hasDevOptions) {
       ProxyWrapper.logger = MemoryProxyLogger();
     }
-    var zcashPassword = await secureStorageShared.read(key: "com.cakewallet.cw_zcash/zec.db");
-    if (zcashPassword == null || zcashPassword.isEmpty) {
-      zcashPassword = generateKey().substring(0, 32);
-      secureStorageShared.write(key: "com.cakewallet.cw_zcash/zec.db", value: zcashPassword);
+
+    if (!Platform.isWindows) {
+      var zcashPassword = await secureStorageShared.read(
+          key: "com.cakewallet.cw_zcash/zec.db");
+      if (zcashPassword == null || zcashPassword.isEmpty) {
+        zcashPassword = generateKey().substring(0, 32);
+        secureStorageShared.write(
+            key: "com.cakewallet.cw_zcash/zec.db", value: zcashPassword);
+      }
+      zcash?.unlockDatabase(zcashPassword);
     }
-    zcash?.unlockDatabase(zcashPassword);
 
     // Basically when we're running a test
     if (topLevelKey != null) {
