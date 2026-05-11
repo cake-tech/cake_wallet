dce0c540492492c3baf044fba1d1294a0587ea11
Konstantin Ullrich <konstantinullrich12@gmail.com>
2026-05-11 14:00:04 +0200

    refactor: simplify `ZcashBalance` class by replacing custom fields with inherited properties, update references accordingly

--- cw_zcash/lib/src/zcash_balance.dart
+++ cw_zcash/lib/src/zcash_balance.dart
@@ -3,15 +3,11 @@ import 'package:cw_core/balance.dart';
 import 'package:cw_core/crypto_currency.dart';
 
 class ZcashBalance extends Balance {
-  ZcashBalance({required this.confirmed, required this.unconfirmed, required final Money frozen})
-    : super(confirmed, unconfirmed, frozen: frozen);
+  ZcashBalance(super.available, super.unavailable, {required super.frozen});
 
   factory ZcashBalance.zero() => ZcashBalance(
-    confirmed: Money.zero(CryptoCurrency.zec),
-    unconfirmed: Money.zero(CryptoCurrency.zec),
+    Money.zero(CryptoCurrency.zec),
+    Money.zero(CryptoCurrency.zec),
     frozen: Money.zero(CryptoCurrency.zec),
   );
-
-  final Money confirmed;
-  final Money unconfirmed;
 }
