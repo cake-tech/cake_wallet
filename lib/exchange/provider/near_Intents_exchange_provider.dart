2914952231b13969e4dc68482b6ba08c11a85098
Serhii <17529954+serhii-bor@users.noreply.github.com>
2026-05-19 23:05:12 +0300

    fix:near intents fixed-rate underpayment

--- lib/exchange/provider/near_Intents_exchange_provider.dart
+++ lib/exchange/provider/near_Intents_exchange_provider.dart
@@ -248,6 +248,11 @@ class NearIntentsExchangeProvider extends ExchangeProvider {
       final quoteObj = quote['quote'] as Map<String, dynamic>;
       final depositAddress = quoteObj['depositAddress'] as String;
       final depositMemo = quoteObj['depositMemo'] as String?;
+      final depositAmount = quoteObj['amountInFormatted'] as String?;
+
+      if (depositAmount == null) {
+        throw Exception('Deposit amount is null in quote response');
+      }
 
       final quoteRequest = quote['quoteRequest'] as Map<String, dynamic>;
       final fromAssetId = quoteRequest['originAsset'] as String;
@@ -281,7 +286,7 @@ class NearIntentsExchangeProvider extends ExchangeProvider {
         inputAddress: depositAddress,
         payoutAddress: request.toAddress,
         refundAddress: request.refundAddress,
-        amount: request.fromAmount,
+        amount: depositAmount,
         receiveAmount: quoteObj['amountOutFormatted']?.toString(),
         memo: depositMemo,
         isSendAll: isSendAll,
