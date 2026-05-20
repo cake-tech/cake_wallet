8bed7e20beab56443f22f351343a24df40fcffe2
Omar Hatem <omarh.ismail1@gmail.com>
2026-05-20 22:26:57 +0200

    fix conflict because github is shit

--- lib/exchange/provider/trocador_exchange_provider.dart
+++ lib/exchange/provider/trocador_exchange_provider.dart
@@ -235,11 +235,7 @@ class TrocadorExchangeProvider extends ExchangeProvider {
       if (!isFixedRateMode) 'amount_from': request.fromAmount,
       if (isFixedRateMode) 'amount_to': request.toAmount,
       'address': request.toAddress,
-<<<<<<< CW-1480-Add-zcash-name-resolution
-      'address_memo': request.toAddressExtraId.isNotEmpty ? request.toAddressExtraId : '0', // Trocador says to use '0' for no memo
-=======
       if (request.toAddressExtraId.isNotEmpty) 'address_memo': request.toAddressExtraId,
->>>>>>> dev
       'refund': request.refundAddress,
       'refund_memo': '0',
     };
