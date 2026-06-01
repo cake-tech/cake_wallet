6c1ee1651349cf02ee05c1edc26137118f59bbf9
malik1004x <malikowskirobert@gmail.com>
2026-06-01 21:32:12 +0200

    new-node-list-ui-on-dev (#3259)
    * node list new ui
    
    # Conflicts:
    #	cw_core/lib/node_list.dart
    
    * increase touch target sizes
    
    * fix scroll behavior
    
    * fix concurrent modification
    
    * Update lib/src/screens/settings/manage_nodes_page.dart [skip ci]
    
    * Update lib/src/screens/nodes/node_create_or_edit_page.dart [skip ci]
    
    * run speed test in parallel not sequential
    
    ---------
    
    Co-authored-by: Omar Hatem <omarh.ismail1@gmail.com>

---
+++ lib/src/screens/nodes/node_share_page.dart
@@ -0,0 +1,82 @@
+import 'package:cake_wallet/generated/i18n.dart';
+import 'package:cake_wallet/new-ui/widgets/receive_page/receive_top_bar.dart';
+import 'package:cake_wallet/src/screens/receive/widgets/qr_image.dart';
+import 'package:cake_wallet/src/widgets/cake_image_widget.dart';
+import 'package:cw_core/crypto_currency.dart';
+import 'package:flutter/material.dart';
+
+class NodeSharePage extends StatelessWidget {
+  const NodeSharePage({super.key, required this.uri, required this.currency});
+
+  final Uri uri;
+  final CryptoCurrency currency;
+
+  @override
+  Widget build(BuildContext context) {
+    final qrSize = MediaQuery.of(context).size.width * 0.786;
+    final padding = 16.0;
+    return Container(
+      color: Theme.of(context).colorScheme.surface,
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            ModalTopBar(
+              title: S.of(context).share,
+              leadingIcon: Icon(Icons.arrow_back_ios_new),
+              onLeadingPressed: Navigator.of(context).pop,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 40,
+                children: [
+                  Stack(alignment: Alignment.center, children: [
+                    SizedBox(
+                      width: qrSize,
+                      height: qrSize,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(18),
+                          child: QrImage(
+                              size: qrSize - padding * 2,
+                              embeddedImagePath: "",
+                              data: uri.toString())),
+                    ),
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: Stack(
+                        children: [
+                          CakeImageWidget(
+                            imageUrl: "assets/new-ui/settings_row_icons/nodes.svg",
+                            width: 54,
+                            height: 54,
+                          ),
+                          Align(
+                              alignment: Alignment.bottomRight,
+                              child: CakeImageWidget(
+                                imageUrl: currency.iconPath,
+                                width: 24,
+                                height: 24,
+                              ))
+                        ],
+                      ),
+                    ),
+                  ]),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 36.0),
+                    child: Text(S.of(context).node_share_desc,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Theme.of(context).colorScheme.onSurfaceVariant)),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
