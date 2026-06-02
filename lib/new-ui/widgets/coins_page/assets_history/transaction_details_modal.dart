077a397976310211de234d080f22cd56a492a096
Robert Malikowski <malikowskirobert@gmail.com>
2026-06-02 16:29:15 +0200

    add long press copy to tx history amount

--- lib/new-ui/widgets/coins_page/assets_history/transaction_details_modal.dart
+++ lib/new-ui/widgets/coins_page/assets_history/transaction_details_modal.dart
@@ -1,6 +1,7 @@
 import 'package:cake_wallet/entities/new_ui_entities/list_item/list_item.dart';
 import 'package:cake_wallet/entities/new_ui_entities/list_item/list_item_regular_row.dart';
 import 'package:cake_wallet/generated/i18n.dart';
+import 'package:cake_wallet/new-ui/widgets/copy_wrapper.dart';
 import 'package:cake_wallet/new-ui/widgets/receive_page/receive_top_bar.dart';
 import 'package:cake_wallet/routes.dart';
 import 'package:cake_wallet/src/screens/transaction_details/confirmations_list_item.dart';
@@ -81,9 +82,17 @@ class _TransactionDetailsModalState extends State<TransactionDetailsModal> {
                                       widget.transactionDetailsViewModel.formattedStatus,
                                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
                                 ),
-                                Text(
-                                  widget.transactionDetailsViewModel.formattedCryptoAmount,
-                                  style: TextStyle(fontSize: 28),
+                                CopyWrapper(
+                                  requireLongPress: true,
+                                  data: ClipboardData(text: widget.transactionDetailsViewModel.formattedCryptoAmount),
+                                  builder: (context, copied)=> AnimatedSwitcher(
+                                    duration: Duration(milliseconds: 300),
+                                    child: Text(
+                                      key: ValueKey(copied),
+                                      copied ? S.of(context).copied : widget.transactionDetailsViewModel.formattedCryptoAmount,
+                                      style: TextStyle(fontSize: 28, color: copied ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.onSurface),
+                                    ),
+                                  ),
                                 ),
                                 Padding(
                                   padding: const EdgeInsets.all(16.0),
