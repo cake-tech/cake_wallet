0f064b3e213f69ade7ce9b78eb5ab384c4818993
Robert Malikowski <malikowskirobert@gmail.com>
2026-06-02 15:39:37 +0200

    remove rescan from connections for mweb

--- lib/view_model/settings/connection_sync_view_model.dart
+++ lib/view_model/settings/connection_sync_view_model.dart
@@ -162,7 +162,7 @@ abstract class ConnectionSyncViewModelBase with Store {
   @action
   void setUseBlinkProtection(bool value) => _settingsStore.useBlinkProtection = value;
 
-  bool get hasRescan => _wallet.hasRescan && _wallet.type != WalletType.bitcoin;
+  bool get hasRescan => _wallet.hasRescan && _wallet.type != WalletType.bitcoin && _wallet.type != WalletType.litecoin;
 
   @action
   void setUseEtherscan(bool value) {
