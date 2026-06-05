4e74bf8ff7f954759847ffb96d9abe1bdcc53b43
Blazebrain <davidadegoke16@gmail.com>
2026-06-05 07:55:48 +0100

    fix: Solana GMT token showing the wrong decimal place

--- cw_solana/lib/default_spl_tokens.dart
+++ cw_solana/lib/default_spl_tokens.dart
@@ -86,7 +86,7 @@ class DefaultSPLTokens {
       name: 'GMT',
       symbol: 'GMT',
       mintAddress: '7i5KKsX2weiTkry7jA4ZwSuXGhs5eJBEjY8vVxR4pfRx',
-      decimal: 6,
+      decimal: 9,
       mint: 'ray',
       iconPath: 'assets/images/gmt_icon.png',
       enabled: false,
