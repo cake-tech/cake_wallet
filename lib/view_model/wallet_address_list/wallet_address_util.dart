a2711f851cf6971f5489ad216bbe94a2254636f7
Robert Malikowski <malikowskirobert@gmail.com>
2026-06-13 13:03:40 +0200

    improve address rotation button ui + fix bug when autogenerate is disabled

--- lib/view_model/wallet_address_list/wallet_address_util.dart
+++ lib/view_model/wallet_address_list/wallet_address_util.dart
@@ -29,6 +29,7 @@ Future<void> createNewAddress(WalletBase wallet, String label) async {
         .getSubaddressList(wallet)
         .subaddresses;
     if(subaddressList.isEmpty) {
+      print("subaddress list empty!");
       // this shouldn't happen, we just added an addr.
       // somehow, it happened once in prod regardless.
       // we just return instead of crashing, user can press the button again ig
