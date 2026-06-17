e7ba00bc2ea00c551a4184274934d23648287dea
mmokhi <mmokhi@freebsd.org>
2026-06-17 09:56:39 +0200

    feat: wire Nerva into android build config, codegen and import lint
    cw_nerva added to model_generator.sh (build_runner), android pubspec_gen CONFIG_ARGS, the docker build-dir cleanup, and the no_restricted_imports list.

--- scripts/android/pubspec_gen.sh
+++ scripts/android/pubspec_gen.sh
@@ -10,7 +10,7 @@ case $APP_ANDROID_TYPE in
                 CONFIG_ARGS="--monero"
                 ;;
         $CAKEWALLET)
-                CONFIG_ARGS="--monero --bitcoin --ethereum --polygon --nano --bitcoinCash --solana --tron --wownero --zano --decred --dogecoin --base --zcash --arbitrum --bsc"
+                CONFIG_ARGS="--monero --bitcoin --ethereum --polygon --nano --bitcoinCash --solana --tron --wownero --nerva --zano --decred --dogecoin --base --zcash --arbitrum --bsc"
                 ;;
 esac
 
