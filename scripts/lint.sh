676d24b769bafb10175628896beab66978031758
Robert Malikowski <malikowskirobert@gmail.com>
2026-06-29 19:53:47 +0200

    return to old lint.sh to not format deps in scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,11 +3,16 @@ set -x -e
 cd $(dirname $0)
 cd ..
 
-dart fix --apply .
-dart format --line-length=100 .
-if [[ ! "x$ABORT_ON_CHANGE" == "x" ]]; then
-  if [[ ! -z "$(git status --porcelain -- $(find .))" ]]; then
-    echo "Please run scripts/lint.sh"
-    exit 1
-  fi
-fi
+for i in lib cw_zcash;
+do
+    dart fix --apply $i/
+    dart format --line-length=100 $i/
+    if [[ ! "x$ABORT_ON_CHANGE" == "x" ]];
+    then
+        if [[ ! -z "$(git status --porcelain -- $(find $i))" ]];
+        then
+            echo "Please run scripts/lint.sh ($i has changes)"
+            exit 1
+        fi
+    fi
+done
