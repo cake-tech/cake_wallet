46cda778d8a1234eda586f7b3e72a23520e5ef5a
Robert Malikowski <malikowskirobert@gmail.com>
2026-07-01 17:25:13 +0200

    fix date format in tx details

--- lib/view_model/transaction_details_view_model.dart
+++ lib/view_model/transaction_details_view_model.dart
@@ -30,6 +30,7 @@ import 'package:cw_core/transaction_direction.dart';
 import 'package:cw_core/transaction_priority.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
 import 'package:mobx/mobx.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -77,8 +78,7 @@ class TxDetailRowDefinition {
     TxDetailRowDefinition(
         keyString: "standard_list_item_transaction_details_date_key",
         title: S.current.transaction_details_date,
-        valueGetter: (vm) => DateFormatter.withCurrentLocal().format(vm.transactionInfo.date)),
-
+        valueGetter: (vm) => DateFormat("d MMMM yyyy, HH:mm", vm._appStore.settingsStore.languageCode).format(vm.transactionInfo.date)),
     TxDetailRowDefinition(
       keyString: "standard_list_item_transaction_details_height_key",
       title: S.current.transaction_details_height,
