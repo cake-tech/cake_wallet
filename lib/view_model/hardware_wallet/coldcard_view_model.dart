a79882100e375eff79a7a7ed3d5f4f24094136d8
rift <32559038+warp-records@users.noreply.github.com>
2026-07-05 17:22:22 -0400

    implement some of viewmodel

---
+++ lib/view_model/hardware_wallet/coldcard_view_model.dart
@@ -0,0 +1,50 @@
+import 'package:cake_wallet/entities/hardware_wallet/hardware_wallet_device.dart';
+import 'package:cake_wallet/view_model/hardware_wallet/hardware_wallet_view_model.dart';
+import 'package:coldcard_protocol/client.dart';
+import 'package:coldcard_usb/coldcard_usb_transport.dart';
+import 'package:coldcard_usb/ledger_usb_platform_interface.dart';
+import 'package:cw_core/hardware/hardware_wallet_service.dart';
+import 'package:cw_core/wallet_base.dart';
+import 'package:cw_core/wallet_info.dart';
+import 'package:cw_core/wallet_type.dart';
+
+class ColdcardViewModel implements HardwareWalletViewModel {
+  HardwareWalletType get hardwareWalletType => HardwareWalletType.coldcardUsb;
+  bool get isBleEnabled => false;
+  bool get hasBluetooth => false;
+  bool _isConnecting = false;
+
+  ColdCardDevice? _dev;
+
+  bool isConnected(WalletType type) {
+    return _dev != null;
+  }
+
+  // Future<void> updateBleState();
+
+  // Stream<HardwareWalletDevice> scanForBleDevices();
+
+  // only returns first coldcard device found
+  @override
+  Future<List<HardwareWalletDevice>> getAllUsbDevices() async {
+    final usbDevices = await ColdcardUsbPlatform.instance.getDevices();
+    final found = usbDevices.any((d) => d.vendorId == ColdCardDevice.coinkiteVid && d.productId == ColdCardDevice.ckccPid);
+    return found ? [ColdcardHardwareWalletDevice()] : [];
+  }
+
+  // Future<void> stopScanning();
+
+  Future<bool> connectDevice(HardwareWalletDevice device, WalletType type) async {
+    if (!(device is ColdcardHardwareWalletDevice)) return false;
+    if (_isConnecting) return false;
+    _isConnecting = true;
+
+    _dev = await ColdCardDevice.create(ColdcardUsbTransport());
+  }
+
+  // HardwareWalletService getHardwareWalletService(WalletType type);
+
+  // Future<void> initWallet(WalletBase wallet);
+
+  // String? interpretErrorCode(String error) => null;
+}
