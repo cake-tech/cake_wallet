d82a977e9e1382e64e93ad73d3165ce9be4c4ab9
Omar <omarh.ismail1@gmail.com>
2026-07-06 04:48:39 +0300

    ignore flutter error [skip ci]

--- lib/utils/exception_handler.dart
+++ lib/utils/exception_handler.dart
@@ -305,8 +305,8 @@ class ExceptionHandler {
     "SdkError.sparkError(field0: Operator RPC error: Connection error: status: Unavailable, message: \"dns error\", details: []",
     "the timeout of the request was reached",
 
-    "support for coin removed, your seedphrase:"
-    "Exception: Invalid image data"
+    "support for coin removed, your seedphrase:",
+    "Exception: Invalid image data",
   ];
 
   static Future<void> _addDeviceInfo(File file) async {
@@ -441,8 +441,20 @@ class ExceptionHandler {
   }
 
   static bool _flutterErrorIgnore(FlutterErrorDetails errorDetails) {
-    // most probably a flutter context error so just ignore it if there is no stack we can debug with
-    return errorDetails.exception.toString().contains("Null check operator used on a null value") &&
-        errorDetails.stack == null;
+    if (errorDetails.exception.toString().contains("Null check operator used on a null value")) {
+      // Most probably a flutter context error so just ignore it if there is no
+      // stack we can debug with.
+      if (errorDetails.stack == null) {
+        return true;
+      }
+
+      final stack = errorDetails.stack.toString();
+      if (stack.contains("handleFocusHighlightModeChange") ||
+          stack.contains("_HighlightModeManager")) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
