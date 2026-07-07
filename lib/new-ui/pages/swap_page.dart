26c949ebac4dbf43fc684ee1ac704ba49d75ee77
Robert Malikowski <malikowskirobert@gmail.com>
2026-07-07 23:48:49 +0200

    fix rate fetching when switching currency for send all

--- lib/new-ui/pages/swap_page.dart
+++ lib/new-ui/pages/swap_page.dart
@@ -271,7 +271,9 @@ class _NewSwapPageState extends State<NewSwapPage> {
           widget.exchangeViewModel.changeReceiveAmount(amount: receiveAmountController.text);
         } else {
           printV("bestrate");
-          if (depositKey.currentState?._fiatInputMode?? false || depositAmountController.text.isEmpty || depositAmountController.text == S.current.all)
+          if ((depositKey.currentState?._fiatInputMode ?? false) ||
+              depositAmountController.text.isEmpty ||
+              depositAmountController.text == S.current.all)
             widget.exchangeViewModel
                 .changeDepositAmount(amount: widget.exchangeViewModel.depositAmount);
           else
