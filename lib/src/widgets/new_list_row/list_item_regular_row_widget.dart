3706c2afa53ce345bf499a3dd2144529e44c7164
Robert Malikowski <malikowskirobert@gmail.com>
2026-07-14 22:04:37 +0200

    merge

--- lib/src/widgets/new_list_row/list_item_regular_row_widget.dart
+++ lib/src/widgets/new_list_row/list_item_regular_row_widget.dart
@@ -147,7 +147,7 @@ class ListItemRegularRowWidget extends StatelessWidget {
                                       ),
                                   ],
                                 ),
-                              ),
+                              ),),
                             ],
                           ),
                         ),
