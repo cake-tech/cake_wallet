031a117f2e62cc883949a1e6e03a6addc55f0dec
Cindy <bc1cindy@proton.me>
2026-07-13 23:39:03 -0300

    Shuffle input order on bitcoin sends (#3379)

--- cw_bitcoin/lib/electrum_wallet.dart
+++ cw_bitcoin/lib/electrum_wallet.dart
@@ -1537,6 +1537,7 @@ abstract class ElectrumWalletBase
           fee: estimatedTx.fee.amount,
           network: network,
           memo: estimatedTx.memo,
+          inputOrdering: BitcoinOrdering.shuffle,
           outputOrdering: BitcoinOrdering.none,
           enableRBF: !estimatedTx.spendsUnconfirmedTX,
         );
@@ -2302,6 +2303,7 @@ abstract class ElectrumWalletBase
         fee: BigInt.from(newFee),
         network: network,
         memo: memo,
+        inputOrdering: BitcoinOrdering.shuffle,
         outputOrdering: BitcoinOrdering.none,
         enableRBF: true,
       );
