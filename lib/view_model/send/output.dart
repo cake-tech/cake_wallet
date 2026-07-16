9a94fb6532f09ad01c43c8fd9055f88530e4c6e7
Blazebrain <davidadegoke16@gmail.com>
2026-07-16 11:09:41 +0100

    chore: set estimated fee to 0 when switching wallet

--- lib/view_model/send/output.dart
+++ lib/view_model/send/output.dart
@@ -261,6 +261,7 @@ abstract class OutputBase with Store {
   void updateWallet(
       WalletBase<Balance, TransactionHistoryBase<TransactionInfo>, TransactionInfo> newWallet) {
     _wallet = newWallet;
+    estimatedFee = Money.zero(cryptoCurrencyHandler());
   }
 
   @action
