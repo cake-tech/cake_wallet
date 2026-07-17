84e4984c02c86942fa65be256e0d57bbe0aec046
Robert Malikowski <malikowskirobert@gmail.com>
2026-07-17 11:18:39 +0200

    add CakeException extends to preexisting exceptions

--- cw_core/lib/exceptions.dart
+++ cw_core/lib/exceptions.dart
@@ -1,72 +1,135 @@
 import 'package:cw_core/crypto_currency.dart';
+import "package:cw_core/exceptions/cake_exception.dart";
 
-class TransactionWrongBalanceException implements Exception {
+class TransactionWrongBalanceException implements TransactionGenerationException {
   TransactionWrongBalanceException(this.currency, {this.amount});
+
+  @override
+  String get message => "wrong balance: ${currency.symbol} $amount";
 
   final CryptoCurrency currency;
   final int? amount;
 }
 
-class TransactionNoInputsException implements Exception {}
+class TransactionNoInputsException implements TransactionGenerationException {
 
-class TransactionNoFeeException implements Exception {}
+  @override
+  String get message => "Not enough inputs available. Please select more under Coin Control";
+}
 
-class TransactionNoDustException implements Exception {}
+class TransactionNoFeeException implements TransactionGenerationException {
+  @override
+  String get message => "cannot send with zero fee";
+}
 
-class TransactionNoDustOnChangeException implements Exception {
+class TransactionNoDustException implements TransactionGenerationException {
+
+  @override
+  String get message =>
+      "The transaction is rejected by sending an amount too small. Please try increasing the amount.";
+}
+
+class TransactionNoDustOnChangeException implements TransactionGenerationException {
   TransactionNoDustOnChangeException(this.max, this.min);
 
   final String max;
   final String min;
+
+  @override
+  String get message =>
+      "The transaction is rejected with this amount. With these coins you can send ${min} without change or ${max} that returns change.";
 }
 
-class TransactionCommitFailed implements Exception {
+class TransactionCommitFailed implements TransactionSendingException {
   final String? errorMessage;
 
   TransactionCommitFailed({this.errorMessage});
 
   @override
-  String toString() => errorMessage ?? "unknown error";
+  String get message => errorMessage ?? "unknown error";
+
 }
 
-class TransactionCommitFailedDustChange implements Exception {}
+class TransactionCommitFailedDustChange implements TransactionSendingException {
+  @override
+  String get message =>
+      "Transaction rejected by network rules, low change amount (dust). Try sending ALL or reducing the amount.";
+}
 
-class TransactionCommitFailedDustOutput implements Exception {}
+class TransactionCommitFailedDustOutput implements TransactionSendingException {
 
-class TransactionCommitFailedDustOutputSendAll implements Exception {}
+  @override
+  String get message =>
+      "Transaction rejected by network rules, low output amount (dust). Please increase the amount.";
+}
 
-class TransactionCommitFailedVoutNegative implements Exception {}
+class TransactionCommitFailedDustOutputSendAll implements TransactionSendingException {
+  @override
+  String get message =>
+      "Transaction rejected by network rules, low output amount (dust). Please check the balance of coins selected under Coin Control.";
+}
 
-class TransactionCommitFailedBIP68Final implements Exception {}
+class TransactionCommitFailedVoutNegative implements TransactionSendingException {
+  @override
+  String get message =>
+      "Not enough balance to pay for this transaction's fees. Please check the balance of coins under Coin Control.";
+}
 
-class TransactionCommitFailedLessThanMin implements Exception {}
+class TransactionCommitFailedBIP68Final implements TransactionSendingException {
+  @override
+  String get message => "Transaction has unconfirmed inputs and failed to replace by fee.";
+}
 
-class TransactionInputNotSupported implements Exception {}
+class TransactionCommitFailedLessThanMin implements TransactionSendingException {
+  @override
+  String get message =>
+      "Selected Fee is less than the minimum, please increase the fees to be able to send the transaction";
+}
 
-class SignNativeTokenTransactionRentException implements Exception {}
+class TransactionInputNotSupported implements TransactionSendingException {
+  @override
+  String get message => "You are using the wrong input type for this type of payment";
+}
 
-class CreateAssociatedTokenAccountException implements Exception {
+class SignNativeTokenTransactionRentException implements TransactionGenerationException {
+  @override
+  String get message =>
+      "Transaction cannot be completed. Insufficient SOL left for rent after transaction. Kindly top up your SOL balance or reduce the amount of SOL you are sending.";
+}
+
+class CreateAssociatedTokenAccountException implements CakeException {
   final String errorMessage;
 
   CreateAssociatedTokenAccountException(this.errorMessage);
+
+  @override
+  String get message =>
+      "Error creating associated token account for receipient address. " + errorMessage;
 }
 
-class SignSPLTokenTransactionRentException implements Exception {}
+class SignSPLTokenTransactionRentException implements CakeException {
+  @override
+  String get message =>
+      "Transaction cannot be completed. Insufficient SOL left for rent after transaction. Kindly top up your SOL balance.";
+}
 
-class NoAssociatedTokenAccountException implements Exception {}
+class NoAssociatedTokenAccountException implements CakeException {
+  @override
+  String get message => "There is no associated token account for this address.";
+}
 
-class RestoreFromSeedException implements Exception {
+class RestoreFromSeedException implements CakeException {
   final String message;
 
   RestoreFromSeedException(this.message);
 }
 
-class WalletDeprecationException implements Exception {
+class WalletDeprecationException implements CakeException {
   final String seed;
   final CryptoCurrency curr;
 
   @override
-  String toString() => "Wallet type no longer supported";
+  String get message => "Wallet type no longer supported";
 
   WalletDeprecationException({required this.seed, required this.curr});
 }
