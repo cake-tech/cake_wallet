4477939d3b9563f0691fed4437a1608802ea68b2
Konstantin Ullrich <konstantinullrich12@gmail.com>
2026-07-17 14:43:50 +0200

    fix: ui bugs in relation to trezor

--- lib/src/screens/connect_device/monero_hardware_wallet_passphrase_input.dart
+++ lib/src/screens/connect_device/monero_hardware_wallet_passphrase_input.dart
@@ -1,35 +1,33 @@
-import 'package:cake_wallet/generated/i18n.dart';
-import 'package:cake_wallet/new-ui/widgets/new_primary_button.dart';
-import 'package:cake_wallet/new-ui/widgets/receive_page/receive_top_bar.dart';
-import 'package:cake_wallet/src/widgets/base_text_form_field.dart';
-import 'package:flutter/material.dart';
+import "package:cake_wallet/generated/i18n.dart";
+import "package:cake_wallet/new-ui/widgets/new_primary_button.dart";
+import "package:cake_wallet/new-ui/widgets/receive_page/receive_top_bar.dart";
+import "package:cake_wallet/src/widgets/base_text_form_field.dart";
+import "package:flutter/material.dart";
 
 class MoneroHardwareWalletPassphraseInputModal extends StatelessWidget {
-  const MoneroHardwareWalletPassphraseInputModal({super.key, required this.controller});
+  const MoneroHardwareWalletPassphraseInputModal({required this.controller, super.key});
 
   final TextEditingController controller;
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.vertical(top: Radius.circular(18)),
-          color: Theme.of(context).colorScheme.surface,
-        ),
-        child: SafeArea(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ModalTopBar(
-                title: S.of(context).passphrase_entry,
-                leadingIcon: const Icon(Icons.close),
-                onLeadingPressed: Navigator.of(context).pop,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 18),
-                child: Expanded(
+  Widget build(BuildContext context) => Padding(
+        padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(18)),
+            color: Theme.of(context).colorScheme.surface,
+          ),
+          child: SafeArea(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ModalTopBar(
+                  title: S.of(context).passphrase_entry,
+                  leadingIcon: const Icon(Icons.close),
+                  onLeadingPressed: Navigator.of(context).pop,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 18),
                   child: Column(
                     spacing: 12,
                     children: [
@@ -37,22 +35,20 @@ class MoneroHardwareWalletPassphraseInputModal extends StatelessWidget {
                         controller: controller,
                         hintText: S.of(context).passphrase_raw,
                       ),
-                      SizedBox(),
+                      const SizedBox(),
                       NewPrimaryButton(
                         onPressed: Navigator.of(context).pop,
                         text: S.of(context).restore_next,
                         color: Theme.of(context).colorScheme.primary,
                         textColor: Theme.of(context).colorScheme.onPrimary,
                       ),
-                      SizedBox()
+                      const SizedBox(),
                     ],
                   ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
