06b6e8c5b9551f0ed554dbb3bd39ebfbcb54884d
Robert Malikowski <malikowskirobert@gmail.com>
2026-07-27 11:43:46 +0200

    add toString to cw_mweb

--- cw_mweb/lib/mweb_exceptions.dart
+++ cw_mweb/lib/mweb_exceptions.dart
@@ -1,6 +1,9 @@
 abstract class CakeException implements Exception {
   const CakeException(this.message);
   final String message;
+
+  @override
+  String toString() => message;
 }
 
 class ConnectionException extends CakeException {
