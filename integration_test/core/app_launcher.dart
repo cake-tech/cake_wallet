2d48513ac7ae85981fe1183b6e7980177d7855d9
Blazebrain <davidadegoke16@gmail.com>
2026-08-06 14:20:23 +0100

    fix: Import FlutterExceptionHandler from foundation so the test build compiles
    This change:
    - Imports package:flutter/foundation.dart in the launcher, material.dart only re-exports Brightness and UniqueKey from foundation so FlutterExceptionHandler was not in scope
    - Drops the material.dart import, every symbol the launcher uses comes from foundation

--- integration_test/core/app_launcher.dart
+++ integration_test/core/app_launcher.dart
@@ -1,7 +1,7 @@
 import "dart:async";
 
 import "package:cake_wallet/main.dart" as app;
-import "package:flutter/material.dart";
+import "package:flutter/foundation.dart";
 import "package:flutter_test/flutter_test.dart";
 
 import "benign_errors.dart";
