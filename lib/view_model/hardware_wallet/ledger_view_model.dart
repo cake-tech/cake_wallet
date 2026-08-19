1d1430ec5137b882f3c723bdc0fe8d7694a5c146
Konstantin Ullrich <konstantinullrich12@gmail.com>
2026-08-19 17:16:18 +0200

    fix: move `updateBleState` to connection page initialization

--- lib/view_model/hardware_wallet/ledger_view_model.dart
+++ lib/view_model/hardware_wallet/ledger_view_model.dart
@@ -42,8 +42,6 @@ abstract class LedgerViewModelBase extends HardwareWalletViewModel with Store {
         }
       });
 
-      updateBleState();
-
       if (!Platform.isIOS) {
         ledgerPlusUSB = sdk.LedgerInterface.usb();
       }
