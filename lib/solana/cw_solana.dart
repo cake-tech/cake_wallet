cba928b1ab0fb9f896963ab089537bb0f2af2873
Robert Malikowski <malikowskirobert@gmail.com>
2026-08-24 12:06:15 +0200

    revert unrelated change

--- lib/solana/cw_solana.dart
+++ lib/solana/cw_solana.dart
@@ -132,7 +132,7 @@ class CWSolana extends Solana {
 
     if (token == null) {
       throw StateError('No SPL token for symbol ${transaction.amount.currency.symbol}');
-        }
+    }
 
     return token;
   }
