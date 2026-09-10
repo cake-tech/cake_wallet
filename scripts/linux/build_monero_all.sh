ea8e7523856f250ea5c60ee92800b146a2e0372f
Czarek Nakamoto <cyjan@mrcyjanek.net>
2026-09-11 07:42:25 +0800

    chore: migrate to hosted scalable CI

--- scripts/linux/build_monero_all.sh
+++ scripts/linux/build_monero_all.sh
@@ -7,16 +7,23 @@ cd "$(dirname "$0")"
 
 ../prepare_moneroc.sh
 
-for COIN in monero wownero;
+if [[ -n "${COIN:-}" ]]; then
+    COINS=("$COIN")
+else
+    COINS=(monero wownero)
+fi
+
+if [[ -n "${TARGET:-}" ]]; then
+    target="$TARGET"
+elif [[ $(uname -m) == "arm64" || $(uname -m) == "aarch64" ]]; then
+    target="aarch64-linux-gnu"
+else
+    target="x86_64-linux-gnu"
+fi
+
+for COIN in "${COINS[@]}";
 do
     pushd ../monero_c
-        # Determine target architecture based on system architecture
-        if [[ $(uname -m) == "arm64" || $(uname -m) == "aarch64" ]]; then
-            target="aarch64-linux-gnu"
-        else
-            target="x86_64-linux-gnu"
-        fi
-
         ./build_single.sh ${COIN} $target -j$MAKE_JOB_COUNT
     popd
 done
--- scripts/linux/build_torch.sh
+++ scripts/linux/build_torch.sh
@@ -2,8 +2,18 @@
 set -x -e
 cd "$(dirname "$0")"
 
+DEFAULT_TARGETS=(
+  "x86_64-linux-gnu"
+)
+
+if [ "$#" -gt 0 ]; then
+  TARGETS=("$@")
+else
+  TARGETS=("${DEFAULT_TARGETS[@]}")
+fi
+
 ../prepare_torch.sh
 
 cd ../torch_dart
 
-./build.sh x86_64-linux-gnu
+./build.sh "${TARGETS[@]}"
