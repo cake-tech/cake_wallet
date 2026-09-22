b88fbf32ae47cfe894703738d49fd35b263d7cc3
malik1004x <malikowskirobert@gmail.com>
2026-09-22 20:32:38 +0200

    charts (#3162)
    * charts (wip)
    
    * charts (wip)
    
    * wip
    
    * add asset grid
    
    * split files
    
    * logic wip
    
    * proper getPrices logic
    
    * viewmodel (wip)
    
    * charts
    
    * remove unused import
    
    * remove restricted import...?
    
    * fix background gradient when scrolling
    
    * add safeguards for broken db
    
    * save new pin on removal
    
    * add safeguards for concurrency
    
    * add haptic feedback
    
    * fix haptic feedback
    
    * fix fiat ticker
    
    * reduce chart padding
    
    * add line touch indicator
    
    * improve touch target size on range selector
    
    * add date/time display when viewing past amount
    
    * merge
    
    * remove iconSvgPath reference
    
    * merge
    
    * fix state for date display
    
    * merge
    
    * auto-reformat
    
    * only load charts when page is opened
    
    * merge
    
    * reformat and apply lints
    
    * refactor to use `Money` instead of storing amount in `String`
    
    * add slop comment
    
    * fix exception on division by zero
    
    * ci
    
    * workaround toDouble being removed
    
    * disable strict parsing and rounding
    
    * fix abs()
    
    * use new currency picker sheet
    
    * safeguard for max flutter-supported decimals
    
    * fix amount precision
    
    * adjust to new buy/sell
    
    * Update lib/new-ui/pages/charts_page.dart
    
    Co-authored-by: Omar Hatem <omarh.ismail1@gmail.com>
    
    * adjust buy/sell di
    
    * replace -> ignore
    
    * reformat
    
    ---------
    
    Co-authored-by: Omar Hatem <omarh.ismail1@gmail.com>

--- lib/di.dart
+++ lib/di.dart
@@ -52,6 +52,7 @@ import 'package:cake_wallet/exchange/trade.dart';
 import 'package:cake_wallet/haven/cw_haven.dart';
 import 'package:cake_wallet/monero/monero.dart';
 import 'package:cake_wallet/nano/nano.dart';
+import 'package:cake_wallet/new-ui/model/charts/price_store.dart';
 import 'package:cake_wallet/new-ui/new_dashboard.dart';
 import 'package:cake_wallet/new-ui/pages/about_page.dart';
 import 'package:cake_wallet/new-ui/pages/account_customizer.dart';
@@ -59,6 +60,9 @@ import 'package:cake_wallet/new-ui/pages/bridge/bridge_amount_page.dart';
 import 'package:cake_wallet/new-ui/pages/bridge/bridge_network_page.dart';
 import 'package:cake_wallet/new-ui/pages/bridge/bridge_receiving_wallet_page.dart';
 import 'package:cake_wallet/new-ui/pages/buy_sell/buy_sell_amount_page.dart';
+import 'package:cake_wallet/new-ui/pages/bridge/bridge_network_page.dart';
+import 'package:cake_wallet/new-ui/pages/bridge/bridge_receiving_wallet_page.dart';
+import 'package:cake_wallet/new-ui/pages/charts_page.dart';
 import 'package:cake_wallet/new-ui/pages/coin_control_page.dart';
 import 'package:cake_wallet/new-ui/pages/addresses_page.dart';
 import 'package:cake_wallet/new-ui/pages/home_page.dart';
@@ -68,6 +72,7 @@ import 'package:cake_wallet/new-ui/pages/lightning_username_page.dart';
 import 'package:cake_wallet/new-ui/pages/receive_page.dart';
 import "package:cake_wallet/new-ui/pages/seed/pre_seed_page.dart";
 import "package:cake_wallet/new-ui/pages/seed/show_keys_disclaimer_page.dart";
+import 'package:cake_wallet/new-ui/viewmodels/charts/charts_bloc.dart';
 import 'package:cake_wallet/new-ui/viewmodels/lightning_username/lightning_username_bloc.dart';
 import 'package:cake_wallet/new-ui/widgets/addresses_page/address_label_input.dart';
 import 'package:cake_wallet/new-ui/widgets/buy_sell/buy_sell_selector_modal.dart';
@@ -551,6 +556,15 @@ Future<void> setup({
             (displayMode == BitcoinAmountDisplayMode.satoshi ||
                 (displayMode == BitcoinAmountDisplayMode.satoshiForLightning && lightningMode)));
   });
+
+  getIt.registerLazySingleton<PriceStore>(() => PriceStore());
+
+  getIt.registerFactory<ChartsBloc>(
+      () => ChartsBloc(appStore: getIt.get<AppStore>(), priceStore: getIt.get<PriceStore>()));
+
+  getIt.registerFactory<ChartsPage>(() => ChartsPage(
+        chartsBloc: getIt.get<ChartsBloc>(),
+      ));
 
   getIt.registerFactory<AccountCreationModal>(() => AccountCreationModal(
       accountEditOrCreateViewModel: getIt.get<MoneroAccountEditOrCreateViewModel>()));
@@ -1321,11 +1335,11 @@ Future<void> setup({
 
   getIt.registerFactory(() => BuyAmountViewModel());
 
-  getIt.registerFactoryParam<BuySellViewModel, BuySellPageMode, void>((mode, _) => BuySellViewModel(
-      mode: mode, getIt.get<AppStore>(), fiatConversionStore: getIt.get<FiatConversionStore>()));
+  getIt.registerFactoryParam<BuySellViewModel, NewBuySellParams, void>((params, _) => BuySellViewModel(
+      params: params, getIt.get<AppStore>(), fiatConversionStore: getIt.get<FiatConversionStore>()));
 
-  getIt.registerFactoryParam<NewBuySellAmountPage, BuySellPageMode, void>((mode, _) =>
-      NewBuySellAmountPage(buySellViewModel: getIt.get<BuySellViewModel>(param1: mode)));
+  getIt.registerFactoryParam<NewBuySellAmountPage, NewBuySellParams, void>((params, _) =>
+      NewBuySellAmountPage(buySellViewModel: getIt.get<BuySellViewModel>(param1: params)));
 
 
   getIt.registerFactoryParam<OrderDetailsViewModel, Order, void>((order, _) {
