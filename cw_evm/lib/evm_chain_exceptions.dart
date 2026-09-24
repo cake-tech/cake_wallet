cacf5381b6a77226da6dc5ee6f0d4b11efb935d0
Robert Malikowski <malikowskirobert@gmail.com>
2026-09-24 09:45:07 +0200

    make evm exceptions extend generation exception

--- cw_evm/lib/evm_chain_exceptions.dart
+++ cw_evm/lib/evm_chain_exceptions.dart
@@ -1,61 +1,46 @@
-import 'package:cw_core/crypto_currency.dart';
+import "package:cw_core/crypto_currency.dart";
+import "package:cw_core/exceptions/cake_exception.dart";
 
 class EVMChainMnemonicIsIncorrectException implements Exception {
   @override
   String toString() =>
-      'EVM mnemonic has incorrect format. Mnemonic should contain 12 or 24 words separated by space.';
+      "EVM mnemonic has incorrect format. Mnemonic should contain 12 or 24 words separated by space.";
 }
 
-class EVMChainTransactionCreationException implements Exception {
-  final String exceptionMessage;
-
+class EVMChainTransactionCreationException extends TransactionGenerationException {
   EVMChainTransactionCreationException(CryptoCurrency currency)
-      : exceptionMessage = 'Wrong balance. Not enough ${currency.title} on your balance.';
+      : super("Wrong balance. Not enough ${currency.title} on your balance.");
 
-  EVMChainTransactionCreationException.fromMessage(this.exceptionMessage);
-
-  @override
-  String toString() => exceptionMessage;
+  EVMChainTransactionCreationException.fromMessage(super.message);
 }
 
-class EVMChainTransactionFeesException implements Exception {
-  final String exceptionMessage;
-
-  EVMChainTransactionFeesException(String message) : exceptionMessage = message;
+class EVMChainTransactionFeesException extends TransactionGenerationException {
+  EVMChainTransactionFeesException(super.message);
 
   EVMChainTransactionFeesException.fromCurrency(String currency)
-      : exceptionMessage =
-            'Transaction failed due to insufficient $currency balance to cover the fees.';
-
-  @override
-  String toString() => exceptionMessage;
+      : super("Transaction failed due to insufficient $currency balance to cover the fees.");
 }
 
-class InsufficientGasFeeException implements Exception {
-  final String exceptionMessage;
-  final BigInt? requiredGasFee;
-  final BigInt? currentBalance;
-
+class InsufficientGasFeeException extends TransactionGenerationException {
   InsufficientGasFeeException({
     this.requiredGasFee,
     this.currentBalance,
-  }) : exceptionMessage = _buildMessage(requiredGasFee, currentBalance);
+  }) : super(_buildMessage(requiredGasFee, currentBalance));
+  final BigInt? requiredGasFee;
+  final BigInt? currentBalance;
 
   static String _buildMessage(BigInt? requiredGasFee, BigInt? currentBalance) {
-    const baseMessage = 'Insufficient ETH for gas fees.';
-    const addEthMessage = ' Please add ETH to your wallet to cover transaction fees.';
+    const baseMessage = "Insufficient ETH for gas fees.";
+    const addEthMessage = " Please add ETH to your wallet to cover transaction fees.";
 
     if (requiredGasFee != null) {
       final requiredEth = (requiredGasFee / BigInt.from(10).pow(18)).toStringAsFixed(8);
       final balanceInfo = currentBalance != null
-          ? ', Available: ${(currentBalance / BigInt.from(10).pow(18)).toStringAsFixed(8)} ETH'
-          : '';
-      return '$baseMessage Required: ~$requiredEth ETH$balanceInfo.$addEthMessage';
+          ? ", Available: ${(currentBalance / BigInt.from(10).pow(18)).toStringAsFixed(8)} ETH"
+          : "";
+      return "$baseMessage Required: ~$requiredEth ETH$balanceInfo.$addEthMessage";
     }
 
-    return '$baseMessage$addEthMessage';
+    return "$baseMessage$addEthMessage";
   }
-
-  @override
-  String toString() => exceptionMessage;
 }
